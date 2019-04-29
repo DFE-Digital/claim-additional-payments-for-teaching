dbfbd5ad2ddf5cb55187e66778d47471fdc318c7
Tekin Suleyman <tekin@tekin.co.uk>
2019-04-29 16:47:44 +0100

    Generate QTS award year options from valid years
    Fewer places to update if/when the list of allowable years changes.

--- app/helpers/claims_helper.rb
+++ app/helpers/claims_helper.rb
@@ -1,13 +1,8 @@
 module ClaimsHelper
   def options_for_qts_award_year
-    [
-      ["September 1 2013 - August 31 2014", "2013-2014"],
-      ["September 1 2014 - August 31 2015", "2014-2015"],
-      ["September 1 2015 - August 31 2016", "2015-2016"],
-      ["September 1 2016 - August 31 2017", "2016-2017"],
-      ["September 1 2017 - August 31 2018", "2017-2018"],
-      ["September 1 2018 - August 31 2019", "2018-2019"],
-      ["September 1 2019 - August 31 2020", "2019-2020"],
-    ]
+    TslrClaim::VALID_QTS_YEARS.map do |academic_years|
+      start_year, end_year = academic_years.split("-")
+      ["September 1 #{start_year} - August 31 #{end_year}", academic_years]
+    end
   end
 end
