52cc350e3230d67f0a82183b385a52ea65db4ce0
Tekin Suleyman <tekin@tekin.co.uk>
2019-04-29 13:22:47 +0100

    Raise on unpermitted parameters in test and dev
    It's helpful to get noisy feedback when unpermitted params are generated
    in development or test mode.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,6 +44,9 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # Raise an exception if parameters that are not explicitly permitted are found
+  config.action_controller.action_on_unpermitted_parameters = :raise
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
