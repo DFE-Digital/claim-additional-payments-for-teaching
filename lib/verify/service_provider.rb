803dd22d1f11387f4bbee770496fe729ece6e420
Tekin Suleyman <tekin@tekin.co.uk>
2019-06-26 16:35:33 +0100

    Add Verify::ServiceProvider#translate_response
    This method is what will translate SAML responses from Verify to get the
    results of the identity assurance attempt.

--- lib/verify/service_provider.rb
+++ lib/verify/service_provider.rb
@@ -1,6 +1,7 @@
 module Verify
   class ServiceProvider
     GENERATE_REQUEST_URL = "http://localhost:50300/generate-request".freeze
+    TRANSLATE_RESPONSE_URL = "http://localhost:50300/translate-response".freeze
 
     # Makes a request to the Verify Service Provider to generate an
     # authentication request, as described here:
@@ -21,6 +22,33 @@ module Verify
     def generate_request
       uri = URI(GENERATE_REQUEST_URL)
       request = Net::HTTP::Post.new(uri, {})
+      request.content_type = "application/json"
+      response = Net::HTTP.start(uri.hostname, uri.port) { |http| http.request(request) }
+
+      JSON.parse(response.body)
+    end
+
+    # Makes a request to the Verify Service Provider to translate a SAML
+    # response returned by Verify, as described here:
+    #
+    #   https://www.docs.verify.service.gov.uk/get-started/set-up-successful-verification-journey/#request-to-translate-the-saml-response
+    #
+    # The SAML response is the payload representing the result of the Verify
+    # identity assurance attempt by the user.
+    #
+    # Returns the translated response as a Hash, for example:
+    #
+    #   {
+    #       "scenario" => "IDENTITY_VERIFIED",
+    #       "pid" => "etikgj3ewowe",
+    #       "levelOfAssurance" => "LEVEL_2",
+    #       "attributes" => {...}
+    #   }
+    #
+    def translate_response(saml_response, request_id, level_of_assurance)
+      uri = URI(TRANSLATE_RESPONSE_URL)
+      request = Net::HTTP::Post.new(uri)
+      request.body = {"samlResponse" => saml_response, "requestId" => request_id, "levelOfAssurance" => level_of_assurance}.to_json
       request.content_type = "application/json"
       response = Net::HTTP.start(uri.hostname, uri.port) { |http| http.request(request) }
 
--- spec/lib/verify/service_provider_spec.rb
+++ spec/lib/verify/service_provider_spec.rb
@@ -13,4 +13,20 @@ RSpec.describe Verify::ServiceProvider do
       expect(response["samlRequest"]).to eql(stubbed_auth_request_response["samlRequest"])
     end
   end
+
+  describe "#translate_response" do
+    let(:saml_response) { Verify::FakeSso::IDENTITY_VERIFIED_SAML_RESPONSE }
+    let(:request_id) { "THE_REQUEST_ID" }
+    let(:level_of_assurance) { "LEVEL_1" }
+
+    let!(:stubbed_translate_request) { stub_vsp_translate_response_request(translate_request_payload) }
+    let(:translate_request_payload) { {"samlResponse" => saml_response, "requestId" => request_id, "levelOfAssurance" => level_of_assurance} }
+
+    it "makes a POST request to the VSP to translate the SAML response and returns the result as a Hash" do
+      response = Verify::ServiceProvider.new.translate_response(saml_response, request_id, level_of_assurance)
+
+      expect(stubbed_translate_request).to have_been_requested
+      expect(response).to eql(stubbed_vsp_translated_response)
+    end
+  end
 end
