8787f98785f633879383f7f03d12e6c6528f536d
Stuart Harrison <pezholio@gmail.com>
2019-07-02 14:52:02 +0100

    Use a model class to transform parameters

--- app/controllers/verify/authentications_controller.rb
+++ app/controllers/verify/authentications_controller.rb
@@ -18,29 +18,9 @@ module Verify
     #   https://www.docs.verify.service.gov.uk/get-started/set-up-successful-verification-journey/#handle-a-response
     def create
       verify_authentication_response = Verify::ServiceProvider.new.translate_response(params["SAMLResponse"], session[:verify_request_id], "LEVEL_2")
-      current_claim.update!(claim_parameters_from_response(verify_authentication_response))
+      response = VerifyResponse.new(verify_authentication_response)
+      current_claim.update!(response.claim_parameters)
       redirect_to claim_path("teacher-reference-number")
-    end
-
-    private
-
-    def claim_parameters_from_response(response)
-      {
-        full_name: full_name_from_response(response),
-        address_line_1: response.fetch("attributes").fetch("addresses").first.fetch("value").fetch("lines")[0],
-        address_line_2: response.fetch("attributes").fetch("addresses").first.fetch("value").fetch("lines")[1],
-        address_line_3: response.fetch("attributes").fetch("addresses").first.fetch("value").fetch("lines")[2],
-        postcode: response.fetch("attributes").fetch("addresses").first.fetch("value").fetch("postCode"),
-        date_of_birth: response.fetch("attributes").fetch("datesOfBirth").first.fetch("value"),
-      }
-    end
-
-    def full_name_from_response(response)
-      first_name = response.fetch("attributes").fetch("firstNames").first.fetch("value")
-      middle_name = response.fetch("attributes").fetch("middleNames").first.fetch("value")
-      surname = response.fetch("attributes").fetch("surnames").first.fetch("value")
-
-      [first_name, middle_name, surname].join(" ")
     end
   end
 end
