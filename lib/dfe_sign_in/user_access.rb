a34ff566c118fccbcf26fb17ce223cc1ce248c45
Stuart Harrison <pezholio@gmail.com>
2019-07-23 15:43:57 +0100

    Check user's permissions in UserAccess class
    This gets all the roles for a user and checks if a given role is included in the array of roles.

--- lib/dfe_sign_in/user_access.rb
+++ lib/dfe_sign_in/user_access.rb
@@ -3,33 +3,38 @@ module DfeSignIn
     include DfeSignIn::Utils
 
     attr_accessor :organisation_id,
-                  :user_id,
-                  :body
+                  :user_id
 
     def initialize(organisation_id:, user_id:)
       self.organisation_id = organisation_id
       self.user_id = user_id
     end
 
-    def call
-      request = Net::HTTP::Get.new(uri)
-      request["Authorization"] = "bearer #{generate_jwt_token}"
-      request["Content-Type"] = "application/json"
-
-      response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http|
-        http.request(request)
-      }
-
-      raise ExternalServerError if response.code.eql?("500")
-
-      if response.code.eql?("200")
-        self.body = JSON.parse(response.body)
-      end
-
-      self
+    def has_role?(role)
+      roles.include?(role)
     end
 
     private
+
+    def body
+      @body ||= begin
+        request = Net::HTTP::Get.new(uri)
+        request["Authorization"] = "bearer #{generate_jwt_token}"
+        request["Content-Type"] = "application/json"
+
+        response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http|
+          http.request(request)
+        }
+
+        raise ExternalServerError if response.code.eql?("500")
+
+        JSON.parse(response.body) if response.code.eql?("200")
+      end
+    end
+
+    def roles
+      body["roles"].map { |r| r["code"] }
+    end
 
     def uri
       @uri ||= begin
--- spec/lib/dfe_sign_in/user_access_spec.rb
+++ spec/lib/dfe_sign_in/user_access_spec.rb
@@ -3,52 +3,62 @@ require "rails_helper"
 RSpec.describe DfeSignIn::UserAccess do
   subject { described_class.new(user_id: 999, organisation_id: 456) }
 
-  describe "call" do
-    let(:url) { "#{ENV["DFE_SIGN_IN_API_ENDPOINT"]}/services/#{ENV["DFE_SIGN_IN_API_CLIENT_ID"]}/organisations/456/users/999" }
+  let(:url) { "#{DfeSignIn.configuration.base_url}/services/#{DfeSignIn.configuration.client_id}/organisations/456/users/999" }
 
-    before do
-      stub_request(:get, url)
-        .to_return(body: response.to_json, status: status)
+  before do
+    stub_request(:get, url)
+      .to_return(body: response.to_json, status: status)
+  end
+
+  context "with a valid response" do
+    let(:status) { 200 }
+    let(:response) do
+      {
+        "userId" => "999",
+        "serviceId" => "123",
+        "organisationId" => "456",
+        "roles" => [
+          {
+            "id" => "role-id",
+            "name" => "My role",
+            "code" => "my_role",
+            "numericId" => "9999",
+            "status" => {
+              "id" => 1,
+            },
+          },
+        ],
+        "identifiers" => [
+          {
+            "key" => "identifier-key",
+            "value" => "identifier-value",
+          },
+        ],
+      }
     end
 
-    context "with a valid response" do
-      let(:status) { 200 }
-      let(:response) do
-        {
-          "userId" => "999",
-          "serviceId" => "123",
-          "organisationId" => "456",
-          "roles" => [
-            {
-              "id" => "role-id",
-              "name" => "The name of the role",
-              "code" => "The code of the role",
-              "numericId" => "9999",
-              "status" => {
-                "id" => 1,
-              },
-            },
-          ],
-          "identifiers" => [
-            {
-              "key" => "identifier-key",
-              "value" => "identifier-value",
-            },
-          ],
-        }
+    describe "has_role?" do
+      let(:has_role?) { subject.has_role?(role) }
+
+      context "when a role exists" do
+        let(:role) { "my_role" }
+        it { expect(has_role?).to eq(true) }
       end
 
-      it "returns the expected data" do
-        expect(subject.call.body).to eq(response)
+      context "when a role does not exist" do
+        let(:role) { "other_role" }
+        it { expect(has_role?).to eq(false) }
       end
     end
+  end
 
-    context "with an invalid response" do
-      let(:status) { 500 }
-      let(:response) { {} }
+  context "with an invalid response" do
+    let(:status) { 500 }
+    let(:response) { {} }
 
+    describe "has_role?" do
       it "raises an error" do
-        expect { subject.call }.to raise_error(DfeSignIn::ExternalServerError)
+        expect { subject.has_role?("my_role") }.to raise_error(DfeSignIn::ExternalServerError)
       end
     end
   end
