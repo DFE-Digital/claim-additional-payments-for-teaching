72acbb72b8f740bc3a36d46552451604fcadfdf6
Stuart Harrison <pezholio@gmail.com>
2019-07-23 15:17:12 +0100

    Use ENV.fetch to get DfE Signin related vars
    This way, we'll get an error if they're not set
    
    # Conflicts:
    #	app/controllers/admin/auth_controller.rb

--- config/initializers/dfe_sign_in.rb
+++ config/initializers/dfe_sign_in.rb
@@ -1,5 +1,5 @@
 DfeSignIn.configure do |config|
-  config.client_id = ENV["DFE_SIGN_IN_API_CLIENT_ID"]
-  config.secret = ENV["DFE_SIGN_IN_API_SECRET"]
-  config.base_url = ENV["DFE_SIGN_IN_API_ENDPOINT"]
+  config.client_id = ENV.fetch("DFE_SIGN_IN_API_CLIENT_ID")
+  config.secret = ENV.fetch("DFE_SIGN_IN_API_SECRET")
+  config.base_url = ENV.fetch("DFE_SIGN_IN_API_ENDPOINT")
 end
