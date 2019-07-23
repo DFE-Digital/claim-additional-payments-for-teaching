EwanNoble/dfe-teachers-payment-service

Begin the student loan plan micro journey
So we can calculate the tax a user will pay on their student loan repayment
we need to know which student loan 'plan' they are on.

This is not information a user will be able to find out simply so
instead we ask a series of questions that determine the the correct plan or
plans a user is on.

This commit introduces the first question in that series.

db/migrate/20190709100000_add_student_loan_to_tslr_claim.rb
@@ -1,0 +1,5 @@
+class AddStudentLoanToTslrClaim < ActiveRecord::Migration[5.2]
+  def change
+    add_column :tslr_claims, :student_loan, :boolean
+  end
+end

DFE-Digital/claim-additional-payments-for-teaching

db/migrate/20190709100000_add_student_loan_to_tslr_claim.rb
@@ -1,0 +1,5 @@
+class AddStudentLoanToTslrClaim < ActiveRecord::Migration[5.2]
+  def change
+    add_column :tslr_claims, :has_student_loan, :boolean
+  end
+end