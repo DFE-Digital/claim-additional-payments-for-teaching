1f27aa75bf1053eff38b4ab826a8d11391bdabe7
Stuart Harrison <pezholio@gmail.com>
2019-08-01 15:25:13 +0100

    Skip gender question if it's already been verified

--- spec/models/page_sequence_spec.rb
+++ spec/models/page_sequence_spec.rb
@@ -44,6 +44,15 @@ RSpec.describe PageSequence do
         expect(page_sequence.slugs).to include("student-loan-start-date")
       end
     end
+
+    it "excludes the gender page if a response has been returned from Verify" do
+      claim.verified_fields = ["gender"]
+      page_sequence = PageSequence.new(claim, "student-loan-country")
+      expect(page_sequence.slugs).to_not include("gender")
+
+      claim.verified_fields = []
+      expect(page_sequence.slugs).to include("gender")
+    end
   end
 
   describe "#next_slug" do
