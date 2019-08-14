6f2c8fa6303fdc1b19a69903b6d127450d035b0a
Robbie Paul <robbie.paul@dxw.com>
2019-08-14 17:59:00 +0100

    Filter personally identifiable information from logs

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -2,3 +2,19 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += [:password]
+
+# Personally identifiable information
+Rails.application.config.filter_parameters += [
+  :address_line_1,
+  :address_line_2,
+  :address_line_3,
+  :address_line_4,
+  :postcode,
+  :payroll_gender,
+  :teacher_reference_number,
+  :national_insurance_number,
+  :email_address,
+  :bank_sort_code,
+  :bank_account_number,
+  :SAMLResponse,
+]
