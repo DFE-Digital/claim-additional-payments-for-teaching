7b8a9ff9d2f7ab6ae654408b0c05fca617a189be
Stuart Harrison <pezholio@gmail.com>
2019-08-20 12:50:39 +0100

    Add method to check if a user is a support user

--- app/models/admin_session.rb
+++ app/models/admin_session.rb
@@ -2,8 +2,13 @@
 
 class AdminSession < DfeSignIn::AuthenticatedSession
   SERVICE_OPERATOR_DFE_SIGN_IN_ROLE_CODE = "teacher_payments_access"
+  SUPPORT_AGENT_DFE_SIGN_IN_ROLE_CODE = "teacher_payments_support"
 
   def is_service_operator?
     role_codes.include?(SERVICE_OPERATOR_DFE_SIGN_IN_ROLE_CODE)
+  end
+
+  def is_support_agent?
+    role_codes.include?(SUPPORT_AGENT_DFE_SIGN_IN_ROLE_CODE)
   end
 end
--- spec/models/admin_session_spec.rb
+++ spec/models/admin_session_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe AdminSession, type: :model do
       expect(admin_session.is_service_operator?).to eq false
     end
   end
+
+  describe "#is_support_agent?" do
+    it "returns true when the session has the right role" do
+      admin_session = AdminSession.new("user-id", "organisation-id", [AdminSession::SUPPORT_AGENT_DFE_SIGN_IN_ROLE_CODE])
+      expect(admin_session.is_support_agent?).to eq true
+
+      admin_session = AdminSession.new("user-id", "organisation-id", ["other-role"])
+      expect(admin_session.is_support_agent?).to eq false
+    end
+  end
 end
