5ff7e8fc419a92da831ecabb4be89f9c2658b24a
Robbie Paul <robbie.paul@dxw.com>
2019-08-20 13:48:15 +0100

    Cookie banner code now uses cookie functions
    Also took this opportunity to set the visibility of the banner through
    classes as opposed to inline style.

--- app/assets/javascripts/components/cookie_banner.js
+++ app/assets/javascripts/components/cookie_banner.js
@@ -1,28 +1,15 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function() {
-  var cookiePolicyName = "accept_cookie_policy";
-  var acceptCookies = getCookie(cookiePolicyName);
   var cookieBanner = document.querySelector("#global-cookie-message");
   var acceptButton = document.querySelector("#accept-cookies");
 
-  if (!acceptCookies) {
-    cookieBanner.style.display = "block";
+  if (!TeacherPayments.cookies.checkNonEssentialCookiesAccepted()) {
+    cookieBanner.classList.add("govuk-cookie-banner--visible");
   }
 
   acceptButton.onclick = function() {
-    setCookie(cookiePolicyName, 1, 365);
-    cookieBanner.style.display = "none";
+    TeacherPayments.cookies.acceptNonEssentialCookies();
+    cookieBanner.classList.remove("govuk-cookie-banner--visible");
   };
-
-  function getCookie(name) {
-    var v = document.cookie.match("(^|;) ?" + name + "=([^;]*)(;|$)");
-    return v ? v[2] : null;
-  }
-
-  function setCookie(name, value, days) {
-    var date = new Date();
-    date.setTime(date.getTime() + 24 * 60 * 60 * 1000 * days);
-    document.cookie = name + "=" + value + ";path=/;expires=" + date.toGMTString();
-  }
 });
