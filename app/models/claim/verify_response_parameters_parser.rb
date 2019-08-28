29eff8c620003c2b4a79cc685948cd90ea01262a
Tekin Suleyman <tekin@tekin.co.uk>
2019-08-28 16:15:18 +0100

    VerifyResponseParametersParser parses names
    This parses name fields, mimicking the behaviour of Verify::Response.
    The individual parts of the name are broken out to make it easier to
    switch to capturing the name as separate attributes in the future.
    
    This also fixes a bug where a value that didn't have a "from" date would
    cause the response parser to fall over.

--- app/models/claim/verify_response_parameters_parser.rb
+++ app/models/claim/verify_response_parameters_parser.rb
@@ -1,5 +1,7 @@
 class Claim
   class VerifyResponseParametersParser
+    class MissingResponseAttribute < StandardError; end
+
     def initialize(response_parameters)
       @response_parameters = response_parameters
     end
@@ -12,10 +14,38 @@ class Claim
       attributes.fetch("datesOfBirth").first.fetch("value")
     end
 
+    def first_name
+      most_recent_verified("firstNames")
+    end
+
+    def surname
+      most_recent_verified("surnames")
+    end
+
+    def middle_name
+      most_recent_verified("middleNames", required: false)
+    end
+
+    def full_name
+      [first_name, middle_name, surname].compact.join(" ")
+    end
+
     private
 
     def attributes
       @response_parameters.fetch("attributes")
+    end
+
+    def most_recent_verified(attribute_name, required: true)
+      raise MissingResponseAttribute, "No verified value found for #{attribute_name}" if required && no_verified_values?(attribute_name)
+
+      attributes.fetch(attribute_name)
+        .select { |value| value["verified"] }
+        .max_by { |value| Date.parse(value.fetch("from", Date.today.to_s)) }&.fetch("value")
+    end
+
+    def no_verified_values?(attribute_name)
+      attributes.fetch(attribute_name).none? { |value| value["verified"] }
     end
   end
 end
--- spec/models/claim/verify_response_parameters_parser_spec.rb
+++ spec/models/claim/verify_response_parameters_parser_spec.rb
@@ -35,6 +35,125 @@ RSpec.describe Claim::VerifyResponseParametersParser do
     end
   end
 
+  describe "#first_name" do
+    it "returns a 'verified' value for 'firstNames'" do
+      parser = Claim::VerifyResponseParametersParser.new(sample_response_parameters(firstNames: [{value: "Bob", verified: true}]))
+      expect(parser.first_name).to eq "Bob"
+    end
+
+    it "returns the most recent 'verified' value for 'firstNames'" do
+      changed_name_parameters = sample_response_parameters({
+        firstNames: [
+          {value: "Barbara", verified: true, from: "1991-12-12", to: "2018-08-08"},
+          {value: "Bob", verified: true, from: "2018-08-09"},
+        ],
+      })
+      parser = Claim::VerifyResponseParametersParser.new(changed_name_parameters)
+
+      expect(parser.first_name).to eq "Bob"
+    end
+
+    it "returns the 'verified' value without a from/to date (this will be the most recent)" do
+      changed_name_parameters = sample_response_parameters({
+        firstNames: [
+          {value: "Bob", verified: true},
+          {value: "Barbara", verified: true, from: "1991-12-12", to: "2018-08-08"},
+        ],
+      })
+      parser = Claim::VerifyResponseParametersParser.new(changed_name_parameters)
+
+      expect(parser.first_name).to eq "Bob"
+    end
+
+    it "raises a MissingResponseAttribute error if there is no 'verified' value for 'firstNames'" do
+      unverified_name_paramters = sample_response_parameters({
+        firstNames: [value: "Fred", verified: false],
+      })
+
+      expect { Claim::VerifyResponseParametersParser.new(unverified_name_paramters).first_name }.to raise_exception(
+        Claim::VerifyResponseParametersParser::MissingResponseAttribute, "No verified value found for firstNames"
+      )
+    end
+  end
+
+  describe "#surname" do
+    it "returns a 'verified' value for 'surnames'" do
+      parser = Claim::VerifyResponseParametersParser.new(sample_response_parameters(surnames: [{value: "Booker", verified: true}]))
+      expect(parser.surname).to eq "Booker"
+    end
+
+    it "returns the most recent 'verified' value for 'surnames'" do
+      changed_name_parameters = sample_response_parameters({
+        surnames: [
+          {value: "Franklin", verified: true, from: "1991-12-12", to: "2018-08-08"},
+          {value: "Booker", verified: true, from: "2018-08-09"},
+        ],
+      })
+      parser = Claim::VerifyResponseParametersParser.new(changed_name_parameters)
+
+      expect(parser.surname).to eq "Booker"
+    end
+
+    it "returns the 'verified' value without a from/to date (assuming this will be the most recent)" do
+      changed_name_parameters = sample_response_parameters({
+        surnames: [
+          {value: "Brooker", verified: false},
+          {value: "Booker", verified: true},
+          {value: "Franklin", verified: true, from: "1991-12-12", to: "2018-08-08"},
+        ],
+      })
+      parser = Claim::VerifyResponseParametersParser.new(changed_name_parameters)
+
+      expect(parser.surname).to eq "Booker"
+    end
+
+    it "raises a MissingResponseAttribute error if there is no 'verified' value for 'surnames'" do
+      unverified_name_paramters = sample_response_parameters({
+        surnames: [value: "Fred", verified: false],
+      })
+
+      expect { Claim::VerifyResponseParametersParser.new(unverified_name_paramters).surname }.to raise_exception(
+        Claim::VerifyResponseParametersParser::MissingResponseAttribute, "No verified value found for surnames"
+      )
+    end
+  end
+
+  describe "#middle_name" do
+    it "returns the most recent 'verified' value for 'middleNames'" do
+      changed_name_parameters = sample_response_parameters({
+        "middleNames": [
+          {value: "Horacio", verified: true, from: "1991-12-12", to: "2018-08-08"},
+          {value: "Buster", verified: true, from: "2018-08-09"},
+        ],
+      })
+      parser = Claim::VerifyResponseParametersParser.new(changed_name_parameters)
+
+      expect(parser.middle_name).to eq "Buster"
+    end
+
+    it "returns nil if there are no 'middleNames'" do
+      parser = Claim::VerifyResponseParametersParser.new(sample_response_parameters(middleNames: []))
+      expect(parser.middle_name).to be_nil
+    end
+
+    it "returns nil if there is no 'verified' value for 'middleNames'" do
+      parser = Claim::VerifyResponseParametersParser.new(sample_response_parameters(middleNames: [{value: "Horacio", verified: false}]))
+      expect(parser.middle_name).to be_nil
+    end
+  end
+
+  describe "#full_name" do
+    it "joins the first name and surname together" do
+      parser = Claim::VerifyResponseParametersParser.new(sample_response_parameters)
+      expect(parser.full_name).to eq "Isambard Brunel"
+    end
+
+    it "includes a middle name when present" do
+      parser = Claim::VerifyResponseParametersParser.new(sample_response_parameters(middleNames: [{value: "Kingdom", verified: true}]))
+      expect(parser.full_name).to eq "Isambard Kingdom Brunel"
+    end
+  end
+
   private
 
   def sample_response_parameters(overrides = {})
