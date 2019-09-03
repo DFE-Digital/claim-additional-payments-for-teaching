9fdf17b2df29439891ac3a22dd76c94771a71303
Meyric Rawlings <meyric@dxw.com>
2019-09-03 16:45:32 +0100

    Approved email can be sent
    Once a users claim is approved will need to be notify them via email.
    
    There is additional work to actually approve a claim happening
    elsewhere this commit adds the email and method to send it.

--- spec/mailers/claim_mailer_spec.rb
+++ spec/mailers/claim_mailer_spec.rb
@@ -16,4 +16,20 @@ RSpec.describe ClaimMailer, type: :mailer do
       expect(mail.body.encoded).to match("Your unique reference is #{claim.reference}. You will need this if you contact us about your claim.")
     end
   end
+
+  describe "#approved" do
+    let(:claim) { create(:claim, :submitted, first_name: "John", middle_name: "Fitzgerald", surname: "Kennedy") }
+    let(:mail) { ClaimMailer.approved(claim) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Your claim to get your student loan repayments back has been approved, reference number: #{claim.reference}")
+      expect(mail.to).to eq([claim.email_address])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match("Dear John Kennedy,")
+      expect(mail.body.encoded).to match("Your claim to get your student loan repayments back has been approved.")
+      expect(mail.body.encoded).to match("Email studentloanteacherpayment@digital.education.gov.uk giving your reference number: #{claim.reference} if you have any questions.")
+    end
+  end
 end
