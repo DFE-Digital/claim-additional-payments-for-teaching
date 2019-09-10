51cda8e72b8319b877fc5ed7913b31c43e590928
Stuart Harrison <pezholio@gmail.com>
2019-09-10 10:44:51 +0100

    Add link to approve claims

--- spec/features/admin_claim_approve_spec.rb
+++ spec/features/admin_claim_approve_spec.rb
@@ -10,10 +10,10 @@ RSpec.feature "Admin approves a claim" do
 
     scenario "User can approve a claim" do
       freeze_time do
-        submitted_claims = create_list(:claim, 5, :submitted, submitted_at: DateTime.now, approved_at: nil)
+        submitted_claims = create_list(:claim, 5, :submitted)
         claim_to_approve = submitted_claims.first
 
-        visit admin_claims_path
+        click_on "Approve claims"
 
         expect(page).to have_content(claim_to_approve.reference)
 
@@ -24,6 +24,23 @@ RSpec.feature "Admin approves a claim" do
         expect(page).to have_content("Claim has been approved successfully")
         expect(page).to_not have_content(claim_to_approve.reference)
       end
+    end
+  end
+
+  context "User is logged in as a support user" do
+    before do
+      stub_dfe_sign_in_with_role(AdminSession::SUPPORT_AGENT_DFE_SIGN_IN_ROLE_CODE)
+      visit admin_path
+      click_on "Sign in"
+    end
+
+    scenario "User cannot view claims to approve" do
+      expect(page).to_not have_link(nil, href: admin_claims_path)
+
+      visit admin_claims_path
+
+      expect(page.status_code).to eq(401)
+      expect(page).to have_content("Not authorised")
     end
   end
 end
