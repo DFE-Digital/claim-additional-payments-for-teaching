74a5a42613861b2dc1490122c789b07b68e328a2
Tekin Suleyman <tekin@tekin.co.uk>
2019-10-15 09:27:51 +0100

    Simplify spec by working with strings not arrays
    Using Payroll::ClaimCsvRow to generate a string, and then pass that back
    into CSV.parse_line to generate an array to compare against an array
    that was also parsed from a string felt like going around the houses a
    little. Hopefullt this makes the spec easier to follow.

--- spec/models/payroll/claims_csv_spec.rb
+++ spec/models/payroll/claims_csv_spec.rb
@@ -11,51 +11,53 @@ RSpec.describe Payroll::ClaimsCsv do
 
   describe "#file" do
     let(:file) { subject.file }
-    let(:csv) { CSV.read(file) }
+    let(:file_lines) { file.read.split("\n") }
 
     it "returns a Tempfile" do
       expect(file).to be_a(Tempfile)
     end
 
     it "contains a headers row for the payroll file" do
-      expect(csv[0]).to match_array([
-        "TITLE",
-        "FORENAME",
-        "FORENAME2",
-        "SURNAME",
-        "SS_NO",
-        "GENDER",
-        "START_DATE",
-        "END_DATE",
-        "BIRTH_DATE",
-        "EMAIL",
-        "ADDR_LINE_1",
-        "ADDR_LINE_2",
-        "ADDR_LINE_3",
-        "ADDR_LINE_4",
-        "ADDR_LINE_5",
-        "ADDR_LINE_6",
-        "ADDRESS_COUNTRY",
-        "TAX_CODE",
-        "TAX_BASIS",
-        "NEW_EMP_Q_VL",
-        "NI_CATEGORY",
-        "CON_STU_LOAN_I",
-        "PLAN_TYPE",
-        "BANK_NAME",
-        "SORT_CODE",
-        "ACCOUNT_NUMBER",
-        "ROLL_NUMBER",
-        "SCHEME_NAME",
-        "SCHEME_AMOUNT",
-        "CLAIM_ID",
-      ])
+      expected_header_row = %w[
+        TITLE
+        FORENAME
+        FORENAME2
+        SURNAME
+        SS_NO
+        GENDER
+        START_DATE
+        END_DATE
+        BIRTH_DATE
+        EMAIL
+        ADDR_LINE_1
+        ADDR_LINE_2
+        ADDR_LINE_3
+        ADDR_LINE_4
+        ADDR_LINE_5
+        ADDR_LINE_6
+        ADDRESS_COUNTRY
+        TAX_CODE
+        TAX_BASIS
+        NEW_EMP_Q_VL
+        NI_CATEGORY
+        CON_STU_LOAN_I
+        PLAN_TYPE
+        BANK_NAME
+        SORT_CODE
+        ACCOUNT_NUMBER
+        ROLL_NUMBER
+        SCHEME_NAME
+        SCHEME_AMOUNT
+        CLAIM_ID
+      ].join(",")
+
+      expect(file_lines[0]).to eq(expected_header_row)
     end
 
     it "contains data rows for passed in claims" do
-      expected_claim_data_row = CSV.parse_line(Payroll::ClaimCsvRow.new(claim).to_s)
+      expected_claim_data_row = Payroll::ClaimCsvRow.new(claim).to_s.chomp
 
-      expect(csv[1]).to eq(expected_claim_data_row)
+      expect(file_lines[1]).to eq(expected_claim_data_row)
     end
   end
 end
