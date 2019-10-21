56a1b077c7a024c0650a8d5b6ff91543f753e490
Robbie Paul <robbie.paul@dxw.com>
2019-10-21 14:27:01 +0100

    Combine admin approve and reject feature specs
    When deciding where to add a new feature spec for making assertions
    against an already checked claim it didn't make sense to put it either
    the approved or rejected specs. Theses two specs are identical in their
    set up so it makes sense to combine them.

--- spec/features/admin_claim_check_spec.rb
+++ spec/features/admin_claim_check_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.feature "Admin approves a claim" do
+RSpec.feature "Admin checks a claim" do
   let(:user_id) { "userid-345" }
 
   context "User is logged in as a service operator" do
@@ -37,6 +37,33 @@ RSpec.feature "Admin approves a claim" do
         expect(mail.to).to eq([claim_to_approve.email_address])
         expect(mail.body.raw_source).to match("been approved")
       end
+    end
+
+    scenario "they can reject a claim" do
+      submitted_claims = create_list(:claim, 5, :submitted)
+      claim_to_reject = submitted_claims.first
+
+      click_on "View claims"
+
+      expect(page).to have_content(claim_to_reject.reference)
+      expect(page).to have_content("5 claims awaiting checking")
+
+      find("a[href='#{admin_claim_path(claim_to_reject)}']").click
+      choose "Reject"
+      fill_in "Decision notes", with: "TRN doesn't exist"
+      perform_enqueued_jobs { click_on "Submit" }
+
+      expect(claim_to_reject.check.checked_by).to eq(user_id)
+      expect(claim_to_reject.check.notes).to eq("TRN doesn't exist")
+
+      expect(page).to have_content("Claim has been rejected successfully")
+      expect(page).to_not have_content(claim_to_reject.reference)
+
+      mail = ActionMailer::Base.deliveries.last
+
+      expect(mail.subject).to match("been rejected")
+      expect(mail.to).to eq([claim_to_reject.email_address])
+      expect(mail.body.raw_source).to match("been rejected.")
     end
 
     context "When the payroll gender is missing" do
