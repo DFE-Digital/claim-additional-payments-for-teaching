5f8a7ecdaa98cf73f3483267a4c9e24d92432d1b
Robbie Paul <robbie.paul@dxw.com>
2019-10-21 16:58:07 +0100

    Move missing payroll gender check to a before_action

--- app/controllers/admin/claim_checks_controller.rb
+++ app/controllers/admin/claim_checks_controller.rb
@@ -2,9 +2,9 @@ class Admin::ClaimChecksController < Admin::BaseAdminController
   before_action :ensure_service_operator
   before_action :load_claim
   before_action :reject_checked_claims
+  before_action :reject_missing_payroll_gender
 
   def create
-    return redirect_to admin_claim_path(@claim), alert: "Claim cannot be approved" if check_params[:result] == "approved" && @claim.payroll_gender_missing?
     @check = @claim.build_check(check_params.merge(checked_by: admin_session.user_id))
     if @check.save
       send_claim_result_email
@@ -23,6 +23,12 @@ class Admin::ClaimChecksController < Admin::BaseAdminController
   def reject_checked_claims
     if @claim.check.present?
       redirect_to admin_claim_path(@claim), notice: "Claim already checked"
+    end
+  end
+
+  def reject_missing_payroll_gender
+    if check_params[:result] == "approved" && @claim.payroll_gender_missing?
+      redirect_to admin_claim_path(@claim), alert: "Claim cannot be approved"
     end
   end
 
