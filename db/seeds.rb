c51f4391e1fee83d600cf3fd816887c0bdf8b71e
Lawrence Forooghian <lawrence@dxw.com>
2019-10-24 11:18:16 +0100

    Add payroll runs and claims to development seeds
    All the data generated by the factories is constant (e.g. claimants'
    names) so it's not the most realistic seed data in the world, but it
    gives a useful set of data out of the box that allows a developer to
    test / demo all of the admin functionality.
    
    We might want to consider using a gem like Faker to enable the factories
    to generate random realistic data (names, addresses, emails, ...)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-ENV["FIXTURES_PATH"] = "spec/fixtures"
-Rake::Task["db:fixtures:load"].invoke
+if Rails.env.development?
+  ENV["FIXTURES_PATH"] = "spec/fixtures"
+  Rake::Task["db:fixtures:load"].invoke
+
+  class Seeds
+    extend FactoryBot::Syntax::Methods
+
+    create(:payroll_run, :confirmation_report_uploaded, claims_count: 32, created_at: 3.months.ago - 10.days)
+    create(:payroll_run, :confirmation_report_uploaded, claims_count: 19, created_at: 2.months.ago - 5.days)
+    create(:payroll_run, claims_count: 26, created_at: 1.months.ago - 3.days)
+
+    create_list(:claim, 23, :approved)
+    create_list(:claim, 10, :submitted)
+  end
+end
