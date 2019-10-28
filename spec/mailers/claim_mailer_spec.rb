b6bc7db36c14e85ac4be61c00acca6daf6bf4bfc
Stuart Harrison <pezholio@gmail.com>
2019-10-28 13:42:43 +0000

    Create mailer action to send payment notification
    Creating a new mailer action to send a payment confirmation email to a user.
    The payment date needs to be sent as a Unix timestamp (old skool huh?),
    because `ActiveJob` can't serialize Date objects properly. I could have
    calculated the payment date in the mailer, but I don't like to have too much
    logic in mailers, and as the mailers run asynchronously, we could end up with
    unpredictable results if the mailers get stuck in a queue for some reason.

--- spec/mailers/claim_mailer_spec.rb
+++ spec/mailers/claim_mailer_spec.rb
@@ -48,4 +48,31 @@ RSpec.describe ClaimMailer, type: :mailer do
       expect(mail.body.encoded).to match("been rejected")
     end
   end
+
+  describe "#payment_confirmation" do
+    let(:payment) { create(:payment, :with_figures, net_pay: 500.00, claim: claim) }
+    let(:claim) { create(:claim, :submitted, first_name: "John", middle_name: "Fitzgerald", surname: "Kennedy") }
+    let(:payment_date_timestamp) { Time.new(2019, 1, 1).to_i }
+    let(:mail) { ClaimMailer.payment_confirmation(payment.claim, payment_date_timestamp) }
+
+    it "renders the headers" do
+      expect(mail.subject).to match("paying")
+      expect(mail.subject).to match("reference number: #{claim.reference}")
+      expect(mail.to).to eq([claim.email_address])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to include("Dear John Kennedy,")
+      expect(mail.body.encoded).to include("We’re paying your claim")
+      expect(mail.body.encoded).to include("You will receive £500.00 on or after 1 January 2019")
+    end
+
+    context "when student loan repayment is nil" do
+      let(:payment) { create(:payment, :with_figures, student_loan_repayment: nil, claim: claim) }
+
+      it "shows not applicable" do
+        expect(mail.body.encoded).to include("Student loan (deducted): Not applicable")
+      end
+    end
+  end
 end
