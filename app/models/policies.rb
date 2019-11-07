8c69e71e2f26209572023137fdb84f30063b8492
Tekin Suleyman <tekin@tekin.co.uk>
2019-11-07 13:27:56 +0000

    Make PartOfClaimJourney policy-aware
    To now PartOfClaimJourney was coupled to the StudentLoans policy. This
    updates it so that it will correctly initialise new claims and redirect
    unstarted claimants according to the actual policy defined by the
    routing parameters. Note that current_claim needs to be able to behave
    even when there is no `params[:policy]` because
    Verify::AuthenticationsController is not namespaced by policy.

--- app/models/policies.rb
+++ app/models/policies.rb
@@ -7,4 +7,13 @@ module Policies
   def self.all
     POLICIES
   end
+
+  # Returns a policy that matches the provided routing name
+  # For example:
+  #
+  #   Policies["student-loans"] #=> StudentLoans
+  #
+  def self.[](routing_name)
+    POLICIES.find { |policy| policy.routing_name == routing_name }
+  end
 end
