c34e85af995fa608c925544b75179c9e33218f70
Meyric Rawlings <meyric@dxw.com>
2019-11-11 14:40:20 +0000

    A service operator can create a DQT report request
    This uses the new Claim::DatabaseOfQualifiedTeachersReportRequest class to
    export the claims used to create a report from the Database of qualified
    teachers as a csv.
    
    When asked, service operators felt the 'view claims' screen was the right place
    to find this feature.
    
    We do not expect this feature to remain in the service once we have a connection
    to the Database of qualified teachers.

---
+++ spec/features/admin_dqt_report_request_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.feature "DQT Report request" do
+  scenario "Service operator can download CSV for the Database of Qualified Teachers report request" do
+    sign_in_to_admin_with_role(AdminSession::SERVICE_OPERATOR_DFE_SIGN_IN_ROLE_CODE)
+
+    claims = create_list(:claim, 3, :submitted)
+
+    click_on "View claims"
+
+    click_on "Download DQT report request file"
+
+    expect(page.response_headers["Content-Type"]).to eq("text/csv")
+
+    csv = CSV.parse(body, headers: true)
+
+    expect(csv.count).to eq(3)
+    expect(csv[2].fields("Claim reference")).to include(claims.last.reference)
+  end
+end
