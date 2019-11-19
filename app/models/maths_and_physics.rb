f239407dba24f6529e0469cb8c4914f9823c90c1
Tekin Suleyman <tekin@tekin.co.uk>
2019-11-19 16:46:53 +0000

    Parameterise eligiblity URL in rejection email

--- app/models/maths_and_physics.rb
+++ app/models/maths_and_physics.rb
@@ -3,6 +3,10 @@ module MathsAndPhysics
     "/maths-and-physics/start" # Temporary start page during private beta
   end
 
+  def self.eligibility_page_url
+    "https://www.gov.uk/government/publications/additional-payments-for-teaching-eligibility-and-payment-details/claim-a-payment-for-teaching-maths-or-physics-eligibility-and-payment-details"
+  end
+
   def self.routing_name
     "maths-and-physics"
   end
