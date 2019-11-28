4aa35fe5fc673635f99446620eb23dea8d515e03
Meyric Rawlings <meyric@dxw.com>
2019-11-28 13:23:05 +0000

    Copy the payroll run download url with javascript
    A text field input with the 'data-copy-to-clipboard' set to 'true' will get a
    'Copy to clipboard' button added, that when clicked, copies the content of the
    field to the clipboard.
    
    We used the older and simpler method of select() and execCommand('copy') as it
    has the widest browser support and this is just a temporary solution until this
    process is fully automated.

---
+++ app/assets/javascripts/components/copy_to_clipboard.js
@@ -0,0 +1,26 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function() {
+  var fields = document.querySelectorAll("[data-copy-to-clipboard=true]");
+
+  fields.forEach(function(field) {
+    field.parentNode.appendChild(copyLinkBuilder(field));
+  });
+
+  function copyToClipboard(fieldToCopy) {
+    fieldToCopy.select();
+    document.execCommand("copy");
+  }
+
+  function copyLinkBuilder(fieldToCopy) {
+    var copyButton = document.createElement("button");
+    copyButton.innerHTML = "Copy to clipboard";
+    copyButton.className = "govuk-button govuk-!-margin-top-2";
+    copyButton.setAttribute("data-module", "govuk-button");
+    copyButton.addEventListener("click", function() {
+      copyToClipboard(fieldToCopy);
+    });
+
+    return copyButton;
+  }
+});
