78408a88878787af2e35129546c8a1a3c21fcc23
Robbie Paul <robbie.paul@dxw.com>
2019-12-10 11:20:48 +0000

    Payment mailer: handle payment w/ multiple claims
    The app continues to behave correctly in the case of a payment with
    one claim.
    
    The content has been provided by our content designer.
    
    Co-authored-by: Lawrence Forooghian <lawrence@dxw.com>

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,4 @@
 class ApplicationMailer < Mail::Notify::Mailer
   NOTIFY_TEMPLATE_ID = "d72e2ff9-b228-4f16-9099-fd9d411c0334".freeze
+  GENERIC_NOTIFY_REPLY_TO_ID = "2b8fbdcf-16da-49dc-ad07-4a4c45f0e7e6".freeze
 end
