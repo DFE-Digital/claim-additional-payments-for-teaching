9cd91ab81cd9e2bde4406cbdb69c88f5f8e5e323
Meyric Rawlings <meyric@dxw.com>
2019-12-11 09:41:33 +0000

    Refactor maths and physics school eligibility spec
    The exisiting spec used a great deal of nested contexts, instead I've tried to
    demonstate what makes a school eligible or not. For maths and physics there are
    four attributes we use to determine eligibility:
    
    - the school is open
    - the school is state funded
    - the school is in an eligble area
    - the school teaches secondary or it's equivalent
    
    I am hoping this is a simpler document of what makes a school eligble and also
    gives us appropriate test coverage.

--- spec/models/maths_and_physics/school_eligibility_spec.rb
+++ spec/models/maths_and_physics/school_eligibility_spec.rb
@@ -2,172 +2,117 @@ require "rails_helper"
 
 RSpec.describe MathsAndPhysics::SchoolEligibility do
   describe "#eligible_current_school?" do
-    subject { MathsAndPhysics::SchoolEligibility.new(school).eligible_current_school? }
-    let(:school) { build(:school, school_attributes.merge({local_authority_district: local_authority_district})) }
+    context "with a secondary school" do
+      let(:secondary_school) {
+        School.new(
+          school_type_group: :la_maintained,
+          phase: :secondary,
+          close_date: nil,
+          local_authority_district: local_authority_districts(:barnsley)
+        )
+      }
 
-    context "when it is in an eligible local authority district" do
-      let(:local_authority_district) { local_authority_districts(:barnsley) }
-
-      context "and it is a college" do
-        let(:college_attributes) { {school_type_group: :colleges} }
-
-        context "and it has an education phase of secondary, middle deemed secondary, or all-through" do
-          let(:school_attributes) { college_attributes.merge({phase: :middle_deemed_secondary}) }
-          it { is_expected.to be true }
-        end
-
-        context "and it has an education phase of nursery, primary, middle deemed primary, or 16+" do
-          let(:school_attributes) { college_attributes.merge({phase: :sixteen_plus}) }
-          it { is_expected.to be false }
-        end
+      it "returns true with an open, state funded secondary school in an eligible local authority district" do
+        expect(MathsAndPhysics::SchoolEligibility.new(secondary_school).eligible_current_school?).to eql true
       end
 
-      context "and it is an academy" do
-        let(:academy_school_attributes) { {school_type_group: :academies} }
-
-        context "and it has an education phase of secondary, middle deemed secondary, or all-through" do
-          let(:school_attributes) { academy_school_attributes.merge({phase: :secondary}) }
-          it { is_expected.to be true }
-        end
-
-        context "and it has an education phase of nursery, primary, middle deemed primary, or 16+" do
-          let(:school_attributes) { academy_school_attributes.merge({phase: :primary}) }
-          it { is_expected.to be false }
-        end
+      it "returns false when closed" do
+        secondary_school.assign_attributes(close_date: Date.new)
+        expect(MathsAndPhysics::SchoolEligibility.new(secondary_school).eligible_current_school?).to eql false
       end
 
-      context "and it is a free school" do
-        let(:free_school_attributes) { {school_type_group: :free_schools} }
-
-        context "and it has an education phase of secondary, middle deemed secondary, or all-through" do
-          let(:school_attributes) { free_school_attributes.merge({phase: :secondary}) }
-          it { is_expected.to be true }
-        end
-
-        context "and it has an education phase of nursery, primary, middle deemed primary, or 16+" do
-          let(:school_attributes) { free_school_attributes.merge({phase: :middle_deemed_primary}) }
-          it { is_expected.to be false }
-        end
+      it "returns false when not in an eligible local authority district" do
+        secondary_school.assign_attributes(local_authority_district: local_authority_districts(:camden))
+        expect(MathsAndPhysics::SchoolEligibility.new(secondary_school).eligible_current_school?).to eql false
       end
 
-      context "and it is a LA maintained school" do
-        let(:la_maintained_attributes) { {school_type_group: :la_maintained} }
-
-        context "and it has an education phase of secondary, middle deemed secondary, or all-through" do
-          let(:school_attributes) { la_maintained_attributes.merge({phase: :all_through}) }
-          it { is_expected.to be true }
-        end
-
-        context "and it has an education phase of nursery, primary, middle deemed primary, or 16+" do
-          let(:school_attributes) { la_maintained_attributes.merge({phase: :nursery}) }
-          it { is_expected.to be false }
-        end
-      end
-
-      context "and it is a state funded special school" do
-        let(:special_school_attributes) { {school_type: :community_special_school, school_type_group: :special_schools} }
-
-        context "and it has an education phase of secondary, middle deemed secondary, or all-through" do
-          let(:school_attributes) { special_school_attributes.merge({phase: :secondary}) }
-          it { is_expected.to be true }
-        end
-
-        context "and it has an education phase of nursery, primary, middle deemed primary, or 16+" do
-          let(:school_attributes) { special_school_attributes.merge({phase: :nursery}) }
-          it { is_expected.to be false }
-        end
-
-        context "and it doesn't have an education phase" do
-          let(:school_attributes) { special_school_attributes.merge({phase: :not_applicable}) }
-          it { is_expected.to be false }
-
-          context "and it has a statutory high age of 16" do
-            let(:school_attributes) { {statutory_high_age: 16} }
-            it { is_expected.to be(true) }
-          end
-        end
-      end
-
-      context "and it is a special free school" do
-        let(:special_free_school_attributes) { {school_type: :free_school_special, school_type_group: :free_schools} }
-
-        context "and it has an education phase of secondary, middle deemed secondary, or all-through" do
-          let(:school_attributes) { special_free_school_attributes.merge({phase: :secondary}) }
-          it { is_expected.to be true }
-        end
-
-        context "and it has an education phase of nursery, primary, middle deemed primary, or 16+" do
-          let(:school_attributes) { special_free_school_attributes.merge({phase: :nursery}) }
-          it { is_expected.to be false }
-        end
-
-        context "and it doesn't have an education phase" do
-          let(:school_attributes) { special_free_school_attributes.merge({phase: :not_applicable}) }
-          it { is_expected.to be false }
-
-          context "and it has a statutory high age of 16" do
-            let(:school_attributes) { {statutory_high_age: 16} }
-            it { is_expected.to be(true) }
-          end
-        end
-      end
-
-      context "and it is an independent special school" do
-        let(:special_school_attributes) { {school_type: :other_independent_special_school, school_type_group: :special_schools} }
-
-        context "and it has an education phase of secondary, middle deemed secondary, or all-through" do
-          let(:school_attributes) { special_school_attributes.merge({phase: :secondary}) }
-          it { is_expected.to be false }
-        end
-
-        context "and it has an education phase of nursery, primary, middle deemed primary, or 16+" do
-          let(:school_attributes) { special_school_attributes.merge({phase: :nursery}) }
-          it { is_expected.to be false }
-        end
-
-        context "and it doesn't have an education phase" do
-          let(:school_attributes) { special_school_attributes.merge({phase: :not_applicable}) }
-          it { is_expected.to be false }
-        end
-      end
-
-      context "and it is a special post 16 institution" do
-        let(:school_attributes) { {school_type: :special_post_16_institutions, school_type_group: :special_schools, phase: :not_applicable} }
-        it { is_expected.to be false }
-      end
-
-      context "and it is not a state funded school" do
-        let(:school_attributes) { {school_type_group: :independent_schools} }
-        it { is_expected.to be false }
+      it "returns false when not state funded" do
+        secondary_school.assign_attributes(school_type_group: :independent_schools)
+        expect(MathsAndPhysics::SchoolEligibility.new(secondary_school).eligible_current_school?).to eql false
       end
     end
 
-    context "when it is not in an eligible local authority district" do
-      let(:local_authority_district) { local_authority_districts(:camden) }
-      let(:school_attributes) { {} }
-      it { is_expected.to be false }
-    end
+    context "with a special school" do
+      let(:special_school) {
+        School.new(
+          close_date: nil,
+          school_type: :community_special_school,
+          school_type_group: :special_schools,
+          statutory_high_age: 16,
+          local_authority_district: local_authority_districts(:barnsley)
+        )
+      }
 
-    context "when the school is otherwise eligible but is closed" do
-      let(:school) { build(:school, :maths_and_physics_eligible, close_date: Date.yesterday) }
-      it { is_expected.to be false }
+      it "returns true with an open, state funded secondary equivalent special school in an eligible local authority district" do
+        expect(MathsAndPhysics::SchoolEligibility.new(special_school).eligible_current_school?).to eql true
+      end
+
+      it "returns false when closed" do
+        special_school.assign_attributes(close_date: Date.new)
+        expect(MathsAndPhysics::SchoolEligibility.new(special_school).eligible_current_school?).to eql false
+      end
+
+      it "returns false when not in an eligble local authority district" do
+        special_school.assign_attributes(local_authority_district: local_authority_districts(:camden))
+        expect(MathsAndPhysics::SchoolEligibility.new(special_school).eligible_current_school?).to eql false
+      end
+
+      it "returns false when not state funded" do
+        special_school.assign_attributes(school_type_group: :independent_schools)
+        expect(MathsAndPhysics::SchoolEligibility.new(special_school).eligible_current_school?).to eql false
+      end
+
+      it "returns false when not secondary equivalent" do
+        special_school.assign_attributes(statutory_high_age: 11)
+        expect(MathsAndPhysics::SchoolEligibility.new(special_school).eligible_current_school?).to eql false
+      end
     end
 
     context "with alternative provision school" do
-      it "returns true with a state funded secondary equivalent alternative provision school" do
-        alternative_provision_school = School.new(school_type_group: :la_maintained, school_type: :pupil_referral_unit, statutory_high_age: 19, local_authority_district: local_authority_districts(:barnsley))
+      let(:alternative_provision_school) {
+        School.new(
+          close_date: nil,
+          school_type_group: :la_maintained,
+          school_type: :pupil_referral_unit,
+          statutory_high_age: 19,
+          local_authority_district: local_authority_districts(:barnsley)
+        )
+      }
+
+      it "returns true with an open, state funded secondary equivalent alternative provision school in an eligible local authority district" do
         expect(MathsAndPhysics::SchoolEligibility.new(alternative_provision_school).eligible_current_school?).to eq true
+      end
+
+      it "returns false when closed" do
+        alternative_provision_school.assign_attributes(close_date: Date.new)
+        expect(MathsAndPhysics::SchoolEligibility.new(alternative_provision_school).eligible_current_school?).to eql false
+      end
+
+      it "returns false when not in an eligble local authority district" do
+        alternative_provision_school.assign_attributes(local_authority_district: local_authority_districts(:camden))
+        expect(MathsAndPhysics::SchoolEligibility.new(alternative_provision_school).eligible_current_school?).to eql false
+      end
+
+      it "returns false when not secondary equivalent" do
+        alternative_provision_school.assign_attributes(statutory_high_age: 11)
+        expect(MathsAndPhysics::SchoolEligibility.new(alternative_provision_school).eligible_current_school?).to eq false
       end
 
       it "returns true with a secure unit" do
-        alternative_provision_school = School.new(school_type_group: :other, school_type: :secure_unit, statutory_high_age: 19, local_authority_district: local_authority_districts(:barnsley))
+        alternative_provision_school.assign_attributes(school_type_group: :other, school_type: :secure_unit)
         expect(MathsAndPhysics::SchoolEligibility.new(alternative_provision_school).eligible_current_school?).to eq true
       end
+    end
+  end
 
-      it "returns false with a alternative provision school that is not secondary equivalent" do
-        alternative_provision_school = School.new(school_type_group: :la_maintained, school_type: :pupil_referral_unit, statutory_high_age: 11, local_authority_district: local_authority_districts(:barnsley))
-        expect(MathsAndPhysics::SchoolEligibility.new(alternative_provision_school).eligible_current_school?).to eq false
-      end
+  context "when it is not a secondary school" do
+    it "returns false" do
+      primary_school = School.new(
+        phase: :primary,
+        school_type_group: :la_maintained,
+        local_authority_district: local_authority_districts(:barnsley)
+      )
+      expect(MathsAndPhysics::SchoolEligibility.new(primary_school).eligible_current_school?).to eql false
     end
   end
 end
