829712cfcb27e9c9a627068bd243d532d4b4f37b
Stuart Harrison <pezholio@gmail.com>
2019-12-18 14:27:43 +0000

    Add an #all method to DfeSignIn::Api::User class
    This allows us to call the /users endpoint to get all the users in an
    organisation. I've also extended to class to accept and return more arguments
    to give us optionally more information about a user.

--- lib/dfe_sign_in/utils.rb
+++ lib/dfe_sign_in/utils.rb
@@ -8,5 +8,19 @@ module DfeSignIn
       }
       JWT.encode(payload, DfeSignIn.configuration.secret, "HS256")
     end
+
+    def get(uri)
+      request = Net::HTTP::Get.new(uri)
+      request["Authorization"] = "bearer #{generate_jwt_token}"
+      request["Content-Type"] = "application/json"
+
+      response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http|
+        http.request(request)
+      }
+
+      raise ExternalServerError, "#{response.code}: #{response.body}" unless response.code.eql?("200")
+
+      JSON.parse(response.body)
+    end
   end
 end
