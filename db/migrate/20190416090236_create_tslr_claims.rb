fa5e3da3e704211edb33d9e8ad4c3f4caafb0f2f
Tekin Suleyman <tekin@tekin.co.uk>
2019-12-19 10:26:41 +0000

    Ensure first migration will run without error
    Because of the timestamp ordering of the first two migrations, running
    rails db:migrate on a fresh install will fail. This is because the first
    timestamped migration tries to create a table with a UUID primary key
    _before_ the PostgreSQL extension (pgcrypto) required to support UUID
    primary keys has been enabled (happens in the second timestamped
    migration).
    
    This wasn't an issue when the app was first set up as the second
    migration was merged before the first. But this makes install difficult
    to do things like run a Heroku pipeline for review apps where we're
    running rails db:migrate as part of the release process.
    
    Modifying the existing migration to enable the extension feels like the
    least bad option as it won't impact existing deployments (no timestamps
    are changed and thus as far as Rails is concerned, all existing
    migrations have been run), and `enable_extension` is idempotent so
    calling it twice will have no negative impact.

--- db/migrate/20190416090236_create_tslr_claims.rb
+++ db/migrate/20190416090236_create_tslr_claims.rb
@@ -1,5 +1,7 @@
 class CreateTslrClaims < ActiveRecord::Migration[5.2]
   def change
+    enable_extension "pgcrypto"
+
     create_table :tslr_claims, id: :uuid do |t|
       t.string :qts_award_year
 
