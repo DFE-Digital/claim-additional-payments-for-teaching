bcb6709b1c0cc55ae64b0cf5a7b71741c29c3606
Stuart Harrison <pezholio@gmail.com>
2019-12-20 10:06:56 +0000

    Add permission and role checking methods to User
    This will allow us to use the `DfeSignIn::User` class to check a user's
    permissions rather tha having to initialize an `AdminSession` class with
    three session variables.

--- app/models/dfe_sign_in/user.rb
+++ app/models/dfe_sign_in/user.rb
@@ -1,11 +1,31 @@
 module DfeSignIn
   class User < ApplicationRecord
+    SERVICE_OPERATOR_DFE_SIGN_IN_ROLE_CODE = "teacher_payments_access"
+    SUPPORT_AGENT_DFE_SIGN_IN_ROLE_CODE = "teacher_payments_support"
+    PAYROLL_OPERATOR_DFE_SIGN_IN_ROLE_CODE = "teacher_payments_payroll"
+
     def self.table_name
       "dfe_sign_in_users"
     end
 
     def full_name
       [given_name, family_name].join(" ")
+    end
+
+    def is_service_operator?
+      role_codes.include?(SERVICE_OPERATOR_DFE_SIGN_IN_ROLE_CODE)
+    end
+
+    def is_support_agent?
+      role_codes.include?(SUPPORT_AGENT_DFE_SIGN_IN_ROLE_CODE)
+    end
+
+    def is_payroll_operator?
+      role_codes.include?(PAYROLL_OPERATOR_DFE_SIGN_IN_ROLE_CODE)
+    end
+
+    def has_admin_access?
+      is_service_operator? || is_support_agent? || is_payroll_operator?
     end
   end
 end
--- spec/models/dfe_sign_in/user_spec.rb
+++ spec/models/dfe_sign_in/user_spec.rb
@@ -8,4 +8,56 @@ RSpec.describe DfeSignIn::User, type: :model do
       expect(user.full_name).to eq("Jo Bloggs")
     end
   end
+
+  describe "#is_service_operator?" do
+    it "returns true when the user has the right role" do
+      user.role_codes = [DfeSignIn::User::SERVICE_OPERATOR_DFE_SIGN_IN_ROLE_CODE]
+      expect(user.is_service_operator?).to eq true
+
+      user.role_codes = ["other-role"]
+      expect(user.is_service_operator?).to eq false
+    end
+  end
+
+  describe "#is_support_agent?" do
+    it "returns true when the user has the right role" do
+      user.role_codes = [DfeSignIn::User::SUPPORT_AGENT_DFE_SIGN_IN_ROLE_CODE]
+      expect(user.is_support_agent?).to eq true
+
+      user.role_codes = ["other-role"]
+      expect(user.is_support_agent?).to eq false
+    end
+  end
+
+  describe "#has_admin_access?" do
+    it "returns true when user is a service operator" do
+      user.role_codes = [DfeSignIn::User::SERVICE_OPERATOR_DFE_SIGN_IN_ROLE_CODE]
+      expect(user.has_admin_access?).to eq true
+    end
+
+    it "returns true when user is a support user" do
+      user.role_codes = [DfeSignIn::User::SUPPORT_AGENT_DFE_SIGN_IN_ROLE_CODE]
+      expect(user.has_admin_access?).to eq true
+    end
+
+    it "returns true when user is a payroll operator" do
+      user.role_codes = [DfeSignIn::User::PAYROLL_OPERATOR_DFE_SIGN_IN_ROLE_CODE]
+      expect(user.has_admin_access?).to eq true
+    end
+
+    it "returns true when user has multiple roles" do
+      user.role_codes = [
+        DfeSignIn::User::SUPPORT_AGENT_DFE_SIGN_IN_ROLE_CODE,
+        DfeSignIn::User::SERVICE_OPERATOR_DFE_SIGN_IN_ROLE_CODE,
+      ]
+
+      expect(user.has_admin_access?).to eq true
+    end
+
+    it "returns false when user does not have a valid role" do
+      user.role_codes = ["other-role"]
+
+      expect(user.has_admin_access?).to eq false
+    end
+  end
 end
