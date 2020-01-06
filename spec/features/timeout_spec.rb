2a5144cb3e1b08b007c8ca6f3f0472bcc9b318bf
Nick Jackson <nick@dxw.com>
2020-01-06 14:33:05 +0000

    Improve test for timeout refresh prompt

--- spec/features/timeout_spec.rb
+++ spec/features/timeout_spec.rb
@@ -19,6 +19,8 @@ RSpec.feature "Teacher Student Loan Repayments claims", js: true do
     expect(page).to have_content("Your session will expire in #{one_second_in_minutes} minutes")
     expect_any_instance_of(SessionsController).to receive(:update_last_seen_at)
     click_on "Continue session"
+    expect(current_path).to eql(claim_path(StudentLoans.routing_name, "claim-school"))
+    expect(page).not_to have_content("Your session will expire in #{one_second_in_minutes} minutes")
   end
 
   scenario "Claimants are automatically redirected to the timeout page" do
