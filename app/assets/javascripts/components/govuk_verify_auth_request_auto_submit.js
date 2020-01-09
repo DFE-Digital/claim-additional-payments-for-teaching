86227ce618d7c80430cc7c6b2cc06504220760c1
Lawrence Forooghian <lawrence@dxw.com>
2020-01-09 14:36:31 +0000

    Reduce the chance of user submitting GOV.UK Verify SAML form twice
    As we have observed in the past (e.g. commit 0f225f8), submitting the
    same SAML request to Verify twice causes Verify to show a "Sorry,
    something went wrong" error screen.
    
    It was possible to submit the request twice by clicking the "Continue"
    button on the `/verify/authentications/new` page after the JavaScript
    auto-submit had been triggered. (I was able to reproduce this in a
    browser, in our development environment, although not consistently.)
    
    To prevent this, we follow an approach similar to that suggested by the
    Verify documentation [1] and various Verify-using services which I
    looked at, and we hide the submit button when JavaScript is enabled. We
    also change the heading and explanatory messages so that the user knows
    what's going on and how to recover from an error.
    
    I've also updated the JS-disabled message to match other services, which
    explain that the user needs to click the button _because_ JS is
    disabled.
    
    I'm not sure if there is anything we can do to prevent a double
    submission in the non-JS case.
    
    [1] https://www.docs.verify.service.gov.uk/get-started/set-up-successful-verification-journey/#send-the-authentication-request-to-the-testing-service

--- app/assets/javascripts/components/govuk_verify_auth_request_auto_submit.js
+++ app/assets/javascripts/components/govuk_verify_auth_request_auto_submit.js
@@ -7,5 +7,14 @@ document.addEventListener("DOMContentLoaded", function() {
     return;
   }
 
+  var submit_button = form.querySelector("#submit_verify_request");
+  submit_button.style.display = "none";
+
+  var heading = form.querySelector("#verify_heading");
+  heading.innerText = "Continuing to next step…";
+
+  var redirection_message = form.querySelector("#verify_redirection_message");
+  redirection_message.innerText = "If your browser does not redirect in a few seconds, please try refreshing the page.";
+
   form.submit();
 });
