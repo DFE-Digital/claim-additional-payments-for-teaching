4894dc9298dc42c73c283d9812e30f9240a78d4d
Tekin Suleyman <tekin@tekin.co.uk>
2020-01-29 10:12:53 +0000

    Session refresh doesn't try redirect for timeout
    We've seen requests come in for this endpoint with expired claim
    sessions, resulting in the followig exception being raised:
    
      ActionController::UrlGenerationError: No route matches
        {:action=>"timeout", :controller=>"claims", :policy=>nil},
        possible unmatched constraints: [:policy]
    
    This happened because the endpoint isn't namespaced to a policy. This is
    definitely an edge-case as the only place this endpoint is hit from
    during normal use is from the JS-based timeout dialogue that pops
    up when a session is due to expire.
    
    Because this endpoint is hit for both admin and claim sessions we should
    stop relying on the before actions on `BasePublicController` and instead
    explicitly clear sessions that have expired to prevent expired sessions
    from being restarted.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,11 @@
-class SessionsController < BasePublicController
+class SessionsController < ApplicationController
+  include ClaimSessionTimeout
+  include AdminSessionTimeout
+
   def refresh
+    clear_claim_session if claim_session_timed_out?
+    end_expired_admin_sessions
+
     head :ok
   end
 end
