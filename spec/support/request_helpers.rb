71e0f9df30d05df6346cef85ca77884d59244ea1
Tekin Suleyman <tekin@tekin.co.uk>
2020-02-12 15:33:02 +0000

    Extract start_claim request helper
    This will make it easier to write request specs that work across all
    policies.

--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -1,12 +1,20 @@
 module RequestHelpers
   def start_student_loans_claim
-    post claims_path(StudentLoans.routing_name), params: {
-      claim: {
+    start_claim(StudentLoans)
+  end
+
+  def start_claim(policy)
+    post claims_path(policy.routing_name), params: {claim: claim_create_params_for(policy)}
+  end
+
+  def claim_create_params_for(policy)
+    {
+      StudentLoans => {
         eligibility_attributes: {
           qts_award_year: "on_or_after_september_2013",
         },
       },
-    }
+    }.fetch(policy)
   end
 
   def sign_in_to_admin_with_role(*args)
