99042f230ec7d3147eca25c1190c69db4b11157a
Tekin Suleyman <tekin@tekin.co.uk>
2020-02-12 14:47:55 +0000

    Document the PolicyConfiguration class

--- app/models/policy_configuration.rb
+++ app/models/policy_configuration.rb
@@ -1,3 +1,12 @@
+# Policy-specific configuration, managed through the service operator's admin
+# interface.
+#
+# Things that are currently configurable:
+#
+# * open_for_submissions: defines whether the policy is currently accepting
+#   claims or not
+# * availability_message: an optional message that is shown to users when the
+#   policy is closed for submissions
 class PolicyConfiguration < ApplicationRecord
   validates :policy_type, inclusion: {in: Policies.all.map(&:name)}
 
