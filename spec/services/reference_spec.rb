d504f8ed02ee93921307f74de11b418bb933f31a
Tekin Suleyman <tekin@tekin.co.uk>
2020-02-24 15:21:06 +0000

    Simplify Reference specs

--- spec/services/reference_spec.rb
+++ spec/services/reference_spec.rb
@@ -1,17 +1,15 @@
 require "rails_helper"
 
 RSpec.describe Reference do
-  subject { described_class.new }
+  let(:reference) { Reference.new }
 
   describe "to_s" do
-    let(:reference) { subject.to_s }
-
     it "is 8 characters long" do
-      expect(reference.length).to eq(8)
+      expect(reference.to_s.length).to eq(8)
     end
 
     it "contains only numbers and capital letters, excluding 0 and O" do
-      expect(reference).to match(/\A[1-9A-NP-Z]+\Z/)
+      expect(reference.to_s).to match(/\A[1-9A-NP-Z]+\Z/)
     end
   end
 end
