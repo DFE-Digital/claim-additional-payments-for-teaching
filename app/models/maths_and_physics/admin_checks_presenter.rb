099c61db905d0017cf99b73d71d57f339e91d9ca
Tekin Suleyman <tekin@tekin.co.uk>
2020-02-25 10:54:15 +0000

    Extract method  in M&P AdminChecksPresenter

--- app/models/maths_and_physics/admin_checks_presenter.rb
+++ app/models/maths_and_physics/admin_checks_presenter.rb
@@ -18,7 +18,7 @@ module MathsAndPhysics
     def qualifications
       [].tap do |a|
         a << ["Award year", qts_award_year_answer]
-        a << ["ITT subject", (eligibility.initial_teacher_training_subject_specialism || eligibility.initial_teacher_training_subject).humanize]
+        a << ["ITT subject", itt_subject]
         a << ["Maths or Physics degree", maths_or_physics_degree] if eligibility.has_uk_maths_or_physics_degree.present?
       end
     end
@@ -33,6 +33,10 @@ module MathsAndPhysics
 
     def eligibility
       claim.eligibility
+    end
+
+    def itt_subject
+      (eligibility.initial_teacher_training_subject_specialism || eligibility.initial_teacher_training_subject).humanize
     end
 
     def maths_or_physics_degree
