4b747f16683f9d00b63340e2f25a54d26a9cdc72
Tekin Suleyman <tekin@tekin.co.uk>
2020-03-03 11:28:49 +0000

    Set instance var for claim in ClaimMailer actions
    We were setting it in the view_mail_with_claim_and_subject method.
    Setting it earlier means we an use it earlier and don't need to pass it
    around quite as much.

--- app/mailers/claim_mailer.rb
+++ app/mailers/claim_mailer.rb
@@ -2,40 +2,43 @@ class ClaimMailer < ApplicationMailer
   helper :application
 
   def submitted(claim)
-    @claim_description = claim_description(claim)
-    view_mail_with_claim_and_subject(claim, "Your claim #{@claim_description} has been received, reference number: #{claim.reference}")
+    @claim = claim
+    @claim_description = claim_description
+    view_mail_with_claim_and_subject("Your claim #{@claim_description} has been received, reference number: #{claim.reference}")
   end
 
   def approved(claim)
-    @claim_description = claim_description(claim)
-    view_mail_with_claim_and_subject(claim, "Your claim #{@claim_description} has been approved, reference number: #{claim.reference}")
+    @claim = claim
+    @claim_description = claim_description
+    view_mail_with_claim_and_subject("Your claim #{@claim_description} has been approved, reference number: #{claim.reference}")
   end
 
   def rejected(claim)
-    @claim_description = claim_description(claim)
-    @possible_rejection_reasons = I18n.t("#{claim.policy.locale_key}.possible_rejection_reasons", qts_year: ineligible_qts_year(claim))
-    view_mail_with_claim_and_subject(claim, "Your claim #{@claim_description} has been rejected, reference number: #{claim.reference}")
+    @claim = claim
+    @claim_description = claim_description
+    @possible_rejection_reasons = I18n.t("#{@claim.policy.locale_key}.possible_rejection_reasons", qts_year: ineligible_qts_year)
+    view_mail_with_claim_and_subject("Your claim #{@claim_description} has been rejected, reference number: #{claim.reference}")
   end
 
   def update_after_three_weeks(claim)
-    @claim_description = claim_description(claim)
-    view_mail_with_claim_and_subject(claim, "We are still reviewing your claim #{@claim_description}, reference number: #{claim.reference}")
+    @claim = claim
+    @claim_description = claim_description
+    view_mail_with_claim_and_subject("We are still reviewing your claim #{@claim_description}, reference number: #{@claim.reference}")
   end
 
   private
 
-  def claim_description(claim)
-    I18n.t("#{claim.policy.locale_key}.claim_description")
+  def claim_description
+    I18n.t("#{@claim.policy.locale_key}.claim_description")
   end
 
-  def ineligible_qts_year(claim)
-    (claim.policy.first_eligible_qts_award_year - 1).to_s(:long)
+  def ineligible_qts_year
+    (@claim.policy.first_eligible_qts_award_year - 1).to_s(:long)
   end
 
-  def view_mail_with_claim_and_subject(claim, subject)
-    @claim = claim
-    @display_name = [claim.first_name, claim.surname].join(" ")
-    @policy = claim.policy
+  def view_mail_with_claim_and_subject(subject)
+    @display_name = [@claim.first_name, @claim.surname].join(" ")
+    @policy = @claim.policy
 
     view_mail(
       NOTIFY_TEMPLATE_ID,
