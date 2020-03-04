01849dcd4e3059afbc3b094bc1972cfa7b410578
Tekin Suleyman <tekin@tekin.co.uk>
2020-03-04 15:18:33 +0000

    Add StudentLoans#current_financial_year method
    We'll use this during the claim journey to play back the financial year
    that claimants are currently claiming for.

--- app/models/student_loans.rb
+++ app/models/student_loans.rb
@@ -69,6 +69,18 @@ module StudentLoans
     ].max
   end
 
+  # Returns human-friendly String for the financial year that Student Loans
+  # claims are being made against based on the currently-configured academic
+  # year for the StudentLoans policy. For example:
+  #
+  #   "6 April 2018 and 5 April 2019"
+  def current_financial_year
+    end_year = configuration.current_academic_year.start_year
+    start_year = end_year - 1
+
+    "6 April #{start_year} and 5 April #{end_year}"
+  end
+
   def configuration
     PolicyConfiguration.for(self)
   end
--- spec/models/student_loans_spec.rb
+++ spec/models/student_loans_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
 RSpec.describe StudentLoans, type: :model do
-  describe ".first_eligible_qts_award_year" do
-    let(:policy_configuration) { policy_configurations(:student_loans) }
+  let(:policy_configuration) { policy_configurations(:student_loans) }
 
+  describe ".first_eligible_qts_award_year" do
     it "returns 11 years prior to the currently configured academic year, with a floor of the 2013/2014 academic year" do
       policy_configuration.update!(current_academic_year: "2031/2032")
       expect(StudentLoans.first_eligible_qts_award_year).to eq AcademicYear.new(2020)
@@ -20,6 +20,16 @@ RSpec.describe StudentLoans, type: :model do
 
     it "can return the AcademicYear based on a passed-in academic year" do
       expect(StudentLoans.first_eligible_qts_award_year(AcademicYear.new(2030))).to eq AcademicYear.new(2019)
+    end
+  end
+
+  describe ".current_financial_year" do
+    it "returns a human-friendly string for the financial year the policy is currently accepting claims for" do
+      # Based on the current academic year set by the policy_configurations.yml fixtures
+      expect(StudentLoans.current_financial_year).to eq "6 April 2024 and 5 April 2025"
+
+      policy_configuration.update!(current_academic_year: "2020/2021")
+      expect(StudentLoans.current_financial_year).to eq "6 April 2019 and 5 April 2020"
     end
   end
 end
