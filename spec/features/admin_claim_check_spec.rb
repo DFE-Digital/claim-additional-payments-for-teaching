43dc9b847d7d356365ecc4c68e0191985d56a38a
Tekin Suleyman <tekin@tekin.co.uk>
2020-03-09 13:50:08 +0000

    Separate out admin filtering claims feature spec
    The admin_claim_check_spec.rb file has got unwieldy. Let's split it up
    into discrete feature spec files where appropriate.

--- spec/features/admin_claim_check_spec.rb
+++ spec/features/admin_claim_check_spec.rb
@@ -130,32 +130,6 @@ RSpec.feature "Admin checks a claim" do
       expect(page).to have_content(user.full_name)
     end
 
-    context "with a mixture of policy types" do
-      let!(:maths_and_physics_claims) { create_list(:claim, 3, :submitted, policy: MathsAndPhysics) }
-      let!(:student_loan_claims) { create_list(:claim, 2, :submitted, policy: StudentLoans) }
-
-      it "shows the policy types on the index page" do
-        click_on "View claims"
-
-        expect(page.find("table")).to have_content("Maths and Physics").exactly(3).times
-        expect(page.find("table")).to have_content("Student Loans").exactly(2).times
-      end
-
-      it "can filter by claim type" do
-        click_on "View claims"
-        select "Maths and Physics", from: "policy"
-        click_on "Go"
-
-        maths_and_physics_claims.each do |c|
-          expect(page).to have_content(c.reference)
-        end
-
-        student_loan_claims.each do |c|
-          expect(page).to_not have_content(c.reference)
-        end
-      end
-    end
-
     context "when the service operator completes the last check" do
       context "and the payroll gender is missing" do
         let!(:claim) { create(:claim, :submitted, payroll_gender: :dont_know) }
