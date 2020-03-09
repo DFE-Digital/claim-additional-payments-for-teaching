7e0e9fab0de5efd75a3eedff6ea9ddef88123e22
Tekin Suleyman <tekin@tekin.co.uk>
2020-03-09 14:18:36 +0000

    Update payroll gender spec to approve claim
    Now we can amend claims we can make this feature spec cover the full
    journey for checking a claim with missing payroll gender.

--- spec/features/admin_claim_with_missing_payroll_gender_spec.rb
+++ spec/features/admin_claim_with_missing_payroll_gender_spec.rb
@@ -15,5 +15,18 @@ RSpec.feature "Admin checking a claim missing a payroll gender" do
 
     expect(page).to have_field("Approve", disabled: true)
     expect(page).to have_content(I18n.t("admin.unknown_payroll_gender_preventing_approval_message"))
+
+    click_on "Amend claim"
+    select "Male", from: "Payroll gender"
+    fill_in "Change notes", with: "Got payroll gender from pensions data"
+    click_on "Amend claim"
+
+    expect(claim_missing_payroll_gender.reload).to be_approvable
+
+    choose "Approve"
+    fill_in "Decision notes", with: "Everything matches"
+    click_on "Confirm decision"
+
+    expect(page).to have_content("Claim has been approved successfully")
   end
 end
