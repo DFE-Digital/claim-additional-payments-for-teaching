a6521f1e0aef4fb9d301bb85550981ebe8326b6c
Tekin Suleyman <tekin@tekin.co.uk>
2020-03-09 16:14:03 +0000

    Split out separate feature specs for claim tasks
    We're about to start making these two journeys distinct by introducing
    the Student Loan amount check for TSLR claims so it makes sense to split
    out separate feature files for checking a claim for each policy.

--- spec/features/admin_claim_check_spec.rb
+++ spec/features/admin_claim_check_spec.rb
@@ -68,39 +68,6 @@ RSpec.feature "Admin checks a claim" do
       expect(mail.body.raw_source).to match("not been able to approve")
     end
 
-    scenario "User can complete the claim checks and approve a claim" do
-      claim = create(:claim, :submitted, policy: MathsAndPhysics)
-
-      visit admin_claims_path
-      find("a[href='#{admin_claim_checks_path(claim)}']").click
-
-      expect(page).to have_content("1. Qualifications")
-      expect(page).to have_content("2. Employment")
-
-      click_on "Check qualification information"
-      expect(page).to have_content("Qualifications")
-      expect(page).to have_content("Award year")
-      expect(page).to have_content(claim.eligibility.qts_award_year_answer)
-
-      click_on "Complete qualifications check and continue"
-
-      expect(page).to have_content("Employment")
-      expect(page).to have_content("Current school")
-      expect(page).to have_link(claim.eligibility.current_school.name)
-
-      click_on "Complete employment check and continue"
-
-      expect(page).to have_content("Claim decision")
-
-      choose "Approve"
-      fill_in "Decision notes", with: "All checks passed!"
-      click_on "Confirm decision"
-
-      expect(page).to have_content("Claim has been approved successfully")
-      expect(claim.decision).to be_approved
-      expect(claim.decision.created_by).to eq(user)
-    end
-
     scenario "User can see completed checks" do
       ten_minutes_ago = 10.minutes.ago
       checking_user = create(:dfe_signin_user, given_name: "Fred", family_name: "Smith")
