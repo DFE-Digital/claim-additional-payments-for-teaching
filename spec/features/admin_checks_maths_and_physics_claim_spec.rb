a99052002ef532894eac8266b7b71449140ccc76
Stuart Harrison <pezholio@gmail.com>
2020-03-11 16:10:02 +0000

    Add form with yes/no response to tasks view

--- spec/features/admin_checks_maths_and_physics_claim_spec.rb
+++ spec/features/admin_checks_maths_and_physics_claim_spec.rb
@@ -18,17 +18,24 @@ RSpec.feature "Admin checking a Maths & Physics claim" do
     expect(page).to have_content("3. Decision")
 
     click_on "Check qualification information"
-    expect(page).to have_content("Qualifications")
+
+    expect(page).to have_content(I18n.t("maths_and_physics.admin.tasks.qualifications.question"))
     expect(page).to have_content("Award year")
     expect(page).to have_content(claim.eligibility.qts_award_year_answer)
 
-    click_on "Complete qualifications check and continue"
+    choose "Yes"
+    click_on "Save and continue"
 
-    expect(page).to have_content("Employment")
+    expect(claim.tasks.find_by!(name: "qualifications").passed?).to eq(true)
+
+    expect(page).to have_content(I18n.t("maths_and_physics.admin.tasks.employment.question"))
     expect(page).to have_content("Current school")
     expect(page).to have_link(claim.eligibility.current_school.name)
 
-    click_on "Complete employment check and continue"
+    choose "Yes"
+    click_on "Save and continue"
+
+    expect(claim.tasks.find_by!(name: "employment").passed?).to eq(true)
 
     expect(page).to have_content("Claim decision")
 
