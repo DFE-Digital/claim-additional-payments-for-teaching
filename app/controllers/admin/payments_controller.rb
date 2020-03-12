48aa7f3ed1a403d3392012da0a267b5ea0096fc5
Tekin Suleyman <tekin@tekin.co.uk>
2020-03-12 15:37:49 +0000

    Load claims and not just their references
    This will make it easier to turn the references into links.

--- app/controllers/admin/payments_controller.rb
+++ app/controllers/admin/payments_controller.rb
@@ -5,13 +5,14 @@ module Admin
     before_action :find_payment
 
     def remove
+      @claims = @payment.claims
     end
 
     def destroy
       if @payroll_run.confirmation_report_uploaded?
         redirect_to admin_payroll_run_path(@payroll_run), alert: "A payment cannot be removed from an already confirmed payroll run"
       else
-        @claim_references = @payment.claims.pluck(:reference)
+        @claims = @payment.claims.to_a
         @payment.destroy
       end
     end
