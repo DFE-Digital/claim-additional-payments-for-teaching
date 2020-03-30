358dc173ab7618f20d1b5304c8ba2ed61036f04c
Lorna <lorna@dxw.com>
2020-03-30 15:08:30 +0100

    Display "Removed" when personal data from the tasks view is removed

--- spec/features/admin_claim_without_personal_data_spec.rb
+++ spec/features/admin_claim_without_personal_data_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature "Admin checking a claim with personal data removed" do
     sign_in_to_admin_with_role(DfeSignIn::User::SERVICE_OPERATOR_DFE_SIGN_IN_ROLE_CODE, user.dfe_sign_in_id)
   end
 
-  scenario "the service operator sees that the personal data has been removed" do
+  scenario "the service operator sees that personal data has been removed from the full claim view" do
     claim_with_personal_data_removed = create(:claim, :rejected, :personal_data_removed)
 
     visit admin_claim_path(claim_with_personal_data_removed)
@@ -16,5 +16,17 @@ RSpec.feature "Admin checking a claim with personal data removed" do
     expect(page).to have_content("Date of birth Removed")
     expect(page).to have_content("National Insurance number Removed")
     expect(page).to have_content("Address Removed")
+  end
+
+  scenario "the service operator sees that personal data has been removed from the view on tasks page" do
+    claim_with_personal_data_removed = create(:claim, :rejected, :personal_data_removed)
+
+    visit admin_claim_path(claim_with_personal_data_removed)
+
+    click_on "View tasks"
+
+    expect(page).to have_content("Full name Removed")
+    expect(page).to have_content("Date of birth Removed")
+    expect(page).to have_content("NI number Removed")
   end
 end
