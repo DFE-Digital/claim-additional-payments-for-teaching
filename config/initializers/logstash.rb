1e50b9e64a1928fc32a304468401699ad67ea356
Robbie Paul <robbie.paul@dxw.com>
2020-04-06 11:19:40 +0100

    Change SemanticLogger to append to JSON file
    We're swapping our logging shipping method from inline TCP requests to
    being managed by a seperate agent called "Filebeat".
    
    Remove the EnvironmentLogFormatter as we can configure Filebeat to send
    the environment name.

--- config/initializers/logstash.rb
+++ config/initializers/logstash.rb
@@ -1,12 +1,5 @@
-require "environment_log_formatter"
-
 if ENV["LOGSTASH_HOST"].present?
   Rails.application.configure do
-    tcp_logger = LogStashLogger.new(type: :tcp,
-                                    host: ENV.fetch("LOGSTASH_HOST"),
-                                    port: ENV.fetch("LOGSTASH_PORT"),
-                                    ssl_enable: true)
-
-    SemanticLogger.add_appender(logger: tcp_logger, level: :info, formatter: EnvironmentLogFormatter.new)
+    SemanticLogger.add_appender(file_name: "log/#{Rails.env}.json", formatter: :json)
   end
 end
