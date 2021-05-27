7ed753446eba6013f2b0ad08a6f50c7fb1616bb0
Mark Sutton <work.sutton.notifications@gmail.com>
2021-05-28 00:58:17 +0100

    [bug/ECP-903] - fix issue when 'No' is selected for Student Loans question and claim could failing valid?(:submit)

--- app/models/early_career_payments/eligibility.rb
+++ app/models/early_career_payments/eligibility.rb
@@ -59,8 +59,8 @@ module EarlyCareerPayments
     validates :eligible_itt_subject, on: [:"eligible-itt-subject", :submit], presence: {message: "Select if you completed your initial teacher training in Chemistry, Foreign Languages, Mathematics, Physics or None of these subjects"}
     validates :teaching_subject_now, on: [:"teaching-subject-now", :submit], inclusion: {in: [true, false], message: "Select yes if you are currently teaching in your ITT subject now"}
     validates :itt_academic_year, on: [:"itt-year", :submit], presence: {message: "Select if you started your initial teacher training in 2018 - 2019, 2019 - 2020, 2020 - 2021 or None of these academic years"}
-    validates :postgraduate_masters_loan, on: [:"masters-loan", :submit], inclusion: {in: [true, false], message: "Select yes if you have a Postgraduate Master Loan taken out on or after 1st August 2016"}
-    validates :postgraduate_doctoral_loan, on: [:"doctoral-loan", :submit], inclusion: {in: [true, false], message: "Select yes if you have a Postgraduate Doctoral Loan taken out on or after 1st August 2018"}
+    validates :postgraduate_masters_loan, on: [:"masters-loan", :submit], inclusion: {in: [true, false], message: "Select yes if you have a Postgraduate Master Loan taken out on or after 1st August 2016"}, unless: :no_student_loan?
+    validates :postgraduate_doctoral_loan, on: [:"doctoral-loan", :submit], inclusion: {in: [true, false], message: "Select yes if you have a Postgraduate Doctoral Loan taken out on or after 1st August 2018"}, unless: :no_student_loan?
 
     delegate :name, to: :current_school, prefix: true, allow_nil: true
 
@@ -140,6 +140,10 @@ module EarlyCareerPayments
         not_employed_directly? ||
         subject_to_disciplinary_action? ||
         ineligible_itt_academic_year?
+    end
+
+    def no_student_loan?
+      claim.no_student_loan?
     end
   end
 end
