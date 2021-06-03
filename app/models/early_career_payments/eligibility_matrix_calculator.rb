e1ee538732c2d98412a309603ebfa5179b513a2b
Rick Jones <rick.jones@playtimestudios.com>
2021-06-03 16:41:25 +0100

    Revert "Merge pull request #1403 from DFE-Digital/story/ECP-908-change-to-using-correct-amount-in-eligible-later-2022-screen"
    This reverts commit 48ffdd2ba8d41b18a917450089623bcee6b3f41c, reversing
    changes made to 3efa072f21a3b18cfa65ba297896129e1ef74a07.

--- app/models/early_career_payments/eligibility_matrix_calculator.rb
+++ app/models/early_career_payments/eligibility_matrix_calculator.rb
@@ -1,5 +1,12 @@
 module EarlyCareerPayments
   class EligibilityMatrixCalculator
+    MATRIX = {
+      mathematics: ["2018_2019", "2019_2020", "2020_2021"],
+      physics: ["2020_2021"],
+      chemistry: ["2020_2021"],
+      foreign_languages: ["2020_2021"]
+    }.freeze
+
     attr_reader :eligibility
 
     def initialize(eligibility)
@@ -9,7 +16,7 @@ module EarlyCareerPayments
     def eligible_later?
       return false if subject == :mathematics && itt_academic_year == "2018_2019"
 
-      MATRIX.dig(subject.to_s, itt_academic_year).nil? ? false : true
+      MATRIX.dig(subject)&.include?(itt_academic_year) ? true : false
     end
 
     private
--- config/initializers/early_career_payments/eligibility_matrix_calculator.rb
+++
@@ -1,1 +0,0 @@
-MATRIX = YAML.safe_load(File.read(File.expand_path("../eligibility_matrix.yml", __FILE__))).freeze
