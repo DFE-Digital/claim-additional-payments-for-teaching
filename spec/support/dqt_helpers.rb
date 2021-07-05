6e8aaf7806923bb798da8119bcd8983b9dccb8a8
Rick Jones <rick.jones@playtimestudios.com>
2021-07-05 12:09:58 +0100

    Update DQT client qualified teacher status to handle null data response
    ECP-1055

--- spec/support/dqt_helpers.rb
+++ spec/support/dqt_helpers.rb
@@ -1,31 +1,46 @@
 module DqtHelpers
-  def stub_qualified_teaching_status_show(claim:)
-    stub_request(:get, "#{ENV["DQT_CLIENT_HOST"]}:#{ENV["DQT_CLIENT_PORT"]}/api/qualified-teachers/qualified-teaching-status").with(
+  def stub_qualified_teaching_status_show(claim:, overrides: nil)
+    defaults = {
       query: WebMock::API.hash_including(
         {
           trn: claim.teacher_reference_number,
           niNumber: claim.national_insurance_number
         }
-      )
-    ).to_return(
-      body: <<~JSON
-        {
-          "data": [
-            {
-              "trn": "#{claim.teacher_reference_number}",
-              "name": "#{claim.first_name} #{claim.surname}",
-              "doB": "#{claim.date_of_birth}",
-              "niNumber": "#{claim.national_insurance_number}",
-              "qtsAwardDate": "2021-03-23T10:54:57.199Z",
-              "ittSubject1Code": "string",
-              "ittSubject2Code": "string",
-              "ittSubject3Code": "string",
-              "activeAlert": true
-            }
-          ],
-          "message": null
-        }
-      JSON
-    )
+      ),
+      body: {
+        data: [
+          {
+            trn: claim.teacher_reference_number,
+            name: "#{claim.first_name} #{claim.surname}",
+            doB: claim.date_of_birth,
+            niNumber: claim.national_insurance_number,
+            qtsAwardDate: "2021-03-23T10:54:57.199Z",
+            ittSubject1Code: "string",
+            ittSubject2Code: "string",
+            ittSubject3Code: "string",
+            activeAlert: true
+          }
+        ],
+        "message": "null"
+      }
+    }
+
+    args = overrides ? merge_recursively(defaults, overrides) : defaults
+
+    stub_request(:get, "#{ENV["DQT_CLIENT_HOST"]}:#{ENV["DQT_CLIENT_PORT"]}/api/qualified-teachers/qualified-teaching-status")
+      .with(query: args[:query])
+      .to_return(body: args[:body].to_json)
+  end
+
+  private
+
+  def merge_recursively(a, b)
+    a.merge!(b) do |key, a_item, b_item|
+      if a_item.is_a?(Hash)
+        merge_recursively(a_item, b_item)
+      else
+        b_item
+      end
+    end
   end
 end
