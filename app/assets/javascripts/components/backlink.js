9d18f8830ed6456e0bf51277fcabb3e9e0c0a07e
David Georgiou <david.georgiou@gmail.com>
2021-07-20 16:24:41 +0100

    [story/ECP-611] - ran prettier

--- app/assets/javascripts/components/backlink.js
+++ app/assets/javascripts/components/backlink.js
@@ -1,12 +1,12 @@
 "use strict";
 
 (function () {
-  var backlink = document.getElementById('backlink');
+  var backlink = document.getElementById("backlink");
   if (!backlink) return;
-  
+
   // unhide backlink and add listener
-  backlink.classList.remove('govuk-visually-hidden')
-  backlink.addEventListener('click', () => {
+  backlink.classList.remove("govuk-visually-hidden");
+  backlink.addEventListener("click", () => {
     history.back();
-  })
+  });
 })();
