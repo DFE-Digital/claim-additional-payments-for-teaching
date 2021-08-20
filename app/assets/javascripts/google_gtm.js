f688a4f0bcf1bbb348876361918506c982c69417
Katherine Martin <martikat.notifications@gmail.com>
2021-08-20 09:02:26 +0100

    [story/ECP-1061] - Adding Google Tag Manager for application and admin

--- app/assets/javascripts/google_gtm.js
+++ app/assets/javascripts/google_gtm.js
@@ -1,3 +1,5 @@
+"use strict";
+
 (function (w, d, s, l, i) {
   w[l] = w[l] || [];
   w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
--- app/assets/javascripts/google_gtm_iframe.js
+++
@@ -1,8 +0,0 @@
-<noscript>
-  <iframe
-    src="https://www.googletagmanager.com/ns.html?id=GTM-NL8QB4F"
-    height="0"
-    width="0"
-    style="display:none;visibility:hidden"
-  ></iframe>
-</noscript>;
