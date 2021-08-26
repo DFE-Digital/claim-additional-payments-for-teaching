09e33d5c69f45fd849e8ba7078e8ba9cde09f8f7
lucas.kelly <lucas.kelly@and.digital>
2021-08-26 08:56:21 +0100

    added restore script

---
+++ database_scripts/prod_data_restore.sql
@@ -0,0 +1,15 @@
+insert into public.notes
+select * 
+  from notes_bkp;
+
+insert into public.tasks
+select * 
+  from tasks_bkp;
+  
+insert into public.early_career_payments_eligibilities
+select * 
+  from early_career_payments_eligibilities_bkp;
+  
+insert into public.claims
+select * 
+  from claims_bkp;
