32ba76836ecc5785bfe40bb8c664ea318142749a
David Georgiou <david.georgiou@gmail.com>
2021-09-06 15:28:10 +0100

    [story/ECP-1070] - refactor specs

--- db/views/claim_decisions_v01.sql
+++ db/views/claim_decisions_v01.sql
@@ -5,75 +5,72 @@
 --
 -- Unfortunately subject is recorded differently in each table so some finesing
 -- is require around this.
-WITH
-	eligibilities
-AS
-  (
-		SELECT id,
-					current_school_id AS school_id,
-					CASE eligible_itt_subject
-									WHEN 0 THEN 'chemistry'
-									WHEN 1 THEN 'foreign languages'
-									WHEN 2 THEN 'maths'
-									WHEN 3 THEN 'physics'
-									WHEN 4 THEN 'none'
-					end AS subject
-		FROM   early_career_payments_eligibilities
-		UNION ALL
-		SELECT id,
-					current_school_id AS school_id,
-					CASE initial_teacher_training_subject
-									WHEN 0 THEN 'maths'
-									WHEN 1 THEN 'physics'
-									WHEN 2 THEN 'science'
-									WHEN 3 THEN 'none'
-					end AS subject
-		FROM   maths_and_physics_eligibilities
-		UNION ALL
-		SELECT id,
-					claim_school_id AS school_id,
-					CASE
-									WHEN biology_taught IS TRUE THEN 'biology'
-									WHEN chemistry_taught IS TRUE THEN 'chemistry'
-									WHEN computing_taught IS TRUE THEN 'computing'
-									WHEN languages_taught IS TRUE THEN 'languages'
-									WHEN physics_taught IS TRUE THEN 'physics'
-									ELSE 'none'
-					end AS subject
-		FROM   student_loans_eligibilities
-)
+WITH eligibilities AS
+(
+       SELECT id,
+              current_school_id AS school_id,
+              CASE eligible_itt_subject
+                     WHEN 0 THEN 'chemistry'
+                     WHEN 1 THEN 'foreign languages'
+                     WHEN 2 THEN 'maths'
+                     WHEN 3 THEN 'physics'
+                     WHEN 4 THEN 'none'
+              END AS subject
+       FROM   early_career_payments_eligibilities
+       UNION ALL
+       SELECT id,
+              current_school_id AS school_id,
+              CASE initial_teacher_training_subject
+                     WHEN 0 THEN 'maths'
+                     WHEN 1 THEN 'physics'
+                     WHEN 2 THEN 'science'
+                     WHEN 3 THEN 'none'
+              END AS subject
+       FROM   maths_and_physics_eligibilities
+       UNION ALL
+       SELECT id,
+              claim_school_id AS school_id,
+              CASE
+                     WHEN biology_taught IS true THEN 'biology'
+                     WHEN chemistry_taught IS true THEN 'chemistry'
+                     WHEN computing_taught IS true THEN 'computing'
+                     WHEN languages_taught IS true THEN 'languages'
+                     WHEN physics_taught IS true THEN 'physics'
+                     ELSE 'none'
+              END AS subject
+       FROM   student_loans_eligibilities )
 -- Build decsions table
 SELECT c.id                       AS application_id,
-				d.created_at               AS decision_date,
-				c.teacher_reference_number AS trn,
-				--  defined in models/decision.rb
-				CASE d.result
-							WHEN 0 THEN 'approved'
-							WHEN 1 THEN 'rejected'
-				end AS application_decision,
-				--  derived from eligibility class names
-				CASE c.eligibility_type
-							WHEN 'EarlyCareerPayments::Eligibility' THEN 'early career payments'
-							WHEN 'StudentLoans::Eligibility' THEN 'student loans'
-							WHEN 'MathsAndPhysics::Eligibility' THEN 'maths and physics'
-				end AS application_policy,
-				e.subject,
-				s.name                                              AS school_name,
-				la.name                                             AS local_authorities_name,
-				lad.name                                            AS local_authority_district_name,
-				date_part('year', age(now(), c.date_of_birth))::INT AS claimant_age,
-				CASE c.payroll_gender
-							WHEN 0 THEN 'don''t know'
-							WHEN 1 THEN 'female'
-							WHEN 2 THEN 'male'
-				end             AS claimant_gender,
-				c.academic_year AS claimant_year_qualified
+       d.created_at               AS decision_date,
+       c.teacher_reference_number AS trn,
+       -- defined in models/decision.rb
+       CASE d.result
+              WHEN 0 THEN 'approved'
+              WHEN 1 THEN 'rejected'
+       END AS application_decision,
+       -- derived from eligibility class names
+       CASE c.eligibility_type
+              WHEN 'EarlyCareerPayments::Eligibility' THEN 'early career payments'
+              WHEN 'StudentLoans::Eligibility' THEN 'student loans'
+              WHEN 'MathsAndPhysics::Eligibility' THEN 'maths and physics'
+       END AS application_policy,
+       e.subject,
+       s.NAME                                              AS school_name,
+       la.NAME                                             AS local_authorities_name,
+       lad.NAME                                            AS local_authority_district_name,
+       date_part('year', age(now(), c.date_of_birth))::int AS claimant_age,
+       CASE c.payroll_gender
+              WHEN 0 THEN 'don''t know'
+              WHEN 1 THEN 'female'
+              WHEN 2 THEN 'male'
+       END             AS claimant_gender,
+       c.academic_year AS claimant_year_qualified
 FROM   decisions d,
-				claims c,
-				schools s,
-				eligibilities e,
-				local_authorities la,
-				local_authority_districts lad
+       claims c,
+       schools s,
+       eligibilities e,
+       local_authorities la,
+       local_authority_districts lad
 WHERE  d.claim_id = c.id
 AND    c.eligibility_id = e.id
 AND    e.school_id = s.id
