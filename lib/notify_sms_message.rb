912935b4382e991e5ffba8f549a13469dacf20c1
Mark Sutton <work.sutton.notifications@gmail.com>
2021-09-20 19:02:21 +0100

    [story/ECP-887] - Add GOVUK Notify SMS capabilities for use with OTP

---
+++ lib/notify_sms_message.rb
@@ -0,0 +1,34 @@
+require 'notifications/client'
+
+class NotifySmsMessage
+  class NotifySmsError < StandardError; end
+
+  attr_reader :phone_number, :template_id, :personalisation
+
+  def initialize(phone_number:, template_id:, personalisation:)
+    @phone_number = phone_number
+    @template_id = template_id
+    @personalisation = personalisation
+  end
+
+  def deliver!
+    sms_client.send_sms(
+      phone_number: phone_number,
+      template_id: template_id,
+      personalisation: personalisation
+    )
+  rescue Notifications::Client::RequestError => e
+    Rails.logger.error(e.message)
+    raise NotifySmsError, e
+  end
+
+  private
+
+  def sms_client
+    @sms_client ||= Notifications::Client.new(api_key)
+  end
+
+  def api_key
+    ENV["NOTIFY_API_KEY"]
+  end
+end
