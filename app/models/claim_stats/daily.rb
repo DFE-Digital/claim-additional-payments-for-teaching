e4ba7b52fd9a323355c269e1d87c2739227ec0b9
David Georgiou <david.georgiou@gmail.com>
2021-09-21 11:53:51 +0100

    ECP-1253-1254 correct daily stats

--- app/models/claim_stats/daily.rb
+++ app/models/claim_stats/daily.rb
@@ -26,15 +26,15 @@ class ClaimStats
                 AS average_claim_decision_length,
               COUNT(claim_id)
                 AS applications_started_total,
-              COUNT(claim_id) filter (where claim_submitted_at is not null)
+              COUNT(claim_id) filter (where claim_submitted_at < '#{date.end_of_day}')
                 AS applications_submitted_total,
-              COUNT(claim_id) filter (where result = 'rejected')
+              COUNT(claim_id) filter (where result = 'rejected' and decision_made_at < '#{date.end_of_day}')
                 AS applications_rejected_total,
-              COUNT(claim_id) filter (where result = 'accepted')
+              COUNT(claim_id) filter (where result = 'accepted' and decision_made_at < '#{date.end_of_day}')
                 AS applications_accepted_total,
               COUNT(claim_id) filter (where claim_started_at::date = '#{date}')
                 AS applications_started_daily,
-              COUNT(claim_id) filter (where claim_submitted_at::date = '#{date}')
+              COUNT(claim_id) filter (where claim_started_at::date = '#{date}' and claim_submitted_at::date = '#{date}')
                 AS applications_submitted_daily,
               COUNT(claim_id) filter (where result = 'rejected' and decision_made_at::date = '#{date}')
                 AS applications_rejected_daily,
