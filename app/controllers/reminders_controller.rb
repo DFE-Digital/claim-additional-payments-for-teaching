dfbe9edb9068e170a57bc2b26165af1bd7512245
David Georgiou <david.georgiou@gmail.com>
2021-10-15 11:50:39 +0100

    [stroy/ECP-1240] - corrects reminder year

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -76,7 +76,11 @@ class RemindersController < BasePublicController
 
   def build_reminder_from_claim
     return unless current_claim
-    return if current_claim.eligibility.nqt_in_academic_year_after_itt == false && current_claim.has_ecp_policy?
+    if !current_claim.eligibility.trainee_teacher_in_2021? &&
+       current_claim.eligibility.nqt_in_academic_year_after_itt == false &&
+       current_claim.has_ecp_policy?
+      return
+    end
 
     Reminder.new(
       full_name: current_claim.full_name,
