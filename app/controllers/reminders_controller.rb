3f066957ee425bec8ed3b7fe18af79ba2a430263
David Georgiou <david.georgiou@gmail.com>
2021-10-15 14:05:18 +0100

    [stroy/ECP-1240] - standard:fix

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -77,8 +77,8 @@ class RemindersController < BasePublicController
   def build_reminder_from_claim
     return unless current_claim
     if !current_claim.eligibility.trainee_teacher_in_2021? &&
-       current_claim.eligibility.nqt_in_academic_year_after_itt == false &&
-       current_claim.has_ecp_policy?
+        current_claim.eligibility.nqt_in_academic_year_after_itt == false &&
+        current_claim.has_ecp_policy?
       return
     end
 
