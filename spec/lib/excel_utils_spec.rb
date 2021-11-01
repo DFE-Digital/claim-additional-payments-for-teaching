6661e5a87db69be431d638d1d9315cecca940876
Mark Sutton <work.sutton.notifications@gmail.com>
2021-11-01 17:09:40 +0000

    [task/dependabot-update] - Fix CI failure for standard

--- spec/lib/excel_utils_spec.rb
+++ spec/lib/excel_utils_spec.rb
@@ -4,10 +4,10 @@ require "excel_utils"
 RSpec.describe ExcelUtils do
   describe "escape_formulas" do
     it "escapes formula-triggering characters at the beginning of a string" do
-      expect(ExcelUtils.escape_formulas("=1+2")).to eq '\=1+2'
-      expect(ExcelUtils.escape_formulas("@reference")).to eq '\@reference'
-      expect(ExcelUtils.escape_formulas("-12 + 13")).to eq '\-12 + 13'
-      expect(ExcelUtils.escape_formulas("+12 + 13")).to eq '\+12 + 13'
+      expect(ExcelUtils.escape_formulas("=1+2")).to eq "\\=1+2"
+      expect(ExcelUtils.escape_formulas("@reference")).to eq "\\@reference"
+      expect(ExcelUtils.escape_formulas("-12 + 13")).to eq "\\-12 + 13"
+      expect(ExcelUtils.escape_formulas("+12 + 13")).to eq "\\+12 + 13"
     end
 
     it "doesn't escape non-formula-triggering characters" do
@@ -16,7 +16,7 @@ RSpec.describe ExcelUtils do
 
     it "doesn't escape characters in the body of a string" do
       expect(ExcelUtils.escape_formulas("some+address@email.com")).to eq "some+address@email.com"
-      expect(ExcelUtils.escape_formulas('\a-url?')).to eq '\a-url?'
+      expect(ExcelUtils.escape_formulas("\a-url?")).to eq "\a-url?"
     end
   end
 end
