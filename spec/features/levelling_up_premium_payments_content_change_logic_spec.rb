66e9693937cddb48807ffc37b802f5fec2bd2585
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2022-06-14 10:36:11 +0100

    Improve specs

--- spec/features/levelling_up_premium_payments_content_change_logic_spec.rb
+++ spec/features/levelling_up_premium_payments_content_change_logic_spec.rb
@@ -1,21 +1,28 @@
 require "rails_helper"
 
-RSpec.feature "Selecting 2017 for LUP-only claim" do
+RSpec.feature "Claims with different eligibilities content change logic" do
+  let(:claim) { start_early_career_payments_claim }
+
   before do
-    claim = start_early_career_payments_claim
     claim.update!(attributes_for(:claim, :submittable))
     claim.eligibility.update!(attributes_for(:early_career_payments_eligibility, :eligible))
   end
 
-  it "shows foreign languages only for 2017" do
+  it "shows the correct subjects for LUP-only and ECP claims" do
     visit claim_path(claim.policy.routing_name, "itt-year")
     choose "2017 to 2018"
     click_on "Continue"
-    expect(page).not_to have_text("Foreign Languages")
+    expected_subjects = ["Chemistry", "Computing", "Mathematics", "Physics", "None of the above"]
+    expect(radio_labels).to eq(expected_subjects)
 
     click_on "Back"
     choose "2018 to 2019"
     click_on "Continue"
-    expect(page).to have_text("Foreign languages")
+    expected_subjects = ["Chemistry", "Computing", "Foreign languages", "Mathematics", "Physics", "None of the above"]
+    expect(radio_labels).to eq(expected_subjects)
+  end
+
+  def radio_labels
+    all("label.govuk-radios__label").map(&:text)
   end
 end
