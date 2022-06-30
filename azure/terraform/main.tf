224baac7364b950c63e46295a1352903787f9330
welshman1970 <vito.caresimo@digital.education.gov.uk>
2022-06-30 14:51:22 +0100

    test

--- azure/terraform/main.tf
+++ azure/terraform/main.tf
@@ -5,23 +5,29 @@ module "env_vars" {
 }
 
 #container
-module "container" {
-  source                = "./modules/container"
-  app_rg_name           = format("%s-%s", module.env_vars.rg_prefix, "app")
-  projcore_network_prof = data.terraform_remote_state.infra.outputs.projcore_network_prof
-  container_version     = var.input_container_version
-  rg_prefix             = module.env_vars.rg_prefix
-  rg_location           = var.input_region
-  common_tags           = module.env_vars.common_tags
-}
+# module "container" {
+#   source                = "./modules/container"
+#   app_rg_name           = format("%s-%s", module.env_vars.rg_prefix, "app")
+#   projcore_network_prof = data.terraform_remote_state.infra.outputs.projcore_network_prof
+#   container_version     = var.input_container_version
+#   rg_prefix             = module.env_vars.rg_prefix
+#   rg_location           = var.input_region
+#   common_tags           = module.env_vars.common_tags
+# }
 
-module "app_service" {
-  source                  = "./modules/app_service"
-  app_rg_name             = format("%s-%s", module.env_vars.rg_prefix, "app")
-  app_asp_id              = data.terraform_remote_state.infra.outputs.app_asp_id
-  input_container_version = var.input_container_version
-  rg_prefix               = module.env_vars.rg_prefix
-  rg_location             = var.input_region
-  common_tags             = module.env_vars.common_tags
+# module "app_service" {
+#   source                  = "./modules/app_service"
+#   app_rg_name             = format("%s-%s", module.env_vars.rg_prefix, "app")
+#   app_asp_id              = data.terraform_remote_state.infra.outputs.app_asp_id
+#   input_container_version = var.input_container_version
+#   rg_prefix               = module.env_vars.rg_prefix
+#   rg_location             = var.input_region
+#   common_tags             = module.env_vars.common_tags
+# }
 
+module "resourcegroup" {
+  source                  = "./modules/resourcegoup"
+  name                 = format("%s-%s", module.env_vars.rg_prefix, "rg")
+  location             = var.input_region
+  common_tags          = module.env_vars.common_tags
 }
---
+++ azure/terraform/modules/resourcegroup/variable.tf
@@ -0,0 +1,4 @@
+variable "app_rg_name" {
+  type        = string
+  description = "Resource group for the application"
+}
