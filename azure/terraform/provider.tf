bd317c53f1457e8580b961f28e2efef7ea14cf45
welshman1970 <vito.caresimo@digital.education.gov.uk>
2022-07-14 11:51:14 +0100

    Commented out client_sectret var

--- azure/terraform/provider.tf
+++ azure/terraform/provider.tf
@@ -1,5 +1,5 @@
-variable "client_secret" {
-}
+# variable "client_secret" {
+# }
 
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
