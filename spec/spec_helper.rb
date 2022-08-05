74df3bbcf4ed01ac4fbe1b57166e452ea4dcd326
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2022-08-05 23:48:26 +0300

    Skip autocomplete tests on CI and add awesome_print gem (#2080)
    * Skip tests on CI and add awesome_print gem
    
    * Try with another CI ENV variable
    
    * Update flaky tests logic

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -112,4 +112,6 @@ RSpec.configure do |config|
   #   # test failures related to randomization by passing the same `--seed` value
   #   # as the one that triggered the failure.
   #   Kernel.srand config.seed
+
+  config.filter_run_excluding flaky: true unless ENV["RUN_FLAKY_SPECS"]
 end
