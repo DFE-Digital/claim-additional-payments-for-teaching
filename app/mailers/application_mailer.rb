3499154300981e982ea2e31f964b21df78d0e606
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2022-09-01 11:59:32 +0100

    Explicit require for notify

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,5 @@
+require "notify"
+
 class ApplicationMailer < Mail::Notify::Mailer
   NOTIFY_TEMPLATE_ID = "d72e2ff9-b228-4f16-9099-fd9d411c0334".freeze
   OTP_EMAIL_NOTIFY_TEMPLATE_ID = "89e8c33a-1863-4fdd-a73c-1ca01efc0c76".freeze
