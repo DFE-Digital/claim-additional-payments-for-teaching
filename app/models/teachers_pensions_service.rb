082b4bd221c8515f45490bfc1d69da294356a6da
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2022-09-08 13:35:43 +0100

    [CAPT-714] Skip duplicate rows for TCP upload (#2155)

--- app/models/teachers_pensions_service.rb
+++ app/models/teachers_pensions_service.rb
@@ -1,6 +1,8 @@
 class TeachersPensionsService < ApplicationRecord
   self.table_name = "teachers_pensions_service"
 
+  validates :teacher_reference_number, uniqueness: {scope: :start_date}
+
   scope :by_teacher_reference_number, ->(teacher_reference_number) { where(teacher_reference_number: teacher_reference_number) }
   scope :between_claim_dates, ->(start_date, end_date) { where(start_date: start_date..end_date) }
   scope :claim_dates_interval, ->(latest_start_date, earliest_end_date) { where(start_date: ..latest_start_date).or(where(end_date: earliest_end_date..)) }
