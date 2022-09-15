ea5b4618c30df09c3b26c1e66bc6ab8aed162a95
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2022-09-15 17:22:20 +0100

    [CAPT-614] Payment breakdown email bugfix

--- spec/models/levelling_up_premium_payments_spec.rb
+++ spec/models/levelling_up_premium_payments_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe LevellingUpPremiumPayments, type: :model do
     expect(subject).to have_attributes(routing_name: "additional-payments",
       short_name: "Levelling Up Premium Payments",
       locale_key: "levelling_up_premium_payments",
-      notify_reply_to_id: "3f85a1f7-9400-4b48-9a31-eaa643d6b977",
+      notify_reply_to_id: "03ece7eb-2a5b-461b-9c91-6630d0051aa6",
       eligibility_page_url: "https://www.gov.uk/guidance/levelling-up-premium-payments-for-teachers",
       eligibility_criteria_url: "https://www.gov.uk/guidance/levelling-up-premium-payments-for-teachers#eligibility-criteria-for-teachers")
   }
