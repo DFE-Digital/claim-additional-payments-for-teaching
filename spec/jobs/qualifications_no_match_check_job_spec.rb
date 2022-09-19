ba0cfdd834b52a56b64578616363c8bc7e741c83
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2022-09-19 13:06:23 +0100

    [CAPT-725] Add job to re-run the No Match qualifications checks (#2177)
    * [CAPT-725] Add job to re-run the No Match qualifications checks
    
    * Add specs

---
+++ spec/jobs/qualifications_no_match_check_job_spec.rb
@@ -0,0 +1,61 @@
+require "rails_helper"
+
+RSpec.describe QualificationsNoMatchCheckJob do
+  before do
+    stub_qualified_teaching_statuses_show(
+      trn: claim.teacher_reference_number,
+      params: {
+        birthdate: claim.date_of_birth&.to_s,
+        nino: claim.national_insurance_number
+      }
+    )
+
+    create(:task, claim: claim, name: "qualifications", claim_verifier_match: claim_verifier_match, manual: false)
+  end
+
+  let(:claim) { create(:claim, :submitted) }
+  let(:claim_verifier_match) { nil }
+
+  describe "#perform" do
+    context "when qualification task did not run" do
+      before do
+        claim.tasks.delete_all
+      end
+
+      it "does not re-run the task" do
+        expect { described_class.new.perform }
+          .to(not_change { claim.notes.count })
+
+        expect(Task.count).to eq(0)
+      end
+    end
+
+    context "when qualifications check returned no data" do
+      it "does not re-run the task" do
+        expect { described_class.new.perform }
+          .to not_change { claim.notes.count }
+          .and(not_change { claim.tasks.last.created_at })
+      end
+    end
+
+    context "when qualifications check returned no match" do
+      let(:claim_verifier_match) { :none }
+
+      it "re-runs the task" do
+        expect { described_class.new.perform }
+          .to change { claim.notes.count }.by(1)
+          .and(change { claim.tasks.last.created_at })
+      end
+    end
+
+    context "when qualifications check returned passed" do
+      let(:claim_verifier_match) { :all }
+
+      it "does not re-run the task" do
+        expect { described_class.new.perform }
+          .to not_change { claim.notes.count }
+          .and(not_change { claim.tasks.last.created_at })
+      end
+    end
+  end
+end
