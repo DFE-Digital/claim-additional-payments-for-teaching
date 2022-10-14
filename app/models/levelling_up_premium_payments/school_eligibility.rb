4989138524fef4d8819b64ba2c431aeff73d7220
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2022-10-14 16:33:39 +0100

    [CAPT-751] Fix remaining hard-coded academic year (#2231)
    * [CAPT-751] Fix remaining hard-coded academic year
    
    * Implement changes after review
    
    * Add dynamic itt_academic_year for ECP
    
    * Get year from policy configuration
    
    * Fix specs
    
    * Revert year change
    
    * Revert some PR changes

--- app/models/levelling_up_premium_payments/school_eligibility.rb
+++ app/models/levelling_up_premium_payments/school_eligibility.rb
@@ -12,7 +12,6 @@ module LevellingUpPremiumPayments
     end
 
     def eligible?
-      # TODO: use first year of LUP for now but this must come from a PolicyConfiguration
       LevellingUpPremiumPayments::Award.new(school: @school, year: AcademicYear.new(2022)).has_award?
     end
   end
