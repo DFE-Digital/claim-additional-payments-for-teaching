32aa2f8dc2f44dafa376b812ed663088733870aa
Steve Lorek <steve@stevelorek.com>
2022-10-25 15:48:20 +0100

    Rename Payroll status label

--- spec/features/admin_views_completed_claim_spec.rb
+++ spec/features/admin_views_completed_claim_spec.rb
@@ -31,7 +31,7 @@ RSpec.feature "Admin can view completed claims" do
 
     visit admin_claim_tasks_path(claim_with_payment)
 
-    expect(page).to have_content("Payroll status #{payroll_run_date}")
+    expect(page).to have_content("Status #{payroll_run_date}")
     expect(page).to have_link(payroll_run_date, href: admin_payroll_run_path(payroll_run))
   end
 
@@ -40,6 +40,6 @@ RSpec.feature "Admin can view completed claims" do
 
     visit admin_claim_tasks_path(claim_with_decision)
 
-    expect(page).to have_content("Payroll status Approved awaiting payroll")
+    expect(page).to have_content("Status Approved awaiting payroll")
   end
 end
