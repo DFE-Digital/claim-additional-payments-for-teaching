7f15e849e4cef2754e880ddce3c2cc69c5f3dc36
Steve Lorek <steve@stevelorek.com>
2022-11-11 15:07:15 +0000

    Fix award amounts

--- spec/models/levelling_up_premium_payments/award_spec.rb
+++ spec/models/levelling_up_premium_payments/award_spec.rb
@@ -41,7 +41,7 @@ RSpec.describe LevellingUpPremiumPayments::Award do
 
       # *attempt* to check if the hash has been altered
       specify { expect(subject.values).to all be_positive }
-      specify { expect(subject.values.sum).to eq(6_024_500) }
+      specify { expect(subject.values.sum).to eq(6_022_500) }
       specify { expect(subject.values.uniq).to contain_exactly(1_500, 2_000, 2_500, 3_000) }
       specify { expect(subject.count).to eq(2_767) }
       specify {
