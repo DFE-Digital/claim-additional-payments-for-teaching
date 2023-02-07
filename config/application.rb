a9611217db2d213366cb3dec3604a2acd29e4306
Steve Lorek <steve@stevelorek.com>
2023-02-07 14:51:32 +0000

    Install Rails 7 and upgrade all dependencies

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,11 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
+# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
 # require "action_cable/engine"
 require "sprockets/railtie"
@@ -36,6 +39,7 @@ module DfeTeachersPaymentService
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
+    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
