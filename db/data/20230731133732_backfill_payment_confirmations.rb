eb5b64ceb4bb427b65a6ebdbbaac3d938884ca8c
Giuseppe Lobraico <your@users.noreply.github.com>
2023-07-31 18:26:25 +0100

    Add backfilling data migration for payment confirmations
    Existing payroll runs store the `scheduled_payment_date`, and the user
    who uploaded the report in `confirmation_report_uploaded_by`.
    
    With this script, we are creating payment confirmations for hypothetical
    batches that may have been uploaded in the past, making sure that the
    `scheduled_payment_date` and the user who uploaded them are stored.
    
    Once this is completed, another migration can be created to remove
    the `scheduled_payment_date` and `confirmation_report_uploaded_by`
    from `payroll_runs`. DfE analytics config should be amended accordingly.

---
+++ db/data/20230731133732_backfill_payment_confirmations.rb
@@ -0,0 +1,18 @@
+# Run me with `rails runner db/data/20230731133732_backfill_payment_confirmations.rb`
+
+PayrollRun.where.not(confirmation_report_uploaded_by: nil).each do |payroll_run|
+  next if payroll_run.payment_confirmations.any?
+
+  puts "Backfilling payroll run #{payroll_run.id}..."
+  batches = payroll_run.payments.includes(:claims).in_batches(of: PayrollRun::MAX_BATCH_SIZE)
+  batches.each.with_index(1) do |payment_batch, index|
+    confirmation = PaymentConfirmation.create!(
+      payroll_run: payroll_run,
+      scheduled_payment_date: payroll_run.scheduled_payment_date,
+      created_by: payroll_run.confirmation_report_uploaded_by
+    )
+    payment_batch.update_all(confirmation_id: confirmation.reload.id)
+    puts "   Created confirmation #{index} of #{batches.count} for #{payment_batch.count} payments scheduled on #{payroll_run.scheduled_payment_date}"
+  end
+  puts
+end
