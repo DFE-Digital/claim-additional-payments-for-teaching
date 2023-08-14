baeed21b0054e8d8291e264ded71e9daa11e8fa3
Giuseppe Lobraico <your@users.noreply.github.com>
2023-08-14 11:32:36 +0100

    Add induction_completed to the list of editable attributes

--- app/models/early_career_payments/eligibility.rb
+++ app/models/early_career_payments/eligibility.rb
@@ -5,6 +5,7 @@ module EarlyCareerPayments
     EDITABLE_ATTRIBUTES = [
       :nqt_in_academic_year_after_itt,
       :current_school_id,
+      :induction_completed,
       :employed_as_supply_teacher,
       :has_entire_term_contract,
       :employed_directly,
