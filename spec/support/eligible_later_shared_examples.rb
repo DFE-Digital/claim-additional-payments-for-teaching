4da17e09dd2b9b4b9c1051741f0026d1d241a611
Giuseppe Lobraico <your@users.noreply.github.com>
2023-08-25 17:24:39 +0100

    Add test for eligible later screen when induction not completed
    This scenario is applicable to ECP-only claims.

--- spec/support/eligible_later_shared_examples.rb
+++ spec/support/eligible_later_shared_examples.rb
@@ -4,22 +4,12 @@ RSpec.shared_examples "Eligible later" do |opts|
     let(:next_eligible_year) { opts[:next_eligible_year] }
     let(:policy_year) { opts[:policy_year] }
     let(:qualification) { opts[:qualification] }
-
-    before do
-      create(:policy_configuration, :additional_payments, current_academic_year: policy_year)
-
-      eligibility_attrs = attributes_for(:early_career_payments_eligibility, :eligible, qualification: qualification)
-      eligibility_attrs[:current_school] = current_school
-      claim.eligibility.update!(eligibility_attrs)
-      lup_claim.eligibility.update!(eligibility_attrs)
-    end
+    let(:eligibility_attrs) { attributes_for(:early_career_payments_eligibility, :eligible, qualification: qualification).merge(current_school:) }
+    let!(:policy_configuration) { create(:policy_configuration, :additional_payments, current_academic_year: policy_year) }
 
     scenario "with ITT subject mathematics in ITT academic year #{opts[:itt_academic_year]} with a #{opts[:qualification]} qualification" do
-      claim.eligibility.update(
-        eligible_itt_subject: itt_subject,
-        itt_academic_year: itt_academic_year
-      )
-      lup_claim.eligibility.update(
+      claim.eligibility.update!(
+        **eligibility_attrs,
         eligible_itt_subject: itt_subject,
         itt_academic_year: itt_academic_year
       )
@@ -57,6 +47,33 @@ RSpec.shared_examples "Eligible later" do |opts|
 
       expect(page).to have_text("You may be eligible next year")
       expect(page).to have_text("So long as your circumstances stay the same, you could claim for an early-career payment in the #{next_eligible_year.to_s(:long)} academic year.")
+
+      expect(page).to have_text("Set a reminder to apply next year")
+    end
+
+    scenario "when induction was not completed" do
+      start_early_career_payments_claim
+
+      # - Which school do you teach at
+      expect(page).to have_text(I18n.t("early_career_payments.questions.current_school_search"))
+
+      choose_school current_school
+
+      # - Have you started your first year as a newly qualified teacher?
+      expect(page).to have_text(I18n.t("early_career_payments.questions.nqt_in_academic_year_after_itt.heading"))
+
+      choose "Yes"
+      click_on "Continue"
+
+      # - Have you completed your induction as an early-career teacher?
+      expect(page).to have_text(I18n.t("early_career_payments.questions.induction_completed.heading"))
+
+      choose "No"
+      click_on "Continue"
+
+      expect(page).to have_text("You are not eligible for the early-career payment (opens in new tab) because you have not completed your induction.")
+      expect(page).to have_text("You may be eligible next year")
+      expect(page).to have_text("If you have completed your induction, you could claim for an early-career payment in the #{next_eligible_year.to_s(:long)} academic year.")
 
       expect(page).to have_text("Set a reminder to apply next year")
     end
