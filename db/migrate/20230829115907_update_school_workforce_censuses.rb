273eadc3357adcfc5f645242ba9cdebd4c769059
martin <martinferndfe@gmail.com>
2023-09-21 11:28:08 +0100

    Fixed deployment issue of schema

--- db/migrate/20230829115907_update_school_workforce_censuses.rb
+++ db/migrate/20230829115907_update_school_workforce_censuses.rb
@@ -1,18 +1,42 @@
 class UpdateSchoolWorkforceCensuses < ActiveRecord::Migration[7.0]
   def change
+    add_columns_if_not_exist(
+      :school_workforce_censuses,
+      {
+        school_urn: :integer,
+        contract_agreement_type: :string,
+        totfte: :integer,
+        subject_description_sfr: :string,
+        general_subject_code: :string,
+        hours_taught: :integer
+      }
+    )
+
     (1..15).each do |n|
-      remove_column :school_workforce_censuses, "subject_#{n}", :string
+      add_column_if_not_exist(
+        :school_workforce_censuses,
+        "subject_#{n}".to_sym,
+        :string
+      )
     end
 
-    # Add new columns
-    add_column :school_workforce_censuses, :school_urn, :integer
-    add_column :school_workforce_censuses, :contract_agreement_type, :string
-    add_column :school_workforce_censuses, :totfte, :integer
-    add_column :school_workforce_censuses, :subject_description_sfr, :string
-    add_column :school_workforce_censuses, :general_subject_code, :string
-    add_column :school_workforce_censuses, :hours_taught, :integer
+    add_index_if_not_exist(
+      :school_workforce_censuses,
+      :teacher_reference_number
+    )
+  end
 
-    # Add index
-    add_index :school_workforce_censuses, :teacher_reference_number
+  private
+
+  def add_column_if_not_exist(table, column, type)
+    add_column table, column, type unless column_exists?(table, column)
+  end
+
+  def add_columns_if_not_exist(table, columns)
+    columns.each { |column, type| add_column_if_not_exist(table, column, type) }
+  end
+
+  def add_index_if_not_exist(table, column)
+    add_index table, column unless index_exists?(table, column)
   end
 end
