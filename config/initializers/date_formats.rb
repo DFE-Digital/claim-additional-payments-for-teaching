0e88867151b1eaf54f0af952182a1175439c740f
Giuseppe Lobraico <your@users.noreply.github.com>
2023-10-04 14:48:42 +0100

    Include day of the week in custom ordinal date format

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,1 +1,1 @@
-DateTime::DATE_FORMATS[:custom_ordinal] = lambda { |time| time.strftime("#{time.day.ordinalize} %B %Y") }
+DateTime::DATE_FORMATS[:custom_ordinal] = lambda { |time| time.strftime("%A #{time.day.ordinalize} %B %Y") }
