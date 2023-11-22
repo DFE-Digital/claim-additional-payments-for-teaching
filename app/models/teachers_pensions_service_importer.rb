9fe14c2c3d0df7a975458bc2b37e183c1abcca49
Giuseppe Lobraico <your@users.noreply.github.com>
2023-11-22 16:13:26 +0000

    Refactor existing data importers using `CsvImporter::Base`

--- app/models/teachers_pensions_service_importer.rb
+++ app/models/teachers_pensions_service_importer.rb
@@ -1,47 +1,28 @@
-require "file_download"
-require "csv"
-
-class TeachersPensionsServiceImporter
-  attr_reader :errors, :rows
-
-  EXPECTED_HEADERS = [
-    "Teacher reference number",
-    "NINO",
-    "Start Date",
-    "End Date",
-    "Employer ID",
-    "LA URN",
-    "School URN"
-  ].freeze
-
-  def initialize(file)
-    @errors = []
-    @rows = parse_csv(file)
-    check_headers
-  end
-
-  # NOTE: Duplicate trn with same start_dates are skipped
-  def run
-    batch = 1
-    rows.each_slice(2000) do |batch_rows|
-      Rails.logger.info "Processing TPS upload batch #{batch}"
-
-      record_hashes = batch_rows.map do |row|
-        next if row.fetch("Teacher reference number").blank?
-
-        row_to_tps_hash(row)
-      end.compact
-
-      TeachersPensionsService.insert_all(record_hashes) unless record_hashes.empty?
-
-      batch += 1
-    end
-  end
+class TeachersPensionsServiceImporter < CsvImporter::Base
+  import_options(
+    target_data_model: TeachersPensionsService,
+    append_only: true, # Note: the table is never purged
+    batch_size: 2000,
+    transform_rows_with: :row_to_hash,
+    skip_row_if: :skip_row_conditions?,
+    mandatory_headers: [
+      "Teacher reference number",
+      "NINO",
+      "Start Date",
+      "End Date",
+      "Employer ID",
+      "LA URN",
+      "School URN"
+    ]
+  )
 
   private
 
-  # NOTE: since there will be lots of rows, avoid instantiating model object
-  def row_to_tps_hash(row)
+  def skip_row_conditions?(row)
+    row.fetch("Teacher reference number").blank?
+  end
+
+  def row_to_hash(row)
     now = Time.now.utc
     trn = row.fetch("Teacher reference number")
 
@@ -57,25 +38,6 @@ class TeachersPensionsServiceImporter
       created_at: now,
       updated_at: now
     }
-  end
-
-  def check_headers
-    return unless rows
-
-    missing_headers = EXPECTED_HEADERS - rows.headers
-    errors.append("The selected file is missing some expected columns: #{missing_headers.join(", ")}") if missing_headers.any?
-  end
-
-  def parse_csv(file)
-    if file.nil?
-      errors.append("Select a file")
-      nil
-    else
-      CSV.read(file.to_io, headers: true, encoding: "BOM|UTF-8")
-    end
-  rescue CSV::MalformedCSVError
-    errors.append("The selected file must be a CSV")
-    nil
   end
 
   # First 7 digits
