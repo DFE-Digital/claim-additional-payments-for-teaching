5e0934eb5890d7e58c22eb026c3adf653514e429
Giuseppe Lobraico <your@users.noreply.github.com>
2023-12-07 11:07:19 +0000

    Add methods to retrieve all plan types and total amount from SLC data

--- app/models/student_loans_data.rb
+++ app/models/student_loans_data.rb
@@ -1,3 +1,13 @@
 class StudentLoansData < ApplicationRecord
-  scope :by_nino, ->(nino) { where(nino: nino) }
+  scope :by_nino, ->(nino) do
+    where(nino: nino).extending do
+      def repaying_plan_types
+        pluck(:plan_type_of_deduction).uniq.sort.join("_and_").presence&.prepend("plan_")
+      end
+
+      def total_repayment_amount
+        sum(:amount)
+      end
+    end
+  end
 end
