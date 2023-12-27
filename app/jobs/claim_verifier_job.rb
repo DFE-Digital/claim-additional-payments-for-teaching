dfeac94738ee3682c32de0bc64f93b8ff7109eda
Steve Lorek <steve@stevelorek.com>
2023-12-27 20:41:27 +0000

    Don't call DQT again if the data is already saved on the Claim

--- app/jobs/claim_verifier_job.rb
+++ app/jobs/claim_verifier_job.rb
@@ -2,7 +2,7 @@ class ClaimVerifierJob < ApplicationJob
   def perform(claim)
     AutomatedChecks::ClaimVerifier.new(
       claim:,
-      dqt_teacher_status: Dqt::Client.new.teacher.find(
+      dqt_teacher_status: claim.has_dqt_record? ? Dqt::Teacher.new(claim.dqt_teacher_status) : Dqt::Client.new.teacher.find(
         claim.teacher_reference_number,
         birthdate: claim.date_of_birth.to_s,
         nino: claim.national_insurance_number
---
+++ spec/jobs/claim_verifier_job_spec.rb
@@ -0,0 +1,57 @@
+require "rails_helper"
+
+RSpec.describe ClaimVerifierJob do
+  describe "#perform" do
+    let(:claim) { build(:claim, dqt_teacher_status:) }
+    let(:dbl) { double(find: mock_payload) }
+    let(:verifier) { double(perform: true) }
+    let(:mock_payload) { Dqt::Teacher.new({ "mock" => "mock" }) }
+
+    before do
+      allow(Dqt::TeacherResource).to receive(:new).and_return(dbl)
+      allow(AutomatedChecks::ClaimVerifier).to receive(:new).and_return(verifier)
+    end
+
+    context "when the claim has a DQT record payload" do
+      let(:dqt_teacher_status) { { "test" => "test" } }
+
+      it "does not request a new DQT payload" do
+        expect(dbl).not_to receive(:find)
+        described_class.new.perform(claim)
+      end
+
+      it "performs the verifier job" do
+        expect(AutomatedChecks::ClaimVerifier).to receive(:new).with(claim:, dqt_teacher_status: Dqt::Teacher.new(dqt_teacher_status))
+        described_class.new.perform(claim)
+      end
+    end
+
+    context "when the claim has an empty DQT record payload" do
+      let(:dqt_teacher_status) { {} }
+
+      it "does requests a new DQT payload" do
+        expect(dbl).to receive(:find)
+        described_class.new.perform(claim)
+      end
+
+      it "performs the verifier job" do
+        expect(AutomatedChecks::ClaimVerifier).to receive(:new).with(claim:, dqt_teacher_status: mock_payload)
+        described_class.new.perform(claim)
+      end
+    end
+
+    context "when the claim does not have a DQT record payload" do
+      let(:dqt_teacher_status) { nil }
+
+      it "does requests a new DQT payload" do
+        expect(dbl).to receive(:find)
+        described_class.new.perform(claim)
+      end
+
+      it "performs the verifier job" do
+        expect(AutomatedChecks::ClaimVerifier).to receive(:new).with(claim:, dqt_teacher_status: mock_payload)
+        described_class.new.perform(claim)
+      end
+    end
+  end
+end
