8741dc427a7b7b62158b8da54f5513e375269b1b
Serafeim Maroulis <serafeim@hey.com>
2024-01-22 11:48:47 +0000

    Index PolicyConfigurations#policy_types
    policy_types are been querried multiple times through
    
    `PolicyConfigurations::for`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_24_180838) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_22_114800) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "pgcrypto"
@@ -308,6 +308,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_24_180838) do
     t.text "policy_types", default: [], array: true
     t.boolean "teacher_id_enabled", default: true
     t.index ["created_at"], name: "index_policy_configurations_on_created_at"
+    t.index ["policy_types"], name: "index_policy_configurations_on_policy_types"
   end
 
   create_table "reminders", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
