b54c0f07c2884a97710e82e123b93fb54ddddbec
Serafeim Maroulis <serafeim@hey.com>
2024-01-25 09:53:16 +0000

    Refactor Admin feature specs
    * Remove loops to Policies
    * Combine scenarios

--- spec/features/admin_search_spec.rb
+++ spec/features/admin_search_spec.rb
@@ -1,5 +1,51 @@
 require "rails_helper"
 
 RSpec.feature "Admin search" do
-  Policies.all.each { |policy| it_behaves_like "Admin Search Feature", policy }
+  before do
+    create(:policy_configuration)
+    sign_in_as_service_operator
+  end
+
+  let!(:claim1) { create(:claim, :submitted, surname: "Wayne") }
+  let!(:claim2) { create(:claim, :submitted, surname: "Wayne") }
+  let!(:unsubmitted_claim) { create(:claim, surname: "Smith") }
+
+  scenario "redirects a service operator to the claim if there is only one match" do
+    visit search_admin_claims_path
+
+    fill_in :reference, with: claim1.reference
+    click_button "Search"
+
+    expect(page).to have_content(claim1.reference)
+    expect(page).to have_no_content(claim2.reference)
+
+    expect(page).to have_content(claim1.teacher_reference_number)
+    expect(page).to have_content(claim1.policy.short_name)
+  end
+
+  scenario "it lists claims if there is more than one match" do
+    visit search_admin_claims_path
+
+    fill_in :reference, with: "wayne"
+    click_button "Search"
+
+    expect(page).to have_content(claim1.reference)
+    expect(page).to have_content(claim2.reference)
+
+    find("a[href='#{admin_claim_tasks_path(claim1)}']").click
+
+    expect(page).to have_content(claim1.reference)
+    expect(page).to have_no_content(claim2.reference)
+    expect(page).to have_content(claim1.teacher_reference_number)
+    expect(page).to have_content(claim1.policy.short_name)
+  end
+
+  scenario "unsubmitted claims filtered from search results" do
+    visit search_admin_claims_path
+
+    fill_in :reference, with: "Smith"
+    click_button "Search"
+
+    expect(page).to have_content("Cannot find a claim for query \"Smith\"")
+  end
 end
