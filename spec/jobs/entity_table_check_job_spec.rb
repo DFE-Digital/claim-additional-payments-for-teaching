7fe42ce77a76a7482cd2a69aa67c832b477e65a2
Erica Porter <ericaporter@users.noreply.github.com>
2024-01-30 10:36:36 +0000

    Update test

--- spec/jobs/entity_table_check_job_spec.rb
+++ spec/jobs/entity_table_check_job_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "EntityTableCheckJob" do
+RSpec.describe DfE::Analytics::EntityTableCheckJob do
   describe "#perform" do
     it "runs the entity table check job" do
       expect_any_instance_of(DfE::Analytics::EntityTableCheckJob).to receive(:perform)
