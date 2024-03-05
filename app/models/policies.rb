90921095c1d1e702328ac2e18bd53fe9b2dbf15a
Serafeim Maroulis <serafeim@hey.com>
2024-03-05 14:05:50 +0000

    Namespace StudentLoans to Policies::StudentLoans

--- app/models/policies.rb
+++ app/models/policies.rb
@@ -32,7 +32,7 @@ module Policies
   # Map PolicyConfiguration.policy_types and Claim.policy_options_provided to the Policies namespace
   #
   def self.constantize(policy)
-    if %w[EarlyCareerPayments].include?(policy)
+    if %w[StudentLoans EarlyCareerPayments].include?(policy)
       "Policies::#{policy}"
     else
       policy
