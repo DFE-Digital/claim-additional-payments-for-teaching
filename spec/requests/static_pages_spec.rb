55a0fd546edbdf2272995a0a7d61848a6c9ff7d7
Giuseppe Lobraico <your@users.noreply.github.com>
2024-03-15 16:06:24 +0000

    Replace Claim privacy notice with external link

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "Static pages", type: :request do
-  %i[privacy_notice terms_conditions cookies accessibility_statement contact_us].each do |static_page|
+  %i[terms_conditions cookies accessibility_statement contact_us].each do |static_page|
     it "renders the #{static_page} page for the student loans policy" do
       get public_send(:"#{static_page}_path", Journeys::TeacherStudentLoanReimbursement::ROUTING_NAME)
 
