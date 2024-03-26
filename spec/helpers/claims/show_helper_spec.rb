1e9f9d5abf0e2cbb0735aa9b4b12fb94a90a8a7e
Steve Lorek <steve@stevelorek.com>
2024-03-26 18:16:38 +0000

    Separate submission page template by journey

--- spec/helpers/claims/show_helper_spec.rb
+++ spec/helpers/claims/show_helper_spec.rb
@@ -3,32 +3,6 @@ require "rails_helper"
 RSpec.describe Claims::ShowHelper do
   let(:claim) { build(:claim, policy: policy) }
 
-  describe "#claim_submitted_title(claim)" do
-    context "with a StudentLoans policy" do
-      let(:policy) { Policies::StudentLoans }
-
-      it "returns the correct content block" do
-        expect(helper.claim_submitted_title(claim)).to include("Claim submitted")
-      end
-    end
-
-    context "with a EarlyCareerPayments policy" do
-      let(:policy) { Policies::EarlyCareerPayments }
-
-      it "returns the correct content block" do
-        expect(helper.claim_submitted_title(claim)).to include("You applied for an early-career payment")
-      end
-    end
-
-    context "with a LevellingUpPremiumPayments policy" do
-      let(:policy) { Policies::LevellingUpPremiumPayments }
-
-      it "returns the correct content block" do
-        expect(helper.claim_submitted_title(claim)).to include("You applied for a levelling up premium payment")
-      end
-    end
-  end
-
   describe "#shared_view_css_class_size(claim)" do
     context "with a StudentLoans policy" do
       let(:policy) { Policies::StudentLoans }
