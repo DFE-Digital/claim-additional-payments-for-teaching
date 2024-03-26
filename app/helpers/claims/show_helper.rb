76c2b720668a18ceba93c9908bc88114f27cc6fd
Steve Lorek <steve@stevelorek.com>
2024-03-26 18:16:38 +0000

    Separate submission page template by journey

--- app/helpers/claims/show_helper.rb
+++ app/helpers/claims/show_helper.rb
@@ -1,17 +1,5 @@
 module Claims
   module ShowHelper
-    def claim_submitted_title(claim)
-      if claim.has_ecp_or_lupp_policy?
-        text = "You applied for "
-        text << "an early-career payment" if claim.has_ecp_policy?
-        text << "a levelling up premium payment" if claim.has_lupp_policy?
-
-        content_tag(:h1, text, class: "govuk-panel__title", id: "submitted-title")
-      else
-        content_tag(:h1, "Claim submitted", class: "govuk-panel__title", id: "submitted-title")
-      end
-    end
-
     def shared_view_css_class_size(claim)
       claim.has_ecp_or_lupp_policy? ? "l" : "xl"
     end
