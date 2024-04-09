64787398fbe077413718dd1406cb567abbe16d5a
vacabor <166112501+vacabor@users.noreply.github.com>
2024-04-09 19:22:57 +0100

    Implement PR feedback

--- app/forms/induction_completed_form.rb
+++ app/forms/induction_completed_form.rb
@@ -3,9 +3,9 @@
 class InductionCompletedForm < Form
   attribute :induction_completed
 
-  validates :induction_completed, presence: { message: lambda { |object, _data|
-                                                         object.i18n_errors_path('select_yes_if_completed')
-                                                       } }
+  validates :induction_completed, presence: {message: lambda { |object, _data|
+                                                        object.i18n_errors_path("select_yes_if_completed")
+                                                      }}
 
   def initialize(claim:, journey:, params:)
     super
@@ -16,13 +16,13 @@ class InductionCompletedForm < Form
   def save
     return false unless valid?
 
-    update!({ 'eligibility_attributes' => { 'induction_completed' => induction_completed } })
+    update!({"eligibility_attributes" => {"induction_completed" => induction_completed}})
   end
 
   private
 
   def i18n_form_namespace
-    'induction_completed'
+    "induction_completed"
   end
 
   def permitted_params
