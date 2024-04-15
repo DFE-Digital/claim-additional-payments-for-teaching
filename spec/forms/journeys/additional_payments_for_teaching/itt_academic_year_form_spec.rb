51e020ec18fe27c9665782bbc28689415eafdb71
vacabor <166112501+vacabor@users.noreply.github.com>
2024-04-15 10:41:43 +0100

    Supply valid academic years to spec

--- spec/forms/journeys/additional_payments_for_teaching/itt_academic_year_form_spec.rb
+++ spec/forms/journeys/additional_payments_for_teaching/itt_academic_year_form_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Journeys::AdditionalPaymentsForTeaching::IttAcademicYearForm do
     }
 
     let(:current_claim) do
-      claims = journey::POLICIES.map { |policy| create(:claim, policy: policy, eligibility_attributes: {qualification: :postgraduate_itt}) }
+      claims = journey::POLICIES.map { |policy| create(:claim, policy: policy, eligibility_attributes: {qualification: "postgraduate_itt"}) }
       CurrentClaim.new(claims: claims)
     end
 
@@ -43,7 +43,7 @@ RSpec.describe Journeys::AdditionalPaymentsForTeaching::IttAcademicYearForm do
       end
 
       context "claim eligibility DOES have a current value" do
-        let(:current_value) { AcademicYear.new(2.years.ago.year) }
+        let(:current_value) { AcademicYear.new(4.years.ago.year) }
 
         let(:current_claim) do
           claims = journey::POLICIES.map { |policy| create(:claim, policy: policy, eligibility_attributes: {itt_academic_year: current_value}) }
@@ -51,14 +51,14 @@ RSpec.describe Journeys::AdditionalPaymentsForTeaching::IttAcademicYearForm do
         end
 
         it "returns the current value" do
-          expect(form.itt_academic_year).to be current_value
+          expect(form.itt_academic_year).to eq current_value
         end
       end
     end
 
     describe "#save" do
       context "itt_academic_year submitted" do
-        let(:new_value) { AcademicYear.new(1.year.ago.year) }
+        let(:new_value) { AcademicYear.new(2.years.ago.year) }
 
         let(:params) { ActionController::Parameters.new({slug: slug, claim: {itt_academic_year: new_value}}) }
 
@@ -74,16 +74,16 @@ RSpec.describe Journeys::AdditionalPaymentsForTeaching::IttAcademicYearForm do
             current_claim.claims.each do |claim|
               eligibility = claim.eligibility.reload
 
-              expect(eligibility.itt_academic_year).to be new_value
+              expect(eligibility.itt_academic_year).to eq new_value
             end
           end
         end
 
         context "claim eligibility already had a itt_academic_year" do
-          let(:new_value) { AcademicYear.new(1.year.ago.year) }
+          let(:new_value) { AcademicYear.new(2.years.ago.year) }
 
           let(:current_claim) do
-            claims = journey::POLICIES.map { |policy| create(:claim, policy: policy, eligibility_attributes: {itt_academic_year: AcademicYear.new(2.years.ago.year)}) }
+            claims = journey::POLICIES.map { |policy| create(:claim, policy: policy, eligibility_attributes: {itt_academic_year: AcademicYear.new(4.years.ago.year)}) }
             CurrentClaim.new(claims: claims)
           end
 
@@ -93,7 +93,7 @@ RSpec.describe Journeys::AdditionalPaymentsForTeaching::IttAcademicYearForm do
             current_claim.claims.each do |claim|
               eligibility = claim.eligibility.reload
 
-              expect(eligibility.itt_academic_year).to be new_value
+              expect(eligibility.itt_academic_year).to eq new_value
             end
           end
         end
@@ -114,6 +114,26 @@ RSpec.describe Journeys::AdditionalPaymentsForTeaching::IttAcademicYearForm do
           expect(form.save).to be false
           expect(form.errors[:itt_academic_year]).to eq ["Select the academic year you started your postgraduate ITT"]
         end
+      end
+    end
+
+    describe "#qualification_is?" do
+      let(:params) { ActionController::Parameters.new({slug: slug}) }
+
+      it "returns true for a single argument match" do
+        expect(form.qualification_is?(:postgraduate_itt)).to be true
+      end
+
+      it "returns false for a single argument miss" do
+        expect(form.qualification_is?(:undergraduate_itt)).to be false
+      end
+
+      it "returns true for an argument match" do
+        expect(form.qualification_is?(:undergraduate_itt, :postgraduate_itt, :other)).to be true
+      end
+
+      it "returns false for an argument miss" do
+        expect(form.qualification_is?(:undergraduate_itt, :assessment_only, :other)).to be false
       end
     end
   end
