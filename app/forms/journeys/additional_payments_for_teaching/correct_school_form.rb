67ae4d18c3d37408c2a6f842ec005c2756e04a87
Felix Clack <felixclack@gmail.com>
2024-04-19 15:41:34 +0100

    Add a form object for choosing the correct school
    When using DfE Sign In, we present the school currently associated with
    the user to check if it is still the correct school.
    
    Currently, we retrieve the associated TPS school, store it in the
    session so that we can display it in the view. Then on confirmation from
    the user, this value gets persisted as the current school.
    
    The form object allows us to extract this logic and it can define these
    references, ensuring that the controller is simpler.
    
    As far as I can tell, the session itself is unnecessary as the values
    stored there are already persisted in the DB.

---
+++ app/forms/journeys/additional_payments_for_teaching/correct_school_form.rb
@@ -0,0 +1,38 @@
+module Journeys
+  module AdditionalPaymentsForTeaching
+    class CorrectSchoolForm < Form
+      attribute :change_school, :boolean
+      attribute :current_school_id, :string
+
+      delegate :address, :name, to: :current_school, prefix: true, allow_nil: true
+      delegate :eligibility, to: :claim
+      delegate :school_somewhere_else?, to: :eligibility
+
+      def save
+        claim.update(eligibility_attributes:)
+        claim.reset_eligibility_dependent_answers(["current_school_id"])
+        true
+      end
+
+      def current_school_id
+        @current_school_id ||= permitted_params.fetch(:current_school_id, current_school&.id)
+      end
+
+      private
+
+      def current_school
+        @current_school ||= claim.recent_tps_school || claim.school
+      end
+
+      def change_school?
+        change_school || current_school_id.nil? || current_school_id == "somewhere_else"
+      end
+
+      def eligibility_attributes
+        return {current_school_id: nil, school_somewhere_else: true} if change_school?
+
+        {current_school_id:, school_somewhere_else: false}
+      end
+    end
+  end
+end
