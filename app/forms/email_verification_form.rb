5ba8e23d4146c84aac1d5b3e0d5b1f0f99fd7371
Giuseppe Lobraico <your@users.noreply.github.com>
2024-05-13 02:35:24 +0100

    Validate OTP code generation timestamp

--- app/forms/email_verification_form.rb
+++ app/forms/email_verification_form.rb
@@ -4,7 +4,8 @@ class EmailVerificationForm < Form
   # Required for shared partial in the view
   delegate :email_address, to: :claim
 
-  validate :otp_validate
+  validate :sent_one_time_password_must_be_valid
+  validate :otp_must_be_valid, if: :sent_one_time_password_at?
 
   before_validation do
     self.one_time_password = one_time_password.gsub(/\D/, "")
@@ -22,12 +23,24 @@ class EmailVerificationForm < Form
     claim.sent_one_time_password_at
   end
 
-  def otp_validate
+  def sent_one_time_password_must_be_valid
+    return if sent_one_time_password_at?
+
+    errors.add(:one_time_password, i18n_errors_path(:"one_time_password.invalid"))
+  end
+
+  def otp_must_be_valid
     otp = OneTimePassword::Validator.new(
       one_time_password,
       sent_one_time_password_at
     )
 
     errors.add(:one_time_password, otp.warning) unless otp.valid?
+  end
+
+  def sent_one_time_password_at?
+    sent_one_time_password_at&.to_datetime || false
+  rescue Date::Error
+    false
   end
 end
