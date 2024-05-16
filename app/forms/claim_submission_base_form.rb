ca0df27e03f54ac66ed5c3085441c05b63b2bba8
Richard Lynch <rjlynchdev@gmail.com>
2024-05-16 22:04:01 +0100

    Refactor form to take advantage of attributes
    Now we're working with typed attributes on the journey session we can
    simplify the claim submission forms.

--- app/forms/claim_submission_base_form.rb
+++ app/forms/claim_submission_base_form.rb
@@ -1,70 +1,15 @@
 class ClaimSubmissionBaseForm
   include ActiveModel::Model
-  include ActiveModel::Attributes
-
-  attribute :selected_policy, :string
-
-  CLAIM_ATTRIBUTES = [
-    [:address_line_1, :string],
-    [:address_line_2, :string],
-    [:address_line_3, :string],
-    [:address_line_4, :string],
-    [:postcode, :string],
-    [:date_of_birth, :date],
-    [:teacher_reference_number, :string],
-    [:national_insurance_number, :string],
-    [:email_address, :string],
-    [:bank_sort_code, :string],
-    [:bank_account_number, :string],
-    [:details_check, :boolean],
-    [:payroll_gender, :string],
-    [:first_name, :string],
-    [:middle_name, :string],
-    [:surname, :string],
-    [:banking_name, :string],
-    [:building_society_roll_number, :string],
-    [:academic_year, AcademicYear::Type.new],
-    [:bank_or_building_society, :integer],
-    [:provide_mobile_number, :boolean],
-    [:mobile_number, :string],
-    [:email_verified, :boolean],
-    [:mobile_verified, :boolean],
-    [:hmrc_bank_validation_succeeded, :boolean],
-    [:hmrc_bank_validation_responses, nil], # , :json
-    [:logged_in_with_tid, :boolean],
-    [:teacher_id_user_info, nil], # :json
-    [:email_address_check, :boolean],
-    [:mobile_check, :string],
-    [:qualifications_details_check, :boolean]
-  ]
-
-  CLAIM_ATTRIBUTES.each do |name, type|
-    attribute name, type
-  end
 
   validate :not_already_submitted
-
-  validates :email_address,
-    presence: {
-      message: ->(form, _) { form.i18n_error_message(:email_address) }
-    }
-
-  validates :email_verified,
-    inclusion: {
-      in: [true],
-      message: ->(form, _) { form.i18n_error_message(:email_verified) }
-    },
-    if: -> { email_address.present? }
-
+  validate :email_address_is_preesent
+  validate :email_address_verified
   validate :mobile_number_verified
-
   validate :claim_is_eligible
   validate :main_claim_is_submittable
 
   def initialize(journey_session:)
     @journey_session = journey_session
-
-    super(journey_session.answers.attributes)
   end
 
   def save
@@ -83,17 +28,11 @@ class ClaimSubmissionBaseForm
     @claim ||= build_claim
   end
 
-  def i18n_error_message(attr)
-    I18n.t("#{i18n_namespace}.forms.claim_submission_form.errors.#{attr}")
-  end
-
   private
 
   attr_reader :journey_session
 
-  def journey
-    journey_session.journey_module
-  end
+  delegate :answers, to: :journey_session
 
   def build_claim
     claim = Claim.new
@@ -107,18 +46,16 @@ class ClaimSubmissionBaseForm
 
     claim.eligibility = main_eligibility
 
-    claim_attribute_names.each do |attribute_name|
-      claim.public_send(:"#{attribute_name}=", public_send(attribute_name))
+    answers.attributes.each do |name, value|
+      if claim.respond_to?(:"#{name}=")
+        claim.public_send(:"#{name}=", value)
+      end
     end
 
     claim.policy_options_provided = generate_policy_options_provided
     claim.reference = generate_reference
     claim.submitted_at = Time.zone.now
     claim
-  end
-
-  def claim_attribute_names
-    CLAIM_ATTRIBUTES.map(&:first)
   end
 
   def eligibilities
@@ -131,11 +68,9 @@ class ClaimSubmissionBaseForm
   end
 
   def set_eligibility_attributes(eligibility)
-    eligibility_attribute_names.each do |attribute_name|
-      setter = "#{attribute_name}="
-      # Not all eligibilities have the same fields
-      if eligibility.respond_to?(setter)
-        eligibility.public_send(setter, public_send(attribute_name))
+    answers.attributes.each do |name, value|
+      if eligibility.respond_to?(:"#{name}=")
+        eligibility.public_send(:"#{name}=", value)
       end
     end
   end
@@ -150,6 +85,20 @@ class ClaimSubmissionBaseForm
   def not_already_submitted
     if journey_session.submitted?
       errors.add(:base, i18n_error_message(:already_submitted))
+    end
+  end
+
+  def email_address_is_preesent
+    if answers.email_address.blank?
+      errors.add(:email_address, i18n_error_message(:email_address))
+    end
+  end
+
+  def email_address_verified
+    return unless answers.email_address.present?
+
+    unless answers.email_verified
+      errors.add(:email_verified, i18n_error_message(:email_verified))
     end
   end
 
@@ -176,5 +125,9 @@ class ClaimSubmissionBaseForm
   def main_claim_is_submittable
     return if claim.valid?(:submit)
     claim.errors.full_messages.each { |message| errors.add(:base, message) }
+  end
+
+  def i18n_error_message(attr)
+    I18n.t("#{journey::I18N_NAMESPACE}.forms.claim_submission_form.errors.#{attr}")
   end
 end
