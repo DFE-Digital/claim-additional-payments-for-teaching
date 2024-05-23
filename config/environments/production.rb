b5b43f90e3bef9ea43f7a6d9820ffb078163dcb8
vacabor <166112501+vacabor@users.noreply.github.com>
2024-05-23 09:22:45 +0100

    Exclude healthcheck from SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,6 +41,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
+  config.ssl_options = { redirect: { exclude: -> request { /healthcheck/.match?(request.path) } } }
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
