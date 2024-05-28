e3b07bd7118e8ded657027beb9dc06798acb0d91
Richard Lynch <rjlynchdev@gmail.com>
2024-05-28 12:10:38 +0100

    Fix test
    We want to avoid just testing that `update!` has been called and instead
    test that the model has actually been updated correctly.
    Just testing that `update!` has been called fails to catch bugs if the
    model we're saving is changed, or if we change the implementation of
    `update!` in the form base class.

--- spec/forms/journeys/additional_payments_for_teaching/reminders/email_verification_form_spec.rb
+++ spec/forms/journeys/additional_payments_for_teaching/reminders/email_verification_form_spec.rb
@@ -1,11 +1,13 @@
 require "rails_helper"
 
 RSpec.describe Journeys::AdditionalPaymentsForTeaching::Reminders::EmailVerificationForm do
-  subject(:form) { described_class.new(claim: form_data_object, journey:, journey_session:, params:) }
+  subject(:form) do
+    described_class.new(claim: reminder, journey:, journey_session:, params:)
+  end
 
   let(:journey) { Journeys::AdditionalPaymentsForTeaching }
   let(:journey_session) { build(:additional_payments_session) }
-  let(:form_data_object) { Reminder.new }
+  let(:reminder) { Reminder.create! }
   let(:slug) { "email-verification" }
   let(:params) { ActionController::Parameters.new({slug:, form: form_params}) }
   let(:form_params) { {one_time_password: "123456"} }
@@ -17,25 +19,32 @@ RSpec.describe Journeys::AdditionalPaymentsForTeaching::Reminders::EmailVerifica
   describe "#save" do
     subject(:save) { form.save }
 
-    before do
-      allow(form).to receive(:update!).and_return(true)
-    end
-
     context "valid params" do
-      let(:form_params) { {"one_time_password" => OneTimePassword::Generator.new.code, "sent_one_time_password_at" => Time.now} }
+      let(:form_params) do
+        {
+          "one_time_password" => OneTimePassword::Generator.new.code,
+          "sent_one_time_password_at" => Time.now
+        }
+      end
 
       it "saves the attributes" do
         expect(save).to eq(true)
-        expect(form).to have_received(:update!).with(email_verified: true)
+        expect(reminder.reload.email_verified).to be true
       end
     end
 
     context "invalid params" do
-      let(:form_params) { {"one_time_password" => OneTimePassword::Generator.new.code, "sent_one_time_password_at" => ""} }
+      let(:form_params) do
+        {
+          "one_time_password" => OneTimePassword::Generator.new.code,
+          "sent_one_time_password_at" => ""
+        }
+      end
 
       it "does not save the attributes" do
-        expect(save).to eq(false)
-        expect(form).not_to have_received(:update!)
+        expect { expect(save).to eq(false) }.not_to(
+          change { reminder.reload.email_verified }
+        )
       end
     end
   end
