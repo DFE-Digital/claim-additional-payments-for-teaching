92da4311c5ef8af725ff9b615b1fe24ccc534af7
vacabor <166112501+vacabor@users.noreply.github.com>
2024-05-29 09:53:35 +0100

    Configure rails_semantic_logger for STDOUT

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,10 @@ Rails.application.configure do
     logger = ActiveSupport::Logger.new($stdout)
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
+
+    $stdout.sync = true
+    config.rails_semantic_logger.add_file_appender = false
+    config.semantic_logger.add_appender(io: $stdout, formatter: config.rails_semantic_logger.format)
   end
 
   # Do not dump schema after migrations.
