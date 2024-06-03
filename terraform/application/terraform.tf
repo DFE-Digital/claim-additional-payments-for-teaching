c2da698747028ee1bd6fad601125d49efecfdeec
vacabor <166112501+vacabor@users.noreply.github.com>
2024-06-03 07:52:30 +0100

    Upgrade terraform providers

--- terraform/application/terraform.tf
+++ terraform/application/terraform.tf
@@ -3,15 +3,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.82.0"
+      version = "3.104.2"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.24.0"
+      version = "2.30.0"
     }
     statuscake = {
       source  = "StatusCakeDev/statuscake"
-      version = "2.1.0"
+      version = "2.2.0"
     }
   }
   backend "azurerm" {
