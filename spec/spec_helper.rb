94dafa50c83cb8b20b71f676adc74c3fa805997c
Phil Lee <asmega@users.noreply.github.com>
2024-06-04 12:30:35 +0100

    retry flaky tests by default

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,6 +36,11 @@ require "rspec/retry"
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  # run retry around flaky tests
+  config.around :each, :flaky do |example|
+    example.run_with_retry retry: 3
+  end
+
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
