aec16ab361a1848ce12d145de7b335af7a20b3c8
vacabor <166112501+vacabor@users.noreply.github.com>
2024-06-18 12:30:22 +0100

    Don't add the STDOUT logger in test

--- config/initializers/semantic_logger.rb
+++ config/initializers/semantic_logger.rb
@@ -4,5 +4,7 @@ Rails.application.configure do
   config.log_tags = [:request_id]         # Prepend all log lines with the following tags
 end
 
-SemanticLogger.add_appender(io: STDOUT, level: Rails.application.config.log_level, formatter: Rails.application.config.log_format)
-Rails.application.config.logger.info('Application logging to STDOUT')
+unless Rails.env.test?
+  SemanticLogger.add_appender(io: STDOUT, level: Rails.application.config.log_level, formatter: Rails.application.config.log_format)
+  Rails.application.config.logger.info('Application logging to STDOUT')
+end
