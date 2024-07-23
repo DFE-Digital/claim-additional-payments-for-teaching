ebbe3cce2036059836e7a312d18fd2578270feda
Steve Lorek <steve@stevelorek.com>
2024-07-23 11:14:27 +0100

    Fix merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_09_110147) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_10_080536) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "pgcrypto"
