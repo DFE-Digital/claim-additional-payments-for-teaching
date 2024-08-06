854b6aaefba3d3dd109cf4b9963075486a4baec4
vacabor <166112501+vacabor@users.noreply.github.com>
2024-08-06 12:59:40 +0100

    [LUPEYALPHA-761] Early years provider import via Journey Configuration page (#3050)

---
+++ app/controllers/admin/eligible_ey_providers_controller.rb
@@ -0,0 +1,38 @@
+module Admin
+  class EligibleEyProvidersController < BaseAdminController
+    before_action :ensure_service_operator
+
+    helper_method :journey_configuration
+
+    def create
+      @upload_form = EligibleEyProvidersForm.new(upload_params)
+
+      if @upload_form.invalid?
+        render "admin/journey_configurations/edit"
+      else
+        @upload_form.importer.run
+        flash[:notice] = @upload_form.importer.results_message
+
+        redirect_to edit_admin_journey_configuration_path(Journeys::EarlyYearsPayment::Provider::ROUTING_NAME)
+      end
+    end
+
+    def show
+      send_data EligibleEyProvider.csv,
+        type: "text/csv",
+        filename: "eligible_early_years_providers.csv"
+    end
+
+    private
+
+    def journey_configuration
+      @journey_configuration ||= Journeys::Configuration.find_by(
+        routing_name: Journeys::EarlyYearsPayment::Provider::ROUTING_NAME
+      )
+    end
+
+    def upload_params
+      params.require(:eligible_ey_providers_upload).permit(:file)
+    end
+  end
+end
