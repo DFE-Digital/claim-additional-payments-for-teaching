91dc5a26c38196d3e61964f41f4c6494542939b3
Richard Lynch <rjlynchdev@gmail.com>
2024-08-09 14:56:58 +0100

    Add school to FE eligibility
    As part of the FE provider journey we need to check the ukprn on the
    claim's school. Claims get their school from their eligibility's
    `current_school` method, so we need to make sure that is set for FE
    eligibility.
    We've named the field `school_id` rather than `current_school_id` as the
    FE journey stores the school in the answers `school_id` column. We alias
    `school` to `current_school` so the `Claim#school` method still works.

--- spec/factories/policies/further_education_payments/eligibilities.rb
+++ spec/factories/policies/further_education_payments/eligibilities.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :further_education_payments_eligibility, class: "Policies::FurtherEducationPayments::Eligibility" do
+    school { create(:school, :further_education) }
+
     trait :eligible do
     end
   end
