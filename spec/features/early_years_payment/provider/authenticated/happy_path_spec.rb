02cc29dd6cc5fae090293aa61a518d64609631cc
vacabor <166112501+vacabor@users.noreply.github.com>
2024-08-14 13:47:09 +0100

    Add happy-path spec

--- spec/features/early_years_payment/provider/authenticated/happy_path_spec.rb
+++ spec/features/early_years_payment/provider/authenticated/happy_path_spec.rb
@@ -1,21 +1,27 @@
 require "rails_helper"
 
 RSpec.feature "Early years payment provider" do
+  let(:email_address) { "johndoe@example.com" }
   let(:journey_session) { Journeys::EarlyYearsPayment::Provider::Authenticated::Session.last }
   let(:mail) { ActionMailer::Base.deliveries.last }
   let(:magic_link) { mail[:personalisation].unparsed_value[:magic_link] }
+  let!(:nursery) { create(:eligible_ey_provider, primary_key_contact_email_address: email_address) }
 
   scenario "magic link onwards" do
     when_early_years_payment_provider_authenticated_journey_configuration_exists
     when_early_years_payment_provider_start_journey_completed
 
     visit magic_link
-    expect(journey_session.reload.answers.email_address).to eq "johndoe@example.com"
+    expect(journey_session.reload.answers.email_address).to eq email_address
     expect(journey_session.reload.answers.email_verified).to be true
     expect(page).to have_content("Declaration of Employee Consent")
     check "I confirm that I have obtained consent from my employee and have provided them with the relevant privacy notice."
     click_button "Continue"
     expect(page.current_path).to eq "/early-years-payment-provider/current-nursery"
+
+    choose nursery.nursery_name
+    click_button "Continue"
+    expect(page.current_path).to eq "/early-years-payment-provider/claimant-name"
   end
 
   scenario "using magic link after having completed some of the journey" do
