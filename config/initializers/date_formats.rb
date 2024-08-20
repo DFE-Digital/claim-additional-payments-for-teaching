5a00ab301beb584501c03919e55e9e12bf16a85c
Abigail McPhillips <abigail.mcphillips@gmail.com>
2024-08-20 15:23:17 +0100

    Update day format to remove leading 0

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,3 +1,3 @@
 DateTime::DATE_FORMATS[:custom_ordinal] = lambda { |time| time.strftime("%A #{time.day.ordinalize} %B %Y") }
 DateTime::DATE_FORMATS[:govuk_date] = lambda { |time| time.strftime("%d-%m-%Y") }
-DateTime::DATE_FORMATS[:long_date] = lambda { |time| time.strftime("%d %B %Y") }
+DateTime::DATE_FORMATS[:long_date] = lambda { |time| time.strftime("%-d %B %Y") }
