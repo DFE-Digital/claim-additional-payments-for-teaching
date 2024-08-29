9e6f48a6372ff5cb6611b298431267a4030f8288
Abigail McPhillips <abigail.mcphillips@gmail.com>
2024-08-29 14:14:01 +0100

    Add some extra scenarios to the policy_eligibility_checker_spec

--- spec/models/policies/early_years_payments/policy_eligibility_checker_spec.rb
+++ spec/models/policies/early_years_payments/policy_eligibility_checker_spec.rb
@@ -4,7 +4,56 @@ describe Policies::EarlyYearsPayments::PolicyEligibilityChecker do
   subject { described_class.new(answers: answers) }
 
   describe "#status, #ineligible?, #ineligibility_reason" do
-    context "when ineligible as :returner" do
+    context "not a returner" do
+      let(:answers) do
+        build(
+          :early_years_payment_answers,
+          returning_within_6_months: false,
+          returner_worked_with_children: nil,
+          returner_contract_type: nil
+        )
+      end
+
+      it "is eligible" do
+        expect(subject).to_not be_ineligible
+        expect(subject.status).to eql(:eligible_now)
+      end
+    end
+
+    context "returner, worked with children in non-permanent position" do
+      let(:answers) do
+        build(
+          :early_years_payment_answers,
+          returning_within_6_months: true,
+          returner_worked_with_children: false,
+          returner_contract_type: "voluntary_or_unpaid"
+        )
+      end
+
+      it "is eligible" do
+        expect(subject).to_not be_ineligible
+        expect(subject.status).to eql(:eligible_now)
+      end
+    end
+
+    # If user jumps between questions out-of-order, this situation could arise
+    context "returner, did not work with children in permanent position" do
+      let(:answers) do
+        build(
+          :early_years_payment_answers,
+          returning_within_6_months: true,
+          returner_worked_with_children: false,
+          returner_contract_type: "permanent"
+        )
+      end
+
+      it "is eligible" do
+        expect(subject).to_not be_ineligible
+        expect(subject.status).to eql(:eligible_now)
+      end
+    end
+
+    context "returner, worked with children in permanent position" do
       let(:answers) do
         build(
           :early_years_payment_answers,
@@ -14,7 +63,7 @@ describe Policies::EarlyYearsPayments::PolicyEligibilityChecker do
         )
       end
 
-      it "is ineligble as :returner" do
+      it "is ineligible as :returner" do
         expect(subject).to be_ineligible
         expect(subject.status).to eql(:ineligible)
         expect(subject.ineligibility_reason).to eql(:returner)
