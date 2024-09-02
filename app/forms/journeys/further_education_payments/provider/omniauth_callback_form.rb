47d5e8979fd20ea2b38b33ae9e858606043a16ac
Richard Lynch <rjlynchdev@gmail.com>
2024-09-02 17:07:31 +0100

    Capture dfe sign in org name
    Captures the organisation name from DfE sign in, we'll want to know
    this if we're creating `DfeSignIn::User` records for the providers.

--- app/forms/journeys/further_education_payments/provider/omniauth_callback_form.rb
+++ app/forms/journeys/further_education_payments/provider/omniauth_callback_form.rb
@@ -12,6 +12,7 @@ module Journeys
             dfe_sign_in_uid: dfe_sign_in_uid,
             dfe_sign_in_organisation_id: dfe_sign_in_organisation_id,
             dfe_sign_in_organisation_ukprn: dfe_sign_in_organisation_ukprn,
+            dfe_sign_in_organisation_name: dfe_sign_in_organisation_name,
             dfe_sign_in_service_access: dfe_sign_in_service_access?,
             dfe_sign_in_role_codes: dfe_sign_in_role_codes,
             dfe_sign_in_first_name: dfe_sign_in_first_name,
@@ -36,6 +37,10 @@ module Journeys
 
         def dfe_sign_in_organisation_id
           auth.dig("extra", "raw_info", "organisation", "id")
+        end
+
+        def dfe_sign_in_organisation_name
+          auth.dig("extra", "raw_info", "organisation", "name")
         end
 
         def dfe_sign_in_service_access?
