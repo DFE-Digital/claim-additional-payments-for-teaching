c3f49e03e57e2590021db609b776c31b661ff4eb
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2024-09-05 09:49:29 +0100

    content updates

--- app/models/policies/further_education_payments.rb
+++ app/models/policies/further_education_payments.rb
@@ -9,8 +9,6 @@ module Policies
     # Percentage of claims to QA
     MIN_QA_THRESHOLD = 10
 
-    URL_SPREADSHEET_ELIGIBLE_PROVIDERS = "https://assets.publishing.service.gov.uk/media/667300fe64e554df3bd0db92/List_of_eligible_FE_providers_and_payment_value_for_levelling_up_premium.xlsx".freeze
-
     VERIFIERS = [
       AutomatedChecks::ClaimVerifiers::ProviderVerification
     ]
