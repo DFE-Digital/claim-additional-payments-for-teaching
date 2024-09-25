3e8a46f0f3ee1979553bf7207a696272772f7765
Phil Lee <asmega@users.noreply.github.com>
2024-09-25 12:51:01 +0100

    add EY practitioner scaffoling

---
+++ app/models/journeys/early_years_payment/practitioner/slug_sequence.rb
@@ -0,0 +1,27 @@
+module Journeys
+  module EarlyYearsPayment
+    module Practitioner
+      class SlugSequence
+        SLUGS = %w[
+          find-reference
+        ].freeze
+
+        def self.start_page_url
+          Rails.application.routes.url_helpers.landing_page_path("early-years-payment-practitioner")
+        end
+
+        attr_reader :journey_session
+
+        delegate :answers, to: :journey_session
+
+        def initialize(journey_session)
+          @journey_session = journey_session
+        end
+
+        def slugs
+          SLUGS
+        end
+      end
+    end
+  end
+end
