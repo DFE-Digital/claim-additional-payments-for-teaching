de0153789bd4d16f392a3ccdbccc0c6e18b18fa9
vacabor <166112501+vacabor@users.noreply.github.com>
2024-10-04 16:50:00 +0100

    Spec to cover the unhappy path

--- spec/features/early_years_payment/practitioner/find_reference_spec.rb
+++ spec/features/early_years_payment/practitioner/find_reference_spec.rb
@@ -18,17 +18,13 @@ RSpec.feature "Early years find reference" do
     fill_in "Claim reference number", with: claim.reference
     click_button "Submit"
 
-    expect(page).to have_content "Enter your claim reference"
+    expect(page).to have_content "This claim reference isn’t correct."
   end
 
   scenario "after multiple attempts should work" do
     when_early_years_payment_practitioner_journey_configuration_exists
 
     visit "/early-years-payment-practitioner/find-reference?skip_landing_page=true&email=user@example.com"
-    expect(page).to have_content "Enter your claim reference"
-    fill_in "Claim reference number", with: "foo"
-    click_button "Submit"
-
     expect(page).to have_content "Enter your claim reference"
     fill_in "Claim reference number", with: claim.reference
     click_button "Submit"
@@ -40,5 +36,26 @@ RSpec.feature "Early years find reference" do
     fill_in "Claim reference number", with: claim.reference
     click_button "Submit"
     expect(page).to have_content "Sign in with GOV.UK One Login"
+  end
+
+  scenario "should show ineligibility page when an invalid reference is given" do
+    when_early_years_payment_practitioner_journey_configuration_exists
+
+    visit "/early-years-payment-practitioner/find-reference?skip_landing_page=true&email=user@example.com"
+    expect(page).to have_content "Enter your claim reference"
+    fill_in "Claim reference number", with: "invalid"
+    click_button "Submit"
+
+    expect(page).to have_content "This claim reference isn’t correct."
+    click_link "Try again"
+
+    expect(page).to have_content "Enter your claim reference"
+    fill_in "Claim reference number", with: "also invalid"
+    click_button "Submit"
+
+    expect(page).to have_content "This claim reference isn’t correct."
+    click_link "Back"
+
+    expect(page).to have_content "Enter your claim reference"
   end
 end
