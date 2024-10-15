026191d1521b7bbc64c93535fb49da00fbb3e134
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2024-10-15 09:16:41 +0100

    EY Practitioner check-your-answers page

--- spec/features/early_years_payment/practitioner/happy_path_spec.rb
+++ spec/features/early_years_payment/practitioner/happy_path_spec.rb
@@ -71,6 +71,12 @@ RSpec.feature "Early years payment practitioner" do
 
     expect(page).to have_text(I18n.t("forms.gender.questions.payroll_gender"))
     choose "Female"
-    # click_on "Continue"
+    click_on "Continue"
+    
+    expect(page).to have_content("Check your answers before submitting this claim")
+    expect do
+        click_on "Accept and send"
+    end.to change { Claim.count }.by(1)
+    .and change { Policies::EarlyYearsPayments::Eligibility.count }.by(1)
   end
 end
