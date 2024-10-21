905ee70707de488036f4dcea65814948538663a5
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2024-10-21 10:45:42 +0100

    do not perform DQT check for EY

--- app/jobs/claim_verifier_job.rb
+++ app/jobs/claim_verifier_job.rb
@@ -1,12 +1,14 @@
 class ClaimVerifierJob < ApplicationJob
   def perform(claim)
-    AutomatedChecks::ClaimVerifier.new(
-      claim:,
-      dqt_teacher_status: claim.has_dqt_record? ? Dqt::Teacher.new(claim.dqt_teacher_status) : Dqt::Client.new.teacher.find(
-        claim.eligibility.teacher_reference_number,
-        birthdate: claim.date_of_birth.to_s,
-        nino: claim.national_insurance_number
-      )
-    ).perform
+    if claim.eligibility.respond_to?(:teacher_reference_number)
+      AutomatedChecks::ClaimVerifier.new(
+        claim:,
+        dqt_teacher_status: claim.has_dqt_record? ? Dqt::Teacher.new(claim.dqt_teacher_status) : Dqt::Client.new.teacher.find(
+          claim.eligibility.teacher_reference_number,
+          birthdate: claim.date_of_birth.to_s,
+          nino: claim.national_insurance_number
+        )
+      ).perform
+    end
   end
 end
