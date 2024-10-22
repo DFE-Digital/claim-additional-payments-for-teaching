0746324fad5287bc375a3508a0b88f3b9c9dfc74
Richard Lynch <rjlynchdev@gmail.com>
2024-10-22 15:36:40 +0100

    Fix link to approve/reject for reversed claims
    The link to "approve/reject claim" changes depending on whether a
    decision has been made. When determining if a decision has been made we
    need to ignore decisions which have been undone.

---
+++ spec/features/admin/admin_reverse_claim_decision_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.feature "Admin reverses a claim decision" do
+  it "allows admins to reverse a claim decision" do
+    sign_in_as_service_operator
+
+    claim = create(:claim, :submitted)
+
+    visit admin_claim_tasks_path(claim)
+    click_on "Approve or reject this claim"
+    choose "Reject"
+    check "Ineligible subject"
+    click_button "Confirm decision"
+
+    visit new_admin_claim_amendment_path(claim)
+    click_link "Undo decision"
+    fill_in "Change notes", with: "test"
+    click_button "Undo rejection"
+
+    visit admin_claim_tasks_path(claim)
+    click_on "Approve or reject this claim"
+    choose "Approve"
+    click_button "Confirm decision"
+
+    expect(page).to have_content "Claim has been approved successfully"
+  end
+end
