a483f9aede2923eea61e209a013bf16e86c4b425
vacabor <166112501+vacabor@users.noreply.github.com>
2024-10-22 09:48:07 +0100

    WIP: EY claim summary

--- app/models/policies/early_years_payments/eligibility.rb
+++ app/models/policies/early_years_payments/eligibility.rb
@@ -20,6 +20,10 @@ module Policies
       def provider_claim_submitted?
         provider_claim_submitted_at.present?
       end
+
+      def award_amount
+        # TODO: NOOP until confirmed.
+      end
     end
   end
 end
