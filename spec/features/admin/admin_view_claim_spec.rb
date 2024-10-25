7fac61c0688f59f625de4b925497e4609d7ecb3b
vacabor <166112501+vacabor@users.noreply.github.com>
2024-10-25 09:54:45 +0100

    [LUPEYALPHA-1169] EY admin site claim summary (#3335)
    EY claim summary

--- spec/features/admin/admin_view_claim_spec.rb
+++ spec/features/admin/admin_view_claim_spec.rb
@@ -2,5 +2,5 @@ require "rails_helper"
 
 RSpec.feature "Admin view claim" do
   Policies.all.each { |policy| it_behaves_like "Admin View Claim Feature", policy }
-  Policies.all.excluding(Policies::FurtherEducationPayments).each { |policy| it_behaves_like "Admin View Claim logged in with tid", policy }
+  Policies.all.excluding(Policies::EarlyYearsPayments, Policies::FurtherEducationPayments).each { |policy| it_behaves_like "Admin View Claim logged in with tid", policy }
 end
