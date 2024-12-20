e6ea204dcfe8fab6f44d37602dce971d8c40d700
Neill Turner <neill.turner@education.gov.uk>
2024-12-20 12:41:25 +0000

    Migrate gcp wif

--- config/initializers/dfe_analytics.rb
+++ config/initializers/dfe_analytics.rb
@@ -1,55 +1,12 @@
 DfE::Analytics.configure do |config|
-  # Whether to log events instead of sending them to BigQuery.
-  #
-  # config.log_only = true
-  config.log_only = (%w[development test].include?(ENV["RAILS_ENV"]) || ENV["ENVIRONMENT_NAME"].start_with?("review"))
-
-  # Whether to use ActiveJob or dispatch events immediately.
-  #
-  # config.async = true
-
-  # Which ActiveJob queue to put events on
-  #
-  # config.queue = :default
-
-  # The name of the BigQuery table we’re writing to.
-  #
-  # config.bigquery_table_name = ENV['BIGQUERY_TABLE_NAME']
-
-  # The name of the BigQuery project we’re writing to.
-  #
-  # config.bigquery_project_id = ENV['BIGQUERY_PROJECT_ID']
-
-  # The name of the BigQuery dataset we're writing to.
-  #
-  # config.bigquery_dataset = ENV['BIGQUERY_DATASET']
-
-  # Service account JSON key for the BigQuery API. See
-  # https://cloud.google.com/bigquery/docs/authentication/service-account-file
-  #
-  # config.bigquery_api_json_key = ENV['BIGQUERY_API_JSON_KEY']
-
-  # Passed directly to the retries: option on the BigQuery client
-  #
-  # config.bigquery_retries = 3
-
-  # Passed directly to the timeout: option on the BigQuery client
-  #
-  # config.bigquery_timeout = 120
-
-  # A proc which returns true or false depending on whether you want to
-  # enable analytics. You might want to hook this up to a feature flag or
-  # environment variable.
-  #
-  config.enable_analytics = proc { Rails.env.production? }
-
-  # Enable entity table check job
-  #
+  config.queue = :analytics
+  config.environment = HostingEnvironment.environment_name
   config.entity_table_checks_enabled = true
 
-  # The environment we’re running in. This value will be attached
-  # to all events we send to BigQuery.
-  #
-  # config.environment = ENV.fetch('RAILS_ENV', 'development')
+  config.enable_analytics =
+    proc do
+      disabled_by_default = Rails.env.development?
+      ENV.fetch("BIGQUERY_DISABLE", disabled_by_default.to_s) != "true"
+    end
   config.azure_federated_auth = ENV.include? "GOOGLE_CLOUD_CREDENTIALS"
 end
