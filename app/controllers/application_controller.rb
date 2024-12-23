edc7e54470329ab39e739b349750f542439dc539
Kenneth Lee <kenfodder@gmail.com>
2024-12-23 16:19:53 +0000

    CAPT-2087 Skip CSRF checks for unwanted requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   TIMEOUT_WARNING_LENGTH_IN_MINUTES = 2
 
   helper_method :timeout_warning_in_minutes
+  protect_from_forgery except: :handle_unwanted_requests
 
   def handle_unwanted_requests
     render file: Rails.root.join("public", "404.html"), status: :not_found, layout: false
