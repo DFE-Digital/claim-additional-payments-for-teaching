5688882f6d51daf6ce28e75f078aad8fa472dd3f
Richard Lynch <rjlynchdev@gmail.com>
2024-12-27 11:27:33 +0000

    Include claim reference in rollbar errors
    When a job errors we want to be able to easily identify which claim it
    was working with, if there is one.

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,4 +1,14 @@
 class ApplicationJob < ActiveJob::Base
+  around_perform do |job, block|
+    claim = job.arguments.find { |arg| arg.is_a?(Claim) }
+
+    if claim
+      Rollbar.scope!(claim: {reference: claim.reference})
+    end
+
+    block.call
+  end
+
   def priority
     10
   end
