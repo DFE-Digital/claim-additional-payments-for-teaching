0dd3759a929fa97bf1590efc39a7506ab82fe442
Richard Lynch <rjlynchdev@gmail.com>
2024-12-30 12:18:00 +0000

    Log session answers to rollbar
    Some of the errors in rollbar are tricky to debug as we don't know what
    answers the claimant provided. This commit adds a mechanism for flagging
    attributes as PII and updates the ClaimsController to include the non
    pii attributes in the Rollbar scope such that they are available in
    Rollbar.

--- app/models/journeys/early_years_payment/provider/authenticated/session_answers.rb
+++ app/models/journeys/early_years_payment/provider/authenticated/session_answers.rb
@@ -5,17 +5,17 @@ module Journeys
         class SessionAnswers < Journeys::SessionAnswers
           attribute :consent_given, :boolean
           attribute :nursery_urn
-          attribute :paye_reference
+          attribute :paye_reference, pii: true
           attribute :start_date, :date
           attribute :child_facing_confirmation_given, :boolean
           attribute :returning_within_6_months, :boolean
           attribute :returner_worked_with_children, :boolean
           attribute :returner_contract_type
-          attribute :practitioner_email_address
-          attribute :provider_contact_name
-          attribute :provider_email_address
-          attribute :practitioner_first_name
-          attribute :practitioner_surname
+          attribute :practitioner_email_address, pii: true
+          attribute :provider_contact_name, pii: true
+          attribute :provider_email_address, pii: true
+          attribute :practitioner_first_name, pii: true
+          attribute :practitioner_surname, pii: true
 
           def policy
             Policies::EarlyYearsPayments
--- app/models/journeys/further_education_payments/provider/session_answers.rb
+++ app/models/journeys/further_education_payments/provider/session_answers.rb
@@ -7,12 +7,12 @@ module Journeys
         attribute :dfe_sign_in_uid, :string
         attribute :dfe_sign_in_organisation_id, :string
         attribute :dfe_sign_in_organisation_ukprn, :string
-        attribute :dfe_sign_in_organisation_name, :string
+        attribute :dfe_sign_in_organisation_name, :string, pii: true
         attribute :dfe_sign_in_service_access, :boolean, default: false
         attribute :dfe_sign_in_role_codes, default: []
-        attribute :dfe_sign_in_first_name, :string
-        attribute :dfe_sign_in_last_name, :string
-        attribute :dfe_sign_in_email, :string
+        attribute :dfe_sign_in_first_name, :string, pii: true
+        attribute :dfe_sign_in_last_name, :string, pii: true
+        attribute :dfe_sign_in_email, :string, pii: true
 
         def claim
           @claim ||= Claim.includes(eligibility: :school).find(claim_id)
