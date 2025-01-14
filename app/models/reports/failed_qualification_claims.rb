bc4dbfb7a5174cae50f963512f1afde27c8c700b
Richard Lynch <rjlynchdev@gmail.com>
2025-01-14 15:18:01 +0000

    Fix up failed qualification claims
    Some tweaks to the spike code to get this working, we need to handle the
    eligibility not having an eligibile itt subject, and we also want to
    scope the claims we're looking at to the current academic year.

--- app/models/reports/failed_qualification_claims.rb
+++ app/models/reports/failed_qualification_claims.rb
@@ -7,70 +7,111 @@ module Reports
     HEADERS = [
       "Claim reference",
       "Teacher reference number",
-      "Policy name",
+      "Policy",
+      "Status",
       "Decision date",
       "Decision agent",
-      "Answered qualification",
-      "Answered ITT start year",
-      "Answered ITT subject",
-      "DQT ITT subjects",
-      "DQT ITT start year",
-      "DQT QTS award date",
-      "DQT qualification name"
+      "Applicant answers - Qualification",
+      "Applicant answers - ITT start year",
+      "Applicant answers - ITT subject",
+      "DQT API - ITT subjects",
+      "DQT API - ITT start date",
+      "DQT API - QTS award date",
+      "DQT API - Qualification name"
     ].freeze
 
     def initialize
-      @claims = Claim.includes(:tasks).where(tasks: {name: "qualifications", passed: false}).approved
+      @claims = Claim
+        .approved
+        .where(academic_year: AcademicYear.current)
+        .joins(:tasks).merge(Task.where(name: "qualifications", passed: false))
+        .includes(:eligibility, decisions: :created_by)
     end
 
     def to_csv
       CSV.generate(write_headers: true, headers: HEADERS) do |csv|
         @claims.each do |claim|
-          csv << row(
-            claim.reference,
-            claim.eligibility.teacher_reference_number,
-            claim.policy,
-            claim.latest_decision.created_at,
-            claim.latest_decision.created_by.full_name,
-            claim.eligibility.qualification,
-            claim.eligibility.itt_academic_year.start_year,
-            claim.eligibility.eligible_itt_subject,
-            dqt_itt_subjects(claim),
-            dqt_itt_start_date(claim),
-            dqt_qts_date(claim),
-            dqt_qts_qualification_name(claim)
-          )
+          csv << ClaimPresenter.new(claim).to_a
         end
       end
     end
 
-    private
+    class ClaimPresenter
+      include Admin::ClaimsHelper
 
-    def row(*entries)
-      entries.map { |entry| ExcelUtils.escape_formulas(entry) }
-    end
-
-    def dqt_itt_subjects(claim)
-      unless claim.dqt_teacher_status.empty?
-        claim.dqt_teacher_status["initial_teacher_training"].fetch_values("subject1", "subject2", "subject3").compact.join(",")
+      def initialize(claim)
+        @claim = claim
       end
-    end
 
-    def dqt_itt_start_date(claim)
-      unless claim.dqt_teacher_status.empty?
-        claim.dqt_teacher_status["initial_teacher_training"]["programme_start_date"]
+      def to_a
+        [
+          claim.reference,
+          claim.eligibility.teacher_reference_number,
+          I18n.t("#{claim.policy.locale_key}.policy_acronym"),
+          status(claim),
+          I18n.l(approval.created_at.to_date, format: :day_month_year),
+          approval.created_by.full_name,
+          qualification,
+          itt_academic_year,
+          eligible_itt_subject,
+          itt_subjects,
+          itt_start_date,
+          qts_award_date,
+          qualification_name
+        ]
       end
-    end
 
-    def dqt_qts_date(claim)
-      unless claim.dqt_teacher_status.empty?
-        claim.dqt_teacher_status["qualified_teacher_status"]["qts_date"]
+      private
+
+      attr_reader :claim
+
+      def approval
+        @approval ||= claim.decisions.reject(&:undone).last
       end
-    end
 
-    def dqt_qts_qualification_name(claim)
-      unless claim.dqt_teacher_status.empty?
-        claim.dqt_teacher_status["qualified_teacher_status"]["name"]
+      # StudentLoans doesn't have an eligible_itt_subject
+      def eligible_itt_subject
+        claim.eligibility.try(:eligible_itt_subject)
+      end
+
+      # StudentLoans doesn't have an itt_academic_year
+      def itt_academic_year
+        claim.eligibility.try(:itt_academic_year)
+      end
+
+      # StudentLoans doesn't have a qualification
+      def qualification
+        claim.eligibility.try(:qualification)
+      end
+
+      def itt_subjects
+        dqt_teacher_record&.itt_subjects&.join(", ")
+      end
+
+      def itt_start_date
+        date = dqt_teacher_record&.itt_start_date
+
+        return unless date
+
+        I18n.l(date, format: :day_month_year)
+      end
+
+      def qts_award_date
+        date = dqt_teacher_record&.qts_award_date
+
+        return unless date
+
+        I18n.l(date, format: :day_month_year)
+      end
+
+      def qualification_name
+        dqt_teacher_record&.qualification_name
+      end
+
+      def dqt_teacher_record
+        @dqt_teacher_record ||= if claim.has_dqt_record?
+          Dqt::Teacher.new(claim.dqt_teacher_status)
+        end
       end
     end
   end
