3baab71ebdf3b170f76b6fae23fadff80c98e368
Richard Lynch <rjlynchdev@gmail.com>
2025-01-16 12:09:09 +0000

    Don't generate multiple reports
    We don't need to keep old reports around so there's no need to generate
    a new report each time the job runs.

--- app/jobs/reports_job.rb
+++ app/jobs/reports_job.rb
@@ -4,11 +4,22 @@ class ReportsJob < CronJob
   def perform
     Rails.logger.info "Generating Ops reports"
 
-    csv = Reports::DuplicateClaims.new.to_csv
-    Report.create!(name: Reports::DuplicateClaims::NAME, csv: csv, number_of_rows: csv.lines.count - 1)
     csv = Reports::FailedQualificationClaims.new.to_csv
-    Report.create!(name: Reports::FailedQualificationClaims::NAME, csv: csv, number_of_rows: csv.lines.count - 1)
+
+    Report
+      .find_or_initialize_by(name: Reports::FailedQualificationClaims::NAME)
+      .update!(csv: csv, number_of_rows: csv.lines.count - 1)
+
     csv = Reports::FailedProviderCheckClaims.new.to_csv
-    Report.create!(name: Reports::FailedProviderCheckClaims::NAME, csv: csv, number_of_rows: csv.lines.count - 1)
+
+    Report
+      .find_or_initialize_by(name: Reports::FailedProviderCheckClaims::NAME)
+      .update!(csv: csv, number_of_rows: csv.lines.count - 1)
+
+    csv = Reports::DuplicateClaims.new.to_csv
+
+    Report
+      .find_or_initialize_by(name: Reports::DuplicateClaims::NAME)
+      .update!(csv: csv, number_of_rows: csv.lines.count - 1)
   end
 end
