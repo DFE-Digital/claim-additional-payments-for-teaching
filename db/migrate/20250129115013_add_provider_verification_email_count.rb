8428b27d476492d3b4beba283a461d5a1f25544e
Phil Lee <asmega@users.noreply.github.com>
2025-01-30 10:58:04 +0000

    convert migration to use up and down

--- db/migrate/20250129115013_add_provider_verification_email_count.rb
+++ db/migrate/20250129115013_add_provider_verification_email_count.rb
@@ -1,6 +1,8 @@
 class AddProviderVerificationEmailCount < ActiveRecord::Migration[8.0]
-  def change
+  def up
     add_column :further_education_payments_eligibilities, :provider_verification_email_count, :integer, default: 0
+
+    Policies::FurtherEducationPayments::Eligibility.reset_column_information
 
     Policies::FurtherEducationPayments::Eligibility
       .where("provider_verification_email_last_sent_at IS NOT NULL")
@@ -10,5 +12,9 @@ class AddProviderVerificationEmailCount < ActiveRecord::Migration[8.0]
     Policies::FurtherEducationPayments::Eligibility
       .where("provider_verification_chase_email_last_sent_at IS NOT NULL")
       .update_all(provider_verification_email_count: 2)
+  end
+
+  def down
+    remove_column :further_education_payments_eligibilities, :provider_verification_email_count
   end
 end
