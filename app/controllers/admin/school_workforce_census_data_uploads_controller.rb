f319d3a872e96e5389692e4761863ba2b433c5d8
Richard Lynch <rjlynchdev@gmail.com>
2025-02-07 11:39:58 +0000

    Rate limit file uploads
    Feedback from the ITHC.
    We don't have a cache set so rate limiting will use the file cache,
    storing the data in ./tmp on the containers. The rate limiting will be
    per container, rather than app wide but given these uploads are
    restricted to admins it wont be an issue. We don't do a lot of file
    uploads so storing this info in the container file system shouldn't be
    an issue.
    If we start supporting public file uploads we'll want to investigate a
    different cache store.

--- app/controllers/admin/school_workforce_census_data_uploads_controller.rb
+++ app/controllers/admin/school_workforce_census_data_uploads_controller.rb
@@ -2,6 +2,18 @@ module Admin
   class SchoolWorkforceCensusDataUploadsController < BaseAdminController
     before_action :ensure_service_operator
 
+    rate_limit(
+      to: 1,
+      within: 30.seconds,
+      only: :create,
+      with: -> do
+        redirect_to(
+          new_admin_school_workforce_census_data_upload_path,
+          alert: "Too many requests"
+        )
+      end
+    )
+
     def new
     end
 
--- app/controllers/admin/student_loans_data_uploads_controller.rb
+++ app/controllers/admin/student_loans_data_uploads_controller.rb
@@ -2,6 +2,18 @@ module Admin
   class StudentLoansDataUploadsController < BaseAdminController
     before_action :ensure_service_operator
 
+    rate_limit(
+      to: 1,
+      within: 30.seconds,
+      only: :create,
+      with: -> do
+        redirect_to(
+          new_admin_student_loans_data_upload_path,
+          alert: "Too many requests"
+        )
+      end
+    )
+
     def new
     end
 
