4842db1991f9658142355a07d9c4c7fe1f5ce698
Phil Lee <asmega@users.noreply.github.com>
2025-02-17 12:31:17 +0000

    only create tables if they exists
    this resolves a test environment in quirkly state due to rollback

--- db/migrate/20250210115829_create_targeted_retention_incentive_payments_awards.rb
+++ db/migrate/20250210115829_create_targeted_retention_incentive_payments_awards.rb
@@ -1,6 +1,6 @@
 class CreateTargetedRetentionIncentivePaymentsAwards < ActiveRecord::Migration[8.0]
   def change
-    create_table :targeted_retention_incentive_payments_awards, id: :uuid do |t|
+    create_table :targeted_retention_incentive_payments_awards, id: :uuid, if_not_exists: true do |t|
       t.string :academic_year, limit: 9, null: false
       t.integer :school_urn, null: false
       t.decimal :award_amount, precision: 7, scale: 2
