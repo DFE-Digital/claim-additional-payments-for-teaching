f972d89c868bcea8e4a1e7641611b7900c704c54
Richard Lynch <rjlynchdev@gmail.com>
2025-02-26 15:44:41 +0000

    PR feedback
    Add a used column to service access codes to determine if they've been
    used and update that in the claim submission form.

--- spec/models/journeys/service_access_code_spec.rb
+++ spec/models/journeys/service_access_code_spec.rb
@@ -1,22 +1,67 @@
 require "rails_helper"
 
 RSpec.describe Journeys::ServiceAccessCode, type: :model do
-  describe ".unused" do
-    it "returns the used service access codes" do
-      unused_service_access_code = create(:service_access_code)
+  describe "scopes" do
+    describe ".used" do
+      it "returns access codes marked as used" do
+        used_code = create(:service_access_code, used: true)
+        unused_code = create(:service_access_code, used: false)
 
-      used_service_access_code = create(:service_access_code)
+        expect(described_class.used).to include(used_code)
+        expect(described_class.used).not_to include(unused_code)
+      end
+    end
+
+    describe ".unused" do
+      it "returns access codes not marked as used" do
+        used_code = create(:service_access_code, used: true)
+        unused_code = create(:service_access_code, used: false)
+
+        expect(described_class.unused).not_to include(used_code)
+        expect(described_class.unused).to include(unused_code)
+      end
+    end
+  end
+
+  describe "#mark_as_used!" do
+    it "marks the access code as used" do
+      code = create(:service_access_code)
+
+      expect { code.mark_as_used! }.to(
+        change { code.reload.used }.from(false).to(true)
+      )
+    end
+  end
+
+  describe "claim submission with service access code" do
+    it "marks the service access code as used when a claim is submitted" do
+      service_access_code = create(
+        :service_access_code,
+        journey: Journeys::FurtherEducationPayments
+      )
+
+      answers = attributes_for(
+        :further_education_payments_answers,
+        :submittable,
+        logged_in_with_onelogin: true,
+        onelogin_idv_first_name: "John",
+        onelogin_idv_last_name: "Doe",
+        onelogin_idv_full_name: "John Doe",
+        onelogin_idv_date_of_birth: Date.new(1970, 1, 1)
+      ).merge(service_access_code: service_access_code.code)
 
       journey_session = create(
         :further_education_payments_session,
-        answers: {
-          service_access_code: used_service_access_code.code
-        }
+        answers: answers
       )
 
-      create(:claim, journey_session: journey_session)
+      form = Journeys::FurtherEducationPayments::ClaimSubmissionForm.new(
+        journey_session: journey_session
+      )
 
-      expect(described_class.unused).to eq([unused_service_access_code])
+      expect(form.save).to be true
+
+      expect(service_access_code.reload.used?).to be true
     end
   end
 
@@ -52,18 +97,13 @@ RSpec.describe Journeys::ServiceAccessCode, type: :model do
       it "returns false" do
         journey = Journeys::FurtherEducationPayments
 
-        service_access_code = create(:service_access_code, journey: journey)
-
-        code = service_access_code.code
-
-        journey_session = create(
-          :further_education_payments_session,
-          answers: {
-            service_access_code: service_access_code.code
-          }
+        service_access_code = create(
+          :service_access_code,
+          journey: journey,
+          used: true
         )
 
-        create(:claim, journey_session: journey_session)
+        code = service_access_code.code
 
         expect(
           described_class.permits_access?(code: code, journey: journey)
