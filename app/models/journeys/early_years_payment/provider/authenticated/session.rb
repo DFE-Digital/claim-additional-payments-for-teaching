05fe2f989f58c2d31f8351a3c2b756748f97bc11
Richard Lynch <rjlynchdev@gmail.com>
2025-03-03 17:03:42 +0000

    Pass reference to a session to answers
    Sometimes we need a reference to the session in the answers for things
    like timestamps.
    Annoyingly the answers method needs to be defined on each subclass.
    Ideally we could move the `answers` attribute and this method to the
    base class, and lose the `SessionAnswerType` subclasses, but I'm not sure
    how we could do this and still support `SessionAnswer` subclasses for
    each journey.

--- app/models/journeys/early_years_payment/provider/authenticated/session.rb
+++ app/models/journeys/early_years_payment/provider/authenticated/session.rb
@@ -4,6 +4,10 @@ module Journeys
       module Authenticated
         class Session < Journeys::Session
           attribute :answers, SessionAnswersType.new
+
+          def answers
+            super.tap { it.session = self }
+          end
         end
       end
     end
