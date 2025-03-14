b98a8136b059dafca83e3fb28cf5564ecd22a565
Phil Lee <asmega@users.noreply.github.com>
2025-03-14 11:06:11 +0000

    implement claim check tasks for each policy

--- app/models/claim_checking_tasks.rb
+++ app/models/claim_checking_tasks.rb
@@ -21,40 +21,9 @@ class ClaimCheckingTasks
   delegate :policy, to: :claim
 
   def applicable_task_names
-    case policy
-    when Policies::FurtherEducationPayments
-      Policies::FurtherEducationPayments::ClaimCheckingTasks
-        .new(claim)
-        .applicable_task_names
-    when Policies::InternationalRelocationPayments
-      Policies::InternationalRelocationPayments::ClaimCheckingTasks
-        .new(claim)
-        .applicable_task_names
-    when Policies::EarlyYearsPayments
-      Policies::EarlyYearsPayments::ClaimCheckingTasks
-        .new(claim)
-        .applicable_task_names
-    else
-      Task::NAMES.dup.tap do |task_names|
-        task_names.delete("one_login_identity")
-        task_names.delete("previous_payment")
-        task_names.delete("previous_residency")
-        task_names.delete("induction_confirmation") unless claim.policy == Policies::EarlyCareerPayments
-        task_names.delete("student_loan_amount") unless claim.policy == Policies::StudentLoans
-        task_names.delete("student_loan_plan") unless claim.has_ecp_or_targeted_retention_incentive_policy? && claim.submitted_without_slc_data?
-        task_names.delete("payroll_details") unless claim.must_manually_validate_bank_details?
-        task_names.delete("matching_details") unless matching_claims.exists?
-        task_names.delete("payroll_gender") unless claim.payroll_gender_missing? || task_names_for_claim.include?("payroll_gender")
-        task_names.delete("visa")
-        task_names.delete("arrival_date")
-        task_names.delete("employment_contract")
-        task_names.delete("employment_start")
-        task_names.delete("subject")
-        task_names.delete("teaching_hours")
-        task_names.delete("provider_verification")
-        task_names.delete("provider_details")
-      end
-    end
+    policy::ClaimCheckingTasks
+      .new(claim)
+      .applicable_task_names
   end
 
   def pageable_tasks
@@ -82,9 +51,5 @@ class ClaimCheckingTasks
 
   def task_names_for_claim
     claim.tasks.pluck(:name)
-  end
-
-  def matching_claims
-    @matching_claims ||= Claim::MatchingAttributeFinder.new(claim).matching_claims
   end
 end
