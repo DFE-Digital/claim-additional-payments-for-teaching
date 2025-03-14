221efec89558f1e2a4a2750ddfe6cca682af44ed
Phil Lee <asmega@users.noreply.github.com>
2025-03-14 11:06:11 +0000

    implement claim check tasks for each policy

---
+++ app/models/policies/early_career_payments/claim_checking_tasks.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Policies
+  module EarlyCareerPayments
+    class ClaimCheckingTasks
+      attr_reader :claim
+
+      def initialize(claim)
+        @claim = claim
+      end
+
+      delegate :policy, to: :claim
+
+      def applicable_task_names
+        tasks = []
+
+        tasks << "identity_confirmation"
+        tasks << "qualifications"
+        tasks << "induction_confirmation"
+        tasks << "census_subjects_taught"
+        tasks << "employment"
+        tasks << "student_loan_plan" if claim.submitted_without_slc_data?
+        tasks << "payroll_details" if claim.must_manually_validate_bank_details?
+        tasks << "matching_details" if matching_claims.exists?
+        tasks << "payroll_gender" if claim.payroll_gender_missing? || claim.tasks.exists?(name: "payroll_gender")
+
+        tasks
+      end
+
+      private
+
+      def matching_claims
+        @matching_claims ||= Claim::MatchingAttributeFinder.new(claim).matching_claims
+      end
+    end
+  end
+end
---
+++ app/models/policies/targeted_retention_incentive_payments/claim_checking_tasks.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Policies
+  module TargetedRetentionIncentivePayments
+    class ClaimCheckingTasks
+      attr_reader :claim
+
+      def initialize(claim)
+        @claim = claim
+      end
+
+      delegate :policy, to: :claim
+
+      def applicable_task_names
+        tasks = []
+
+        tasks << "identity_confirmation"
+        tasks << "qualifications"
+        tasks << "census_subjects_taught"
+        tasks << "employment"
+        tasks << "student_loan_plan" if claim.submitted_without_slc_data?
+        tasks << "payroll_details" if claim.must_manually_validate_bank_details?
+        tasks << "matching_details" if matching_claims.exists?
+        tasks << "payroll_gender" if claim.payroll_gender_missing? || claim.tasks.exists?(name: "payroll_gender")
+
+        tasks
+      end
+
+      private
+
+      def matching_claims
+        @matching_claims ||= Claim::MatchingAttributeFinder.new(claim).matching_claims
+      end
+    end
+  end
+end
