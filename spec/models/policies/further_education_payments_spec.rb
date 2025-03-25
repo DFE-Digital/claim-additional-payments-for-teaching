caa3fc05c376a7e46d9c5b26bd19b172c5d7c962
Richard Lynch <rjlynchdev@gmail.com>
2025-03-25 11:36:33 +0000

    Add atl idv rejection reason
    We want a new rejection reason for claims that have failed alternative
    identity verification. We only want this rejection reason to be
    avalabile after 31st March.
    The tricky bit is any rejection reasons we add to the notify template
    need to be sent for all rejection emails. To handle this we only include
    the rejection reason in the list of reasons admins see if the claim
    requires alternative identity verification (which is feature flagged)
    however the `Decision#rejected_reasons_hash` which is what is sent to
    notify unconditionally includes this reason.
    
    The `FurtherEducationPayments` policy class now has quite a few methods
    that take a `Claim` as their only argument, while this works fine and is
    pretty maintainable, it hints that we're missing some object.

--- spec/models/policies/further_education_payments_spec.rb
+++ spec/models/policies/further_education_payments_spec.rb
@@ -6,4 +6,53 @@ RSpec.describe Policies::FurtherEducationPayments do
       expect(subject.payroll_file_name).to eql("FELUPEXPANSION")
     end
   end
+
+  describe ".rejected_reasons" do
+    before do
+      FeatureFlag.create!(
+        name: "fe_provider_identity_verification",
+        enabled: true
+      )
+    end
+
+    subject { described_class.rejected_reasons(claim) }
+
+    describe "alternative_identity_verification_check_failed" do
+      context "when the claim requires alternative identity verification" do
+        let(:claim) do
+          create(
+            :claim,
+            :submitted,
+            policy: described_class,
+            onelogin_idv_at: 1.day.ago,
+            identity_confirmed_with_onelogin: false
+          )
+        end
+
+        it do
+          is_expected.to include(
+            :alternative_identity_verification_check_failed
+          )
+        end
+      end
+
+      context "when the claim does not require alternative identity verification" do
+        let(:claim) do
+          create(
+            :claim,
+            :submitted,
+            policy: described_class,
+            onelogin_idv_at: 1.day.ago,
+            identity_confirmed_with_onelogin: true
+          )
+        end
+
+        it do
+          is_expected.not_to include(
+            :alternative_identity_verification_check_failed
+          )
+        end
+      end
+    end
+  end
 end
