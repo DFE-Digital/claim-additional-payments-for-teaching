70c0e5e42d218565558de890806da4fe99da01d6
Phil Lee <asmega@users.noreply.github.com>
2025-03-31 12:05:47 +0100

    EY consent form is now HTML

--- app/controllers/journeys/early_years_payment/provider/start/static_pages_controller.rb
+++ app/controllers/journeys/early_years_payment/provider/start/static_pages_controller.rb
@@ -7,6 +7,10 @@ module Journeys
             @journey_open = journey.accessible?(params[:service_access_code])
             render "#{journey::VIEW_PATH}/guidance"
           end
+
+          def consent_form
+            render "#{journey::VIEW_PATH}/consent_form", layout: "bare"
+          end
         end
       end
     end
