6d6dcb9333f51011f755c3d4212236f53025b546
Phil Lee <asmega@users.noreply.github.com>
2025-04-11 16:07:31 +0100

    solid queue migration considers mail jobs

--- db/migrate/20250107154328_solid_queue_jobs.rb
+++ db/migrate/20250107154328_solid_queue_jobs.rb
@@ -6,11 +6,18 @@ class SolidQueueJobs < ActiveRecord::Migration[8.0]
     # migrate existing delayed jobs to solid queue
     Delayed::Job.all.each do |dj|
       klass = dj.payload_object.job_data["job_class"].constantize
-      args = dj.payload_object.job_data["arguments"].map do |hash|
-        hash.values.map { |v| GlobalID::Locator.locate(v) }
-      end.flatten
 
-      klass.perform_later(args)
+      if klass == ActionMailer::MailDeliveryJob
+        mailer_class = dj.payload_object.job_data["arguments"][0].constantize
+        mailer_method = dj.payload_object.job_data["arguments"][1]
+        mailer_args = ActiveJob::Arguments.deserialize(dj.payload_object.job_data["arguments"][3]["args"])[0]
+
+        mailer_class.public_send(mailer_method, mailer_args).deliver_later
+      else
+        args = ActiveJob::Arguments.deserialize dj.payload_object.job_data["arguments"]
+
+        klass.perform_later(*args)
+      end
     end
   end
 
