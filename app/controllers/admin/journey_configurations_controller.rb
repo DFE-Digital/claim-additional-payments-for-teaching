fb11fdaf71a5bbf241bceec7e7da1af23ab21d16
Richard Lynch <rjlynchdev@gmail.com>
2025-04-23 16:31:45 +0100

    Move tri award upload to tri journey config
    We'll be removing additional payment soon but we still need to be able
    to upload tri awards.

--- app/controllers/admin/journey_configurations_controller.rb
+++ app/controllers/admin/journey_configurations_controller.rb
@@ -5,7 +5,7 @@ module Admin
     after_action :send_reminders, only: [:update]
 
     FILE_UPLOAD_TARGET_DATA_MODELS = {
-      "additional-payments" => Policies::TargetedRetentionIncentivePayments::Award,
+      "targeted-retention-incentive-payments" => Policies::TargetedRetentionIncentivePayments::Award,
       "early-years-payment-provider" => EligibleEyProvider,
       "further-education-payments" => EligibleFeProvider
     }
@@ -15,7 +15,7 @@ module Admin
     end
 
     def edit
-      @csv_upload = Policies::TargetedRetentionIncentivePayments::AwardCsvImporter.new(admin_user:) if journey_configuration.additional_payments?
+      @csv_upload = Policies::TargetedRetentionIncentivePayments::AwardCsvImporter.new(admin_user:) if journey_configuration.targeted_retention_incentive_payments?
 
       @upload_form = EligibleFeProvidersForm.new(upload_params, admin_user)
       @download_form = EligibleFeProvidersForm.new({}, admin_user)
