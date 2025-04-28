b7237d94354ac260146112668f2508b22c32a874
Richard Lynch <rjlynchdev@gmail.com>
2025-04-28 16:50:04 +0100

    Handle teacher id being disabled.
    Only add signin in or continue slug if teacher id is enabled. The other
    teacher id related slugs wont be in the sequence if the claimant doesn't
    go through tid.

--- spec/features/combined_teacher_claim_journey_with_teacher_id_disabled_spec.rb
+++ spec/features/combined_teacher_claim_journey_with_teacher_id_disabled_spec.rb
@@ -1,18 +1,24 @@
 require "rails_helper"
 
 RSpec.feature "Combined journey" do
-  let!(:journey_configuration) { create(:journey_configuration, :additional_payments, teacher_id_enabled: false) }
+  before do
+    FeatureFlag.enable!(:tri_only_journey)
+  end
+
+  let!(:journey_configuration) { create(:journey_configuration, :targeted_retention_incentive_payments_only, teacher_id_enabled: false) }
 
   scenario "Teacher ID is disabled on the policy configuration" do
-    visit landing_page_path(Journeys::AdditionalPaymentsForTeaching::ROUTING_NAME)
+    visit landing_page_path(Journeys::TargetedRetentionIncentivePayments::ROUTING_NAME)
 
     # - Landing (start)
-    expect(page).to have_text(I18n.t("additional_payments.landing_page"))
+    expect(page).to have_text(
+      "Find out if you are eligible for a targeted retention incentive payment"
+    )
     click_on "Start now"
 
     # - Which school do you teach at
-    expect(page).to have_text(I18n.t("additional_payments.forms.current_school.questions.current_school_search"))
-    expect(page.title).to have_text(I18n.t("additional_payments.forms.current_school.questions.current_school_search"))
+    expect(page).to have_text("Which school do you teach at?")
+    expect(page.title).to have_text("Which school do you teach at?")
 
     expect(page).not_to have_link "Back"
   end
