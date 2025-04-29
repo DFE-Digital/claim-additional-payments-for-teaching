bd3d2ace0a355b06413514ce7d7898ff5d1030e1
Richard Lynch <rjlynchdev@gmail.com>
2025-04-29 14:50:18 +0100

    Update spec to work with TRI only

--- spec/features/sign_in_or_continue_spec.rb
+++ spec/features/sign_in_or_continue_spec.rb
@@ -4,8 +4,8 @@ RSpec.feature "Teacher Identity Sign in" do
   include OmniauthMockHelper
 
   # create a school eligible for ECP and Targeted Retention Incentive so can walk the whole journey
-  let!(:journey_configuration) { create(:journey_configuration, :additional_payments) }
-  let!(:school) { create(:school, :combined_journey_eligibile_for_all) }
+  let(:journey_configuration) { create(:journey_configuration, :targeted_retention_incentive_payments_only) }
+  let(:school) { create(:school, :targeted_retention_incentive_payments_eligible) }
   let(:current_academic_year) { journey_configuration.current_academic_year }
   let(:trn) { 1234567 }
   let(:date_of_birth) { "1981-01-01" }
@@ -14,17 +14,20 @@ RSpec.feature "Teacher Identity Sign in" do
   let(:itt_year) { current_academic_year - 3 }
 
   before do
+    FeatureFlag.enable!(:tri_only_journey)
+    journey_configuration
+    school
     set_mock_auth(trn, {date_of_birth:, nino:})
     stub_dqt_empty_response(trn:, params: {birthdate: date_of_birth, nino:})
   end
 
-  scenario "Teacher makes claim for 'Early-Career Payments' claim and select continue without signing in" do
-    visit landing_page_path(Journeys::AdditionalPaymentsForTeaching::ROUTING_NAME)
-    expect(page).to have_link("Claim additional payments for teaching", href: "/additional-payments/landing-page")
-    expect(page).to have_link(href: "mailto:#{I18n.t("additional_payments.feedback_email")}")
+  scenario "Teacher makes claim for 'Targeted Retention Incentive Payments' claim and select continue without signing in" do
+    visit landing_page_path(Journeys::TargetedRetentionIncentivePayments::ROUTING_NAME)
+    expect(page).to have_link("Claim a targeted retention incentive payment", href: "/targeted-retention-incentive-payments/landing-page")
+    expect(page).to have_link(href: "mailto:additionalteachingpayment@digital.education.gov.uk")
 
     # - Landing (start)
-    expect(page).to have_text(I18n.t("additional_payments.landing_page"))
+    expect(page).to have_text("Find out if you are eligible for a targeted retention incentive payment")
     click_on "Start now"
 
     # - Sign in or continue page
@@ -32,17 +35,17 @@ RSpec.feature "Teacher Identity Sign in" do
     click_on "Continue without signing in"
 
     # - Which school do you teach at
-    expect(page).to have_text(I18n.t("additional_payments.forms.current_school.questions.current_school_search"))
-    expect(page.title).to have_text(I18n.t("additional_payments.forms.current_school.questions.current_school_search"))
+    expect(page).to have_text("Which school do you teach at?")
+    expect(page.title).to have_text("Which school do you teach at?")
   end
 
-  scenario "Teacher makes claim for 'Early-Career Payments' claim and select Continue with DfE Identity" do
-    visit landing_page_path(Journeys::AdditionalPaymentsForTeaching::ROUTING_NAME)
-    expect(page).to have_link("Claim additional payments for teaching", href: "/additional-payments/landing-page")
-    expect(page).to have_link(href: "mailto:#{I18n.t("additional_payments.feedback_email")}")
+  scenario "Teacher makes claim for 'Targeted Retention Incentive Payments' claim and select Continue with DfE Identity" do
+    visit landing_page_path(Journeys::TargetedRetentionIncentivePayments::ROUTING_NAME)
+    expect(page).to have_link("Claim a targeted retention incentive payment", href: "/targeted-retention-incentive-payments/landing-page")
+    expect(page).to have_link(href: "mailto:additionalteachingpayment@digital.education.gov.uk")
 
     # - Landing (start)
-    expect(page).to have_text(I18n.t("additional_payments.landing_page"))
+    expect(page).to have_text("Find out if you are eligible for a targeted retention incentive payment")
     click_on "Start now"
 
     # - Sign in or continue page
@@ -50,14 +53,14 @@ RSpec.feature "Teacher Identity Sign in" do
     click_on "Continue with DfE Identity"
 
     # - Teacher details page
-    expect(page).to have_text(I18n.t("questions.check_and_confirm_details"))
-    expect(page).to have_text(I18n.t("questions.details_correct"))
+    expect(page).to have_text("Check and confirm your personal details")
+    expect(page).to have_text("Are these details correct?")
 
     choose "Yes"
     click_on "Continue"
 
     # - Which school do you teach at
-    expect(page).to have_text(I18n.t("additional_payments.forms.current_school.questions.current_school_search"))
-    expect(page.title).to have_text(I18n.t("additional_payments.forms.current_school.questions.current_school_search"))
+    expect(page).to have_text("Which school do you teach at?")
+    expect(page.title).to have_text("Which school do you teach at?")
   end
 end
