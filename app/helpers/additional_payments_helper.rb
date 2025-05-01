af63c5c112dee4fdf10f08137252bd5093f95225
Richard Lynch <rjlynchdev@gmail.com>
2025-05-01 09:49:11 +0100

    Remove eligible_itt_subject_translation
    This method is no longer in use

--- app/helpers/additional_payments_helper.rb
+++ app/helpers/additional_payments_helper.rb
@@ -4,30 +4,4 @@ module AdditionalPaymentsHelper
   def one_time_password_validity_duration
     pluralize(OneTimePassword::Base::DRIFT / 60, "minute")
   end
-
-  def eligible_itt_subject_translation(answers, subjects)
-    if answers.trainee_teacher?
-      return I18n.t("additional_payments.forms.eligible_itt_subject.questions.which_subject_trainee_teacher")
-    end
-
-    qualification_symbol = answers.qualification.to_sym
-
-    if subjects.many?
-      I18n.t("additional_payments.forms.eligible_itt_subject.questions.which_subject", qualification: qualification_to_substring(qualification_symbol))
-    else
-      subject_symbol = subjects.first
-      I18n.t("additional_payments.forms.eligible_itt_subject.questions.single_subject", qualification: qualification_to_substring(qualification_symbol), subject: subject_symbol)
-    end
-  end
-
-  private
-
-  def qualification_to_substring(qualification_symbol)
-    {
-      undergraduate_itt: "undergraduate initial teacher training (ITT)",
-      postgraduate_itt: "postgraduate initial teacher training (ITT)",
-      assessment_only: "assessment",
-      overseas_recognition: "teaching qualification"
-    }[qualification_symbol]
-  end
 end
