98d7648e39342204cf63c4c778d870a196b7e55f
Richard Lynch <rjlynchdev@gmail.com>
2025-05-01 15:18:23 +0100

    Remove some methods from Additional payments
    Will remove the class in a future commit

--- app/models/journeys/additional_payments_for_teaching.rb
+++ app/models/journeys/additional_payments_for_teaching.rb
@@ -20,28 +20,5 @@ module Journeys
         POLICIES
       end
     end
-
-    def set_a_reminder?(itt_academic_year:, policy:)
-      policy_year = configuration.current_academic_year
-      return false if policy_year >= policy::POLICY_END_YEAR
-
-      next_year = policy_year + 1
-      eligible_itt_years = selectable_itt_years_for_claim_year(next_year)
-      eligible_itt_years.include?(itt_academic_year)
-    end
-
-    def requires_student_loan_details?
-      true
-    end
-
-    def selectable_itt_years_for_claim_year(claim_year)
-      policies.flat_map do |policy|
-        policy.selectable_itt_years_for_claim_year(claim_year)
-      end.uniq
-    end
-
-    def uses_reminders?
-      true
-    end
   end
 end
