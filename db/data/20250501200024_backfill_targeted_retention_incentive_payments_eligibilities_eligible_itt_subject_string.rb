97e84602eb761b2d3c04667f5d1eec3db233f2d8
Kenneth Lee <kenfodder@gmail.com>
2025-05-05 21:13:24 +0100

    CAPT-1474 - TRI - Copy eligible_itt_subject enum to eligible_itt_subject_string

---
+++ db/data/20250501200024_backfill_targeted_retention_incentive_payments_eligibilities_eligible_itt_subject_string.rb
@@ -0,0 +1,37 @@
+# Run me with `rails runner db/data/20250501200024_backfill_targeted_retention_incentive_payments_eligibilities_eligible_itt_subject_string.rb`
+
+# NOTE: It's OK to do `update_all` as the `eligible_itt_subject_string` field is in the `analytics_blocklist.yml`
+
+ELIGIBLE_ITT_SUBJECTS = %w[
+  chemistry
+  foreign_languages
+  mathematics
+  physics
+  none_of_the_above
+  computing
+]
+
+# BEFORE
+ELIGIBLE_ITT_SUBJECTS.each do |eligible_itt_subject|
+  count = Policies::TargetedRetentionIncentivePayments::Eligibility
+    .where(eligible_itt_subject: eligible_itt_subject)
+    .count
+
+  puts "#{eligible_itt_subject} - count: #{count}"
+end
+
+# MIGRATE
+ELIGIBLE_ITT_SUBJECTS.each do |eligible_itt_subject|
+  Policies::TargetedRetentionIncentivePayments::Eligibility
+    .where(eligible_itt_subject: eligible_itt_subject)
+    .update_all(eligible_itt_subject_string: eligible_itt_subject)
+end
+
+# AFTER
+ELIGIBLE_ITT_SUBJECTS.each do |eligible_itt_subject|
+  count = Policies::TargetedRetentionIncentivePayments::Eligibility
+    .where(eligible_itt_subject_string: eligible_itt_subject)
+    .count
+
+  puts "#{eligible_itt_subject}_string - count: #{count}"
+end
