8f54f333be538b3bb3fe0bb046d43c7cae3ca340
Richard Lynch <rjlynchdev@gmail.com>
2025-05-07 17:01:36 +0100

    Remove factory registry
    Now we're not using ecp la codes we don't need this mechanisim to handle
    picking random ones

--- lib/factory_helpers.rb
+++
@@ -1,7 +0,0 @@
-module FactoryHelpers
-  extend ActiveSupport::Concern
-
-  def self.create_factory_registry
-    Thread.current[:factory_registry] = FactoryBot::Registry.new(:uniqueness_guarantor)
-  end
-end
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,18 +1,3 @@
-require "./lib/factory_helpers"
-
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
-
-  # This registry ensures the random values selected in factories e.g. using
-  # Array#sample are unique, to avoid validation and state errors. Any value
-  # used should be removed from the registry.
-  #
-  # Usage example:
-  #
-  # code { Thread.current[:factory_registry].find(:local_authority_district_ecp_uplift_codes).shuffle!.pop }
-  FactoryHelpers.create_factory_registry
-
-  config.before :each do
-    FactoryHelpers.reset_factory_registry
-  end
 end
