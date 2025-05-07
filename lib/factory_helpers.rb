27e1c6d63365b34165590a025a4a4c0139d08262
Richard Lynch <rjlynchdev@gmail.com>
2025-05-07 16:46:28 +0100

    Remove ECP school eligibility checker
    This code is no longer in use

--- lib/factory_helpers.rb
+++ lib/factory_helpers.rb
@@ -4,9 +4,4 @@ module FactoryHelpers
   def self.create_factory_registry
     Thread.current[:factory_registry] = FactoryBot::Registry.new(:uniqueness_guarantor)
   end
-
-  def self.reset_factory_registry
-    Thread.current[:factory_registry].register :local_authority_district_ecp_uplift_codes, Policies::EarlyCareerPayments::SchoolEligibility::UPLIFT_LOCAL_AUTHORITY_DISTRICT_CODES
-    Thread.current[:factory_registry].register :local_authority_district_ecp_eligible_codes, Policies::EarlyCareerPayments::SchoolEligibility::ELIGIBLE_LOCAL_AUTHORITY_DISTRICT_CODES
-  end
 end
