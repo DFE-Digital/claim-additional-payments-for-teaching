43ecab0e0f655c6fa8fdc45d944751c93cd087ba
Richard Lynch <rjlynchdev@gmail.com>
2025-05-08 11:07:46 +0100

    Remove journey configuration
    Creating a journey configuration isn't needed for most of these specs
    (only the tri one to get the awards). Creating the journey config in the
    shared setup make removing additional payments difficult.

--- spec/features/admin/admin_checks_targeted_retention_incentive_payments_claim_spec.rb
+++ spec/features/admin/admin_checks_targeted_retention_incentive_payments_claim_spec.rb
@@ -1,5 +1,9 @@
 require "rails_helper"
 
 RSpec.feature "Admin checks a Levelling Up Premium Payments claim" do
+  before do
+    create(:journey_configuration, :targeted_retention_incentive_payments)
+  end
+
   it_behaves_like "Admin Checks", Policies::TargetedRetentionIncentivePayments
 end
