8c82c35c7d39c4372899c0bef70459562cd70aa1
Phil Lee <asmega@users.noreply.github.com>
2025-05-09 10:58:26 +0100

    EY practitioner journey supports navigator

--- spec/features/early_years_payment/practitioner/email_address_spec.rb
+++ spec/features/early_years_payment/practitioner/email_address_spec.rb
@@ -26,24 +26,6 @@ RSpec.feature "Early years payment practitioner email address" do
     expect(ActionMailer::Base.deliveries.count).to eq 2
   end
 
-  scenario "Resend passcode for verified email" do
-    when_early_years_payment_practitioner_journey_configuration_exists
-    when_personal_details_entered_up_to_address
-
-    fill_in "claim-email-address-field", with: "johndoe@example.com"
-    click_on "Continue"
-    mail = ActionMailer::Base.deliveries.last
-    otp_in_mail_sent = mail.personalisation[:one_time_password]
-    fill_in "claim-one-time-password-field", with: otp_in_mail_sent
-    click_on "Confirm"
-
-    click_link "Back"
-    click_link "Resend passcode"
-    click_on "Continue"
-
-    expect(ActionMailer::Base.deliveries.count).to eq 2
-  end
-
   scenario "Change email address for unverified email" do
     when_early_years_payment_practitioner_journey_configuration_exists
     when_personal_details_entered_up_to_address
