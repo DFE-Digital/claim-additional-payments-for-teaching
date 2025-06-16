0e4d8493cc6f0980fc5d8b912bd4a51e122c987b
Phil Lee <asmega@users.noreply.github.com>
2025-06-16 15:31:01 +0100

    remove submissions controller for IRP

--- app/models/journeys/get_a_teacher_relocation_payment/slug_sequence.rb
+++ app/models/journeys/get_a_teacher_relocation_payment/slug_sequence.rb
@@ -28,6 +28,7 @@ module Journeys
         "personal-bank-account",
         "gender",
         "check-your-answers",
+        "confirmation",
         "ineligible"
       ].freeze
 
@@ -109,7 +110,10 @@ module Journeys
       end
 
       def results
-        ["check-your-answers"]
+        %w[
+          check-your-answers
+          confirmation
+        ]
       end
 
       def address_set_by_postcode_search?
