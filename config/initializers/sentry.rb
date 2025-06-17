8d5f9d09d49ea8851a745a8a881488e5dd0f4aad
Phil Lee <asmega@users.noreply.github.com>
2025-06-17 16:45:50 +0100

    config sentry to know about git SHA for releases

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,4 +4,5 @@ Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger]
   config.dsn = ENV["SENTRY_DSN"]
   config.traces_sample_rate = 1.0
+  config.release = ENV["GIT_COMMIT_HASH"]
 end
