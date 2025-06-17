b4d123ee1afd57a6f57995372605c0003a0a4179
Phil Lee <asmega@users.noreply.github.com>
2025-06-17 16:53:28 +0100

    enable sentry tracing

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,4 +5,22 @@ Sentry.init do |config|
   config.dsn = ENV["SENTRY_DSN"]
   config.traces_sample_rate = 1.0
   config.release = ENV["GIT_COMMIT_HASH"]
+
+  config.traces_sampler = lambda do |sampling_context|
+    transaction_context = sampling_context[:transaction_context]
+    op = transaction_context[:op]
+    transaction_name = transaction_context[:name]
+
+    case op
+    when /request/
+      case transaction_name
+      when /healthcheck/
+        0.0 # ignore healthcheck requests
+      else
+        0.01
+      end
+    else
+      0.01
+    end
+  end
 end
