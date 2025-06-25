fa9e43f7dd1bc3a841a2b45f29c8d9bdfeb54e8e
Phil Lee <asmega@users.noreply.github.com>
2025-06-25 11:17:28 +0100

    move dfe sign in file from lib => models

--- config/initializers/dfe_sign_in.rb
+++ config/initializers/dfe_sign_in.rb
@@ -1,3 +1,5 @@
+require "dfe_sign_in"
+
 DfeSignIn.configure do |config|
   config.client_id = ENV.fetch("DFE_SIGN_IN_API_CLIENT_ID")
   config.secret = Rails.env.production? ? ENV.fetch("DFE_SIGN_IN_API_SECRET") : ENV.fetch("DFE_SIGN_IN_API_SECRET", "secret")
