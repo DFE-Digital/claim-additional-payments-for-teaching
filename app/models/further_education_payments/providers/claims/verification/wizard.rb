9e7930dd1eced1fed020747aad6fabf0ca05d9d2
Richard Lynch <rjlynchdev@gmail.com>
2025-07-01 14:15:01 +0100

    Extract wizard management
    The conditionals in the journey are getting a little more complex so we
    want to extract something to manage what form to render next. The
    `Wizard` is like a combination of slug sequence and navigator. It
    decides what forms are part of the journey based on the eligibility and
    then returns the first incomplete form.
    It also handles clearing answers set on the eligibility from forms that
    are now no longer reachable (eg when the user goes down a new branch
    after chaning an answer), though this functionality isn't strictly
    needed until we implement the change links on the check answers page.

---
+++ app/models/further_education_payments/providers/claims/verification/wizard.rb
@@ -0,0 +1,76 @@
+module FurtherEducationPayments
+  module Providers
+    module Claims
+      module Verification
+        class Wizard
+          FORMS = [
+            RoleAndExperienceForm,
+            ContractCoversFullAcademicYearForm,
+            CheckAnswersForm
+          ]
+
+          def initialize(claim:, user:)
+            @claim = claim
+            @user = user
+          end
+
+          def current_form
+            reachable_forms.detect(&:incomplete?)
+          end
+
+          def clear_impermissible_answers!
+            ApplicationRecord.transaction do
+              unreachable_forms.each(&:clear_answers!)
+            end
+          end
+
+          private
+
+          attr_reader :claim, :user, :forms
+
+          delegate :eligibility, to: :claim
+
+          def reachable_forms
+            @reachable_forms ||= build_forms(reachable_steps)
+          end
+
+          def unreachable_forms
+            @unreachable_forms ||= build_forms(unreachable_steps)
+          end
+
+          def reachable_steps
+            return @reachable_steps if @reachable_steps
+
+            @reachable_steps = []
+
+            @reachable_steps << RoleAndExperienceForm
+
+            if eligibility.provider_verification_contract_type == "fixed_term"
+              @reachable_steps << ContractCoversFullAcademicYearForm
+            end
+
+            @reachable_steps << CheckAnswersForm
+
+            @reachable_steps
+          end
+
+          def unreachable_steps
+            FORMS - reachable_steps
+          end
+
+          def build_forms(form_classes)
+            form_classes.map do |form_class|
+              form_class.new(
+                claim: claim,
+                user: user,
+                params: claim.eligibility.attributes.slice(
+                  *form_class.attribute_names.map(&:to_s)
+                )
+              )
+            end
+          end
+        end
+      end
+    end
+  end
+end
