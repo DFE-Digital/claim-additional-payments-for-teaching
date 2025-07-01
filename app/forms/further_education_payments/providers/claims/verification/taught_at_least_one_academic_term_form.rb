963b7604c8ad6e999eb920dc362fa0eaa094074e
Richard Lynch <rjlynchdev@gmail.com>
2025-07-01 16:59:24 +0100

    Add variable hours screen
    If the verifier selects that the claimant has a variable hours contract
    we need to capture whether these hours cover the current academic year.

---
+++ app/forms/further_education_payments/providers/claims/verification/taught_at_least_one_academic_term_form.rb
@@ -0,0 +1,45 @@
+module FurtherEducationPayments
+  module Providers
+    module Claims
+      module Verification
+        class TaughtAtLeastOneAcademicTermForm < BaseForm
+          attribute(
+            :provider_verification_taught_at_least_one_academic_term,
+            :boolean
+          )
+
+          attribute :section_completed, :boolean
+
+          validates(
+            :provider_verification_taught_at_least_one_academic_term,
+            inclusion: {
+              in: ->(form) do
+                form.taught_at_least_one_academic_term_options.map(&:id)
+              end
+            }
+          )
+
+          def taught_at_least_one_academic_term_options
+            [
+              Form::Option.new(id: true, name: "Yes"),
+              Form::Option.new(id: false, name: "No")
+            ]
+          end
+
+          def section_completed_options
+            [
+              Form::Option.new(
+                id: true,
+                name: "Yes"
+              ),
+              Form::Option.new(
+                id: false,
+                name: "No, I want to come back to it later"
+              )
+            ]
+          end
+        end
+      end
+    end
+  end
+end
