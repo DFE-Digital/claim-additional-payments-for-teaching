c818251107797331dd4703125a63b4090494ec43
Richard Lynch <rjlynchdev@gmail.com>
2025-07-09 15:27:21 +0100

    Add contracted hours form
    A bit tricky as we need to display the course names to the provider,
    this is made much easier if we use the use the same option names as we
    do for the claimant.

--- app/forms/further_education_payments/providers/claims/verification/check_answers_form.rb
+++ app/forms/further_education_payments/providers/claims/verification/check_answers_form.rb
@@ -16,7 +16,9 @@ module FurtherEducationPayments
             :provider_verification_taught_at_least_one_academic_term,
             :provider_verification_performance_measures,
             :provider_verification_disciplinary_action,
-            :provider_verification_performance_section_completed,
+            :provider_verification_teaching_hours_per_week,
+            :provider_verification_half_teaching_hours,
+            :provider_verification_subjects_taught,
             to: :eligibility
           )
 
@@ -27,6 +29,20 @@ module FurtherEducationPayments
             when "permanent" then "Permanent"
             else fail "Unknown contract type"
             end
+          end
+
+          def subject_names_sentence
+            claim.eligibility.subjects_taught.map do |subject|
+              I18n.t(
+                subject,
+                scope: %w[
+                  further_education_payments
+                  forms
+                  subjects_taught
+                  options
+                ]
+              )
+            end.map(&:downcase).to_sentence
           end
         end
       end
