57b96744b581c507a112db627b5176574f8f1dab
Raul Gracia <raulgracialario@gmail.com>
2025-07-21 13:00:50 +0100

    Update Dashboard Numbers
    Update controller to use new verification timestamp to show the
    dashboard numbers.

--- app/controllers/further_education_payments/providers/claims_controller.rb
+++ app/controllers/further_education_payments/providers/claims_controller.rb
@@ -3,6 +3,8 @@ module FurtherEducationPayments
     class ClaimsController < BaseController
       def index
         @claims = claim_scope
+        @not_started_count = @claims.joins(:eligibility).where(eligibility: {provider_verification_started_at: nil}).count
+        @in_progress_count = @claims.joins(:eligibility).where.not(eligibility: {provider_verification_started_at: nil}).count
       end
 
       private
