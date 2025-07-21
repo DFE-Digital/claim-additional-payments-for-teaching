bf6c50f050131f5ec35b8c867f2fa6fd490851a8
Richard Lynch <rjlynchdev@gmail.com>
2025-07-21 14:39:37 +0100

    Add additional question for variable hours
    Adds an additional question for variable hours contracts.
    We've left in a place holder we'll need to populate once we add the new
    question to the claimant journey.

---
+++ app/forms/further_education_payments/providers/claims/verification/timetabled_teaching_hours_form.rb
@@ -0,0 +1,37 @@
+module FurtherEducationPayments
+  module Providers
+    module Claims
+      module Verification
+        class TimetabledTeachingHoursForm < BaseForm
+          attribute :provider_verification_timetabled_teaching_hours, :boolean
+
+          validates(
+            :provider_verification_timetabled_teaching_hours,
+            included: {
+              in: ->(form) do
+                form.provider_verification_timetabled_teaching_hours_options.map(&:id)
+              end,
+              message: ->(form, _) do
+                "Tell us if they are timetabled to teach at least 2.5 hours " \
+                "per week in the #{form.term} term"
+              end
+            },
+            allow_nil: :save_and_exit?
+          )
+
+          def provider_verification_timetabled_teaching_hours_options
+            [
+              Form::Option.new(id: true, name: "Yes"),
+              Form::Option.new(id: false, name: "No")
+            ]
+          end
+
+          def term
+            # FIXME - placeholder until CAPT-2711 is implemented
+            "[spring_or_summer]"
+          end
+        end
+      end
+    end
+  end
+end
