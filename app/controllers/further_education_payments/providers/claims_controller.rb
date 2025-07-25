5508f46a5dfdd4f0ec16e4fe6ce0eb6eb852fef2
Richard Lynch <rjlynchdev@gmail.com>
2025-07-25 11:27:43 +0100

    Order unverified claims
    Updates the claims controller to order the list of claims

--- app/controllers/further_education_payments/providers/claims_controller.rb
+++ app/controllers/further_education_payments/providers/claims_controller.rb
@@ -2,13 +2,19 @@ module FurtherEducationPayments
   module Providers
     class ClaimsController < BaseController
       def index
-        @claims = claim_scope
-        @not_started_count = @claims
-          .joins("INNER JOIN further_education_payments_eligibilities ON further_education_payments_eligibilities.id = claims.eligibility_id")
+        @claims = claim_scope.order(
+          Arel.sql(
+            <<~SQL
+              CASE WHEN provider_verification_started_at IS NULL THEN 0 ELSE 1 END,
+              claims.created_at ASC
+            SQL
+          )
+        )
+
+        @not_started_count = claim_scope
           .where(further_education_payments_eligibilities: {provider_verification_started_at: nil})
           .count
-        @in_progress_count = @claims
-          .joins("INNER JOIN further_education_payments_eligibilities ON further_education_payments_eligibilities.id = claims.eligibility_id")
+        @in_progress_count = claim_scope
           .where.not(further_education_payments_eligibilities: {provider_verification_started_at: nil})
           .count
       end
@@ -16,7 +22,14 @@ module FurtherEducationPayments
       private
 
       def claim_scope
-        super.where(id: Claim.fe_provider_unverified.select(:id))
+        super
+          .where(id: Claim.fe_provider_unverified.select(:id))
+          .joins(
+            <<~SQL
+              INNER JOIN further_education_payments_eligibilities
+              ON further_education_payments_eligibilities.id = claims.eligibility_id
+            SQL
+          )
       end
     end
   end
