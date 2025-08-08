4966fc256177392599c28b6a0bbc26a6460df3c1
Richard Lynch <rjlynchdev@gmail.com>
2025-08-08 15:58:23 +0100

    Branch on information param
    We're going to need to show some additional pages in this action

--- app/controllers/further_education_payments/providers/claims/information_controller.rb
+++ app/controllers/further_education_payments/providers/claims/information_controller.rb
@@ -3,6 +3,12 @@ module FurtherEducationPayments
     module Claims
       class InformationController < BaseController
         def show
+          case params[:information]
+          when "progress_saved"
+            render "progress_saved"
+          else
+            redirect_to further_education_payments_providers_claims_path
+          end
         end
       end
     end
