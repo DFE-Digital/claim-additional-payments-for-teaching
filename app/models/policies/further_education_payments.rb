68a566dc888092fd8150156c3a02c22126a981cb
Richard Lynch <rjlynchdev@gmail.com>
2025-08-18 16:17:32 +0100

    Retain required FE data for full policy

--- app/models/policies/further_education_payments.rb
+++ app/models/policies/further_education_payments.rb
@@ -40,23 +40,10 @@ module Policies
     # Attributes to delete from claims submitted before the current academic
     # year
     PERSONAL_DATA_ATTRIBUTES_TO_DELETE = [
-      :first_name,
-      :middle_name,
-      :surname,
-      :date_of_birth,
-      :address_line_1,
-      :address_line_2,
-      :address_line_3,
-      :address_line_4,
-      :postcode,
-      :payroll_gender,
-      :national_insurance_number,
       :bank_sort_code,
       :bank_account_number,
       :building_society_roll_number,
       :banking_name,
-      :hmrc_bank_validation_responses,
-      :mobile_number,
       :teacher_id_user_info,
       :dqt_teacher_status,
       :claimant_date_of_birth,
@@ -67,11 +54,26 @@ module Policies
     ]
 
     # Attributes to retain on submitted claims until EXTENDED_PERIOD_END_DATE
-    PERSONAL_DATA_ATTRIBUTES_TO_RETAIN_FOR_EXTENDED_PERIOD = []
+    PERSONAL_DATA_ATTRIBUTES_TO_RETAIN_FOR_EXTENDED_PERIOD = [
+      :first_name,
+      :middle_name,
+      :surname,
+      :date_of_birth,
+      :address_line_1,
+      :address_line_2,
+      :address_line_3,
+      :address_line_4,
+      :postcode,
+      :national_insurance_number,
+      :mobile_number,
+      :hmrc_bank_validation_responses,
+      :payroll_gender
+    ]
 
     # Claims from before this date will have their retained attributes deleted
-    # NOOP as PERSONAL_DATA_ATTRIBUTES_TO_RETAIN_FOR_EXTENDED_PERIOD is empty
-    EXTENDED_PERIOD_END_DATE = ->(start_of_academic_year) {}
+    EXTENDED_PERIOD_END_DATE = ->(start_of_academic_year) {
+      start_of_academic_year - 5.years
+    }
 
     def notify_reply_to_id
       "89939786-7078-4267-b197-ee505dfad8ae"
