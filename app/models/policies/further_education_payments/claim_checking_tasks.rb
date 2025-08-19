b0173712f11b92a2eaa83727a2f9485aa59202f1
Phil Lee <asmega@users.noreply.github.com>
2025-08-19 11:15:29 +0100

    scope task to FE only

--- app/models/policies/further_education_payments/claim_checking_tasks.rb
+++ app/models/policies/further_education_payments/claim_checking_tasks.rb
@@ -18,7 +18,7 @@ module Policies
         tasks << "provider_verification"
         tasks << "provider_details" if claim.eligibility.provider_and_claimant_details_match?
         tasks << "alternative_identity_verification" if show_alternative_identity_verification_task?
-        tasks << "alternative_verification" if show_alternative_verification_task?
+        tasks << "fe_alternative_verification" if show_alternative_verification_task?
         tasks << "employment" if claim.eligibility.teacher_reference_number.present?
         tasks << "student_loan_plan" if claim.submitted_without_slc_data?
         tasks << "payroll_details" if claim.must_manually_validate_bank_details?
