10adf1a04edf1fb2bcaed307f09087a96a8576f7
Phil Lee <asmega@users.noreply.github.com>
2025-09-11 12:42:59 +0100

    expire journey sessions

---
+++ app/jobs/expire_journey_sessions_job.rb
@@ -0,0 +1,13 @@
+# expire journey sessions so they are no longer available to browsers
+# we still keep them persisted incase user wishes to resume
+class ExpireJourneySessionsJob < ApplicationJob
+  def perform
+    Journeys::JOURNEYS.each do |journey|
+      Rails.logger.info "Expiring #{journey::Session.purgeable.count} #{journey::Session} journeys from the database"
+      journey::Session
+        .where(journey: journey::ROUTING_NAME)
+        .expirable
+        .update_all(expired: true)
+    end
+  end
+end
