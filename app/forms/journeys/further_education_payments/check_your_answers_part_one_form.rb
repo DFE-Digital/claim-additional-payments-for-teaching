5f7b537804d072617d814aa8ada19e734d68eaa9
Phil Lee <asmega@users.noreply.github.com>
2025-09-15 12:49:22 +0100

    FE resumes correctly for 2 stateless pages

--- app/forms/journeys/further_education_payments/check_your_answers_part_one_form.rb
+++ app/forms/journeys/further_education_payments/check_your_answers_part_one_form.rb
@@ -2,7 +2,16 @@ module Journeys
   module FurtherEducationPayments
     class CheckYourAnswersPartOneForm < Form
       def save
-        true
+        journey_session
+          .answers
+          .assign_attributes(check_your_answers_part_one_completed: true)
+        journey_session.save!
+      end
+
+      def completed?
+        journey_session
+          .answers
+          .check_your_answers_part_one_completed
       end
     end
   end
