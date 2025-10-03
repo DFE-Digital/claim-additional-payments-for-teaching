6213fc568c7c1dd75d04372f0486860f0d1dfece
Phil Lee <asmega@users.noreply.github.com>
2025-10-03 10:54:20 +0100

    fix resume bug
    - where resume callback would always be fired

--- app/models/journeys/navigator.rb
+++ app/models/journeys/navigator.rb
@@ -72,7 +72,7 @@ module Journeys
 
           # check if want to resume an existing session
           # if so carry on where we left off
-          if form.resume?
+          if form.resume? && slug == current_slug
             journey_session.answers.before_resume_callback
 
             @forms = nil
