5b93a9df0a3f71247070e415ad266c8306f9ad9c
Kenneth Lee <kenfodder@gmail.com>
2025-10-06 18:43:26 +0100

    FE courses forms error messages

--- spec/forms/journeys/further_education_payments/chemistry_courses_form_spec.rb
+++ spec/forms/journeys/further_education_payments/chemistry_courses_form_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe Journeys::FurtherEducationPayments::ChemistryCoursesForm, type: :
         is_expected.not_to(
           allow_value([""])
           .for(:chemistry_courses)
-          .with_message("Select the courses that you teach, or select ‘I do not teach any of these courses’")
+          .with_message("Select the courses you teach")
         )
       end
     end
@@ -39,7 +39,7 @@ RSpec.describe Journeys::FurtherEducationPayments::ChemistryCoursesForm, type: :
         is_expected.not_to(
           allow_value(["foo"])
           .for(:chemistry_courses)
-          .with_message("Select the courses that you teach, or select ‘I do not teach any of these courses’")
+          .with_message("Select the courses you teach")
         )
       end
     end
