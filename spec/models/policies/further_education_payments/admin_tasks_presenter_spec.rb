395f9340228c1525e9f3bc7430d3f22906a314b3
Richard Lynch <rjlynchdev@gmail.com>
2025-10-06 16:39:26 +0100

    Update admin tasks to show FE qualification reason
    Updates the provider verification task to show the reason for not
    enrolling in a teaching qualification if applicable. The requirement is
    to show either "Valid reason" or "No valid reason" rather than the
    actually option selected. The actual value the provider selected is
    available on the view full claim page (backed by the
    eligibility_admin_answers_presenter)

--- spec/models/policies/further_education_payments/admin_tasks_presenter_spec.rb
+++ spec/models/policies/further_education_payments/admin_tasks_presenter_spec.rb
@@ -1,20 +1,20 @@
 require "rails_helper"
 
 RSpec.describe Policies::FurtherEducationPayments::AdminTasksPresenter do
-  subject do
-    described_class.new(claim)
-  end
-
-  let(:claim) do
-    build(
-      :claim,
-      :further_education,
-      eligibility:
-    )
-  end
-
   describe "#provider_verification_rows" do
     context "continued_employment" do
+      subject do
+        described_class.new(claim)
+      end
+
+      let(:claim) do
+        build(
+          :claim,
+          :further_education,
+          eligibility:
+        )
+      end
+
       context "when provider answers claimant is" do
         let(:eligibility) do
           build(
@@ -24,9 +24,9 @@ RSpec.describe Policies::FurtherEducationPayments::AdminTasksPresenter do
         end
 
         it "returns Yes" do
-          expect(subject.provider_verification_rows[9][0]).to eql("Continued employment")
-          expect(subject.provider_verification_rows[9][1]).to eql("N/A")
-          expect(subject.provider_verification_rows[9][2]).to eql("Yes")
+          expect(subject.provider_verification_rows[10][0]).to eql("Continued employment")
+          expect(subject.provider_verification_rows[10][1]).to eql("N/A")
+          expect(subject.provider_verification_rows[10][2]).to eql("Yes")
         end
       end
 
@@ -40,10 +40,99 @@ RSpec.describe Policies::FurtherEducationPayments::AdminTasksPresenter do
         end
 
         it "returns No" do
-          expect(subject.provider_verification_rows[9][0]).to eql("Continued employment")
-          expect(subject.provider_verification_rows[9][1]).to eql("N/A")
-          expect(subject.provider_verification_rows[9][2]).to eql("No")
+          expect(subject.provider_verification_rows[10][0]).to eql("Continued employment")
+          expect(subject.provider_verification_rows[10][1]).to eql("N/A")
+          expect(subject.provider_verification_rows[10][2]).to eql("No")
         end
+      end
+    end
+
+    context "when the provider has not submitted verification" do
+      it "returns an empty array" do
+        claim = create(
+          :claim,
+          :submitted,
+          policy: Policies::FurtherEducationPayments,
+          eligibility_attributes: {
+            provider_verification_completed_at: nil
+          }
+        )
+
+        expect(described_class.new(claim).provider_verification_rows).to eq []
+      end
+    end
+
+    context "when the provider has submitted verification" do
+      it "returns an array of verification rows" do
+        claim = create(
+          :claim,
+          :submitted,
+          policy: Policies::FurtherEducationPayments,
+          eligibility_trait: %i[eligible provider_verification_completed]
+        )
+
+        rows = described_class.new(claim).provider_verification_rows
+
+        expect(rows).to include(["Teaching responsibilities", "Yes", "Yes"])
+
+        expect(rows).to include([
+          "First 5 years of teaching",
+          AcademicYear.new(2023),
+          "Yes"
+        ])
+
+        expect(rows).to include(["Teaching qualification", "Yes", "Yes"])
+
+        expect(rows).to include([
+          "Contract of employment",
+          "Permanent",
+          "Fixed-term"
+        ])
+
+        expect(rows).to include([
+          "Timetabled teaching hours",
+          "More than 12 hours per week",
+          "20 hours or more per week"
+        ])
+
+        expect(rows).to include(["Age range taught", "Yes", "Yes"])
+
+        expect(rows).to include([
+          "Subject",
+          "Maths<br><br>Physics",
+          "Yes"
+        ])
+
+        expect(rows).to include([
+          "Course",
+          "Qualifications approved for funding at level 3 and below in the <a class=\"govuk-link\" target=\"_blank\" rel=\"noreferrer noopener\" href=\"https://www.qualifications.education.gov.uk/Search?Status=All&amp;Level=0,1,2,3,4&amp;Sub=28&amp;PageSize=10&amp;Sort=Status\">mathematics and statistics (opens in new tab)</a> sector subject area<br><br>GCSE in maths, functional skills qualifications and <a class=\"govuk-link\" target=\"_blank\" rel=\"noreferrer noopener\" href=\"https://submit-learner-data.service.gov.uk/find-a-learning-aim/\">other maths qualifications (opens in new tab)</a> approved for teaching to 16 to 19-year-olds who meet the condition of funding<br><br>GCSE physics",
+          "Yes"
+        ])
+
+        expect(rows).to include(["Performance measures", "No", "No"])
+
+        expect(rows).to include(["Disciplinary action", "No", "No"])
+      end
+
+      it "returns the reason for not enrolling if applicable" do
+        claim = create(
+          :claim,
+          :submitted,
+          policy: Policies::FurtherEducationPayments,
+          eligibility_trait: %i[eligible provider_verification_completed],
+          eligibility_attributes: {
+            provider_verification_teaching_qualification: "no_but_planned",
+            provider_verification_not_started_qualification_reasons: ["no_valid_reason"]
+          }
+        )
+
+        rows = described_class.new(claim).provider_verification_rows
+
+        expect(rows).to include([
+          "Reason for not enrolling",
+          "N/A",
+          "No valid reason"
+        ])
       end
     end
   end
