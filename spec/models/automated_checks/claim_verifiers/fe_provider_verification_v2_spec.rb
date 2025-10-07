3eafab99f957d5a6e21a85508ca3ad09a79a031b
Richard Lynch <rjlynchdev@gmail.com>
2025-10-07 14:01:56 +0100

    Add claim verifier for fe_provider_verification_v2
    If the provider has chosen "no_valid_reason" for why the claimant hasn't
    completed their teaching qualification we want to fail the
    "fe_provider_verification_v2" task.

--- spec/models/automated_checks/claim_verifiers/fe_provider_verification_v2_spec.rb
+++ spec/models/automated_checks/claim_verifiers/fe_provider_verification_v2_spec.rb
@@ -26,6 +26,7 @@ RSpec.describe AutomatedChecks::ClaimVerifiers::FeProviderVerificationV2 do
         }.to change(Task, :count).by(1)
 
         expect(Task.last.passed?).to be_falsey
+        expect(Task.last.data).to eq({"failed_checks" => ["no_continued_employment"]})
       end
 
       context "when task already persisted" do
@@ -57,6 +58,42 @@ RSpec.describe AutomatedChecks::ClaimVerifiers::FeProviderVerificationV2 do
         expect {
           subject.perform
         }.to not_change(Task, :count)
+      end
+    end
+
+    context "when the claimant doesn't have a valid reason for not starting their qualification" do
+      let(:eligibility) do
+        build(
+          :further_education_payments_eligibility,
+          :eligible,
+          :provider_verification_completed,
+          provider_verification_not_started_qualification_reasons: ["no_valid_reason"]
+        )
+      end
+
+      it "creates a failed task" do
+        subject.perform
+
+        task = claim.tasks.find_by!(name: "fe_provider_verification_v2")
+
+        expect(task.failed?).to be true
+        expect(task.manual?).to be false
+        expect(task.data).to eq({"failed_checks" => ["no_valid_reason_for_not_starting_qualification"]})
+      end
+    end
+
+    context "when the claimant has a valid reason for not starting their qualification" do
+      let(:eligibility) do
+        build(
+          :further_education_payments_eligibility,
+          :eligible,
+          :provider_verification_completed,
+          provider_verification_not_started_qualification_reasons: ["workload"]
+        )
+      end
+
+      it "doesn't create a task" do
+        expect { subject.perform }.not_to change { claim.tasks.count }
       end
     end
   end
