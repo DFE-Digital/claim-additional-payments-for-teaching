3d46e326014d41cdd36b239422c2690a2a6f0593
Richard Lynch <rjlynchdev@gmail.com>
2025-10-09 14:42:25 +0100

    Rescue missing submitted claim
    If the session has been cleared and the claimant attempts to visit the
    confirmation page we we're throwing an error. This commit changes the
    behaviour such that we send them back to the journey start page.
    
    We may want to consider doing something else, such as putting the claim
    reference in the url on submission and using that to find the submitted
    claim. However as the confirmation page shows some details about the
    claimant we've opted against that for the time being.

--- app/controllers/concerns/form_submittable.rb
+++ app/controllers/concerns/form_submittable.rb
@@ -14,6 +14,12 @@ module FormSubmittable
       render_template_for_current_slug
 
       @form.after_render
+    rescue ActionView::Template::Error => e
+      if e.cause.is_a?(Journeys::ConfirmationForm::SubmittedClaimNotFound)
+        redirect_to landing_page_path(current_journey_routing_name)
+      else
+        raise e
+      end
     end
 
     def create
