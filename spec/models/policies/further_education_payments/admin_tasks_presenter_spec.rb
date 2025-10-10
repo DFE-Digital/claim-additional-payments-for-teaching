7863a923d64788e44eeab997a87a82376a696904
Richard Lynch <rjlynchdev@gmail.com>
2025-10-10 13:31:56 +0100

    Match provider and claimant answers in presenter
    When we're displaying the provider answers in the admin task presenter
    we want to map them to the equivalent claimant answer to make the list
    easier to scan.

--- spec/models/policies/further_education_payments/admin_tasks_presenter_spec.rb
+++ spec/models/policies/further_education_payments/admin_tasks_presenter_spec.rb
@@ -91,8 +91,8 @@ RSpec.describe Policies::FurtherEducationPayments::AdminTasksPresenter do
 
         expect(rows).to include([
           "Timetabled teaching hours",
-          "12 or more hours per week, but fewer than 20",
-          "20 hours or more per week"
+          "12 or more hours",
+          "12 or more hours"
         ])
 
         expect(rows).to include(["Age range taught", "Yes", "Yes"])
@@ -133,6 +133,67 @@ RSpec.describe Policies::FurtherEducationPayments::AdminTasksPresenter do
           "N/A",
           "No valid reason"
         ])
+      end
+    end
+
+    describe "timetabled_teaching_hours" do
+      let(:claim) do
+        create(
+          :claim,
+          :submitted,
+          policy: Policies::FurtherEducationPayments,
+          eligibility_trait: %i[eligible provider_verification_completed],
+          eligibility_attributes: eligibility_attributes
+        )
+      end
+
+      describe "provider answers" do
+        subject do
+          described_class
+            .new(claim)
+            .provider_verification_rows[4]
+            .last
+        end
+
+        context "when provider answers '20_or_more_hours_per_week" do
+          let(:eligibility_attributes) do
+            {
+              provider_verification_teaching_hours_per_week: "20_or_more_hours_per_week"
+            }
+          end
+
+          it { is_expected.to eq("12 or more hours") }
+        end
+
+        context "when provider answers '12_to_20_hours_per_week" do
+          let(:eligibility_attributes) do
+            {
+              provider_verification_teaching_hours_per_week: "12_to_20_hours_per_week"
+            }
+          end
+
+          it { is_expected.to eq("12 or more hours") }
+        end
+
+        context "when provider answers '2_and_a_half_to_12_hours_per_week" do
+          let(:eligibility_attributes) do
+            {
+              provider_verification_teaching_hours_per_week: "2_and_a_half_to_12_hours_per_week"
+            }
+          end
+
+          it { is_expected.to eq("2.5 or more hours per week, but fewer than 12") }
+        end
+
+        context "when provider answers 'fewer_than_2_and_a_half_hours_per_week" do
+          let(:eligibility_attributes) do
+            {
+              provider_verification_teaching_hours_per_week: "fewer_than_2_and_a_half_hours_per_week"
+            }
+          end
+
+          it { is_expected.to eq("Fewer than 2.5 hours each week") }
+        end
       end
     end
   end
