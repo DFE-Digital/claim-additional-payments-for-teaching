cb4f9dcc4b59a35a91e5d334c1a9ee75511b7229
Richard Lynch <rjlynchdev@gmail.com>
2025-10-15 16:31:19 +0100

    Rename trait
    Renames the `verified` trait to `year_one_verified` to avoid confusion.
    Year one FE claims have a different provider verification mechanism to
    year two claims.
    We've also removed some unnecessary uses of `:verified`.

--- spec/factories/policies/further_education_payments/eligibilities.rb
+++ spec/factories/policies/further_education_payments/eligibilities.rb
@@ -23,7 +23,7 @@ FactoryBot.define do
       flagged_as_duplicate { true }
     end
 
-    trait :verified do
+    trait :year_one_verified do
       verification do
         {
           "assertions" => [
@@ -65,7 +65,7 @@ FactoryBot.define do
       end
     end
 
-    trait :verified_variable_hours do
+    trait :year_one_verified_variable_hours do
       contract_type { "variable_hours" }
       teaching_responsibilities { true }
       further_education_teaching_start_year { "2023" }
