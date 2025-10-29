315225b302be4756a93fb8b3286bd81d9f5afcb4
Richard Lynch <rjlynchdev@gmail.com>
2025-10-29 13:30:34 +0000

    Automate task passing / failing
    Adds more scenarios that can auto fail the provider verification task.
    Adds the pass conditions that cause the task to be automatically passed.

--- spec/models/automated_checks/claim_verifiers/fe_provider_verification_v2_spec.rb
+++ spec/models/automated_checks/claim_verifiers/fe_provider_verification_v2_spec.rb
@@ -12,201 +12,392 @@ RSpec.describe AutomatedChecks::ClaimVerifiers::FeProviderVerificationV2 do
       )
     end
 
-    context "when provider declares no for claimant continued employment" do
-      let(:eligibility) do
-        build(
-          :further_education_payments_eligibility,
-          provider_verification_continued_employment: false
-        )
-      end
-
-      it "persists task as failed" do
-        expect {
-          subject.perform
-        }.to change(Task, :count).by(1)
-
-        expect(Task.last.passed?).to be_falsey
-        expect(Task.last.data).to eq({"failed_checks" => ["no_continued_employment"]})
-      end
-
-      context "when task already persisted" do
-        before do
-          Task.create!(
-            name: described_class::TASK_NAME,
-            claim:,
-            passed: true
+    context "automatically failing the task" do
+      context "when the provider has sellected no for teaching responsibilities" do
+        let(:eligibility) do
+          build(
+            :further_education_payments_eligibility,
+            provider_verification_teaching_responsibilities: false
           )
         end
 
-        it "not create another task" do
+        it "persists task as failed" do
+          expect {
+            subject.perform
+          }.to change(Task, :count).by(1)
+
+          expect(Task.last.passed?).to be_falsey
+          expect(Task.last.data).to eq({"failed_checks" => ["no_teaching_responsibilities"]})
+        end
+      end
+
+      context "when the provider and claimant teaching start hours don't match" do
+        let(:eligibility) do
+          build(
+            :further_education_payments_eligibility,
+            provider_verification_teaching_start_year_matches_claim: false
+          )
+        end
+
+        it "persists task as failed" do
+          expect {
+            subject.perform
+          }.to change(Task, :count).by(1)
+
+          expect(Task.last.passed?).to be_falsey
+          expect(Task.last.data).to eq({"failed_checks" => ["teaching_start_year_mismatch"]})
+        end
+      end
+
+      context "when the provider has selected the claimant doesn't teach the correct age range" do
+        let(:eligibility) do
+          build(
+            :further_education_payments_eligibility,
+            provider_verification_half_teaching_hours: false
+          )
+        end
+
+        it "persists task as failed" do
+          expect {
+            subject.perform
+          }.to change(Task, :count).by(1)
+
+          expect(Task.last.passed?).to be_falsey
+          expect(Task.last.data).to eq({"failed_checks" => ["incorrect_age_range_taught"]})
+        end
+      end
+
+      context "when the provider has selected that the claimant doesn't teach the courses they claim" do
+        let(:eligibility) do
+          build(
+            :further_education_payments_eligibility,
+            provider_verification_half_timetabled_teaching_time: false
+          )
+        end
+
+        it "persists task as failed" do
+          expect {
+            subject.perform
+          }.to change(Task, :count).by(1)
+
+          expect(Task.last.passed?).to be_falsey
+          expect(Task.last.data).to eq({"failed_checks" => ["does_not_teach_claimed_courses"]})
+        end
+      end
+
+      context "when the provider has selected yes for performance measures" do
+        let(:eligibility) do
+          build(
+            :further_education_payments_eligibility,
+            provider_verification_performance_measures: true
+          )
+        end
+
+        it "persists task as failed" do
+          expect {
+            subject.perform
+          }.to change(Task, :count).by(1)
+
+          expect(Task.last.passed?).to be_falsey
+          expect(Task.last.data).to eq({"failed_checks" => ["performance_measures"]})
+        end
+      end
+
+      context "when the provider has selected yes for disciplinary action" do
+        let(:eligibility) do
+          build(
+            :further_education_payments_eligibility,
+            provider_verification_disciplinary_action: true
+          )
+        end
+
+        it "persists task as failed" do
+          expect {
+            subject.perform
+          }.to change(Task, :count).by(1)
+
+          expect(Task.last.passed?).to be_falsey
+          expect(Task.last.data).to eq({"failed_checks" => ["disciplinary_action"]})
+        end
+      end
+
+      context "when the provider and claimant contracted of employments don't match" do
+        let(:eligibility) do
+          build(
+            :further_education_payments_eligibility,
+            provider_verification_contract_type: "fixed_term",
+            contract_type: "permanent"
+          )
+        end
+
+        it "persists task as failed" do
+          expect {
+            subject.perform
+          }.to change(Task, :count).by(1)
+
+          expect(Task.last.passed?).to be_falsey
+          expect(Task.last.data).to eq({"failed_checks" => ["contract_of_employment_mismatch"]})
+        end
+      end
+
+      context "when the provider says the claimant does not plan to enroll on a qualification" do
+        let(:eligibility) do
+          build(
+            :further_education_payments_eligibility,
+            provider_verification_teaching_qualification: "no_not_planned"
+          )
+        end
+
+        it "persists task as failed" do
+          expect {
+            subject.perform
+          }.to change(Task, :count).by(1)
+
+          expect(Task.last.passed?).to be_falsey
+          expect(Task.last.data).to eq({"failed_checks" => ["no_plans_for_teaching_qualification"]})
+        end
+      end
+
+      context "when provider declares no for claimant continued employment" do
+        let(:eligibility) do
+          build(
+            :further_education_payments_eligibility,
+            provider_verification_continued_employment: false
+          )
+        end
+
+        it "persists task as failed" do
+          expect {
+            subject.perform
+          }.to change(Task, :count).by(1)
+
+          expect(Task.last.passed?).to be_falsey
+          expect(Task.last.data).to eq({"failed_checks" => ["no_continued_employment"]})
+        end
+
+        context "when task already persisted" do
+          before do
+            Task.create!(
+              name: described_class::TASK_NAME,
+              claim:,
+              passed: true
+            )
+          end
+
+          it "not create another task" do
+            expect {
+              subject.perform
+            }.to not_change(Task, :count)
+          end
+        end
+      end
+
+      context "when provider declares yes for claimant continued employment" do
+        let(:eligibility) do
+          build(
+            :further_education_payments_eligibility,
+            provider_verification_continued_employment: true
+          )
+        end
+
+        it "does not persist a task" do
           expect {
             subject.perform
           }.to not_change(Task, :count)
         end
       end
-    end
 
-    context "when provider declares yes for claimant continued employment" do
-      let(:eligibility) do
-        build(
-          :further_education_payments_eligibility,
-          provider_verification_continued_employment: true
-        )
-      end
+      context "when the claimant doesn't have a valid reason for not starting their qualification" do
+        let(:eligibility) do
+          build(
+            :further_education_payments_eligibility,
+            :eligible,
+            :provider_verification_completed,
+            provider_verification_not_started_qualification_reasons: ["no_valid_reason"]
+          )
+        end
 
-      it "does not persist a task" do
-        expect {
+        it "creates a failed task" do
           subject.perform
-        }.to not_change(Task, :count)
+
+          task = claim.tasks.find_by!(name: "fe_provider_verification_v2")
+
+          expect(task.failed?).to be true
+          expect(task.manual?).to be false
+          expect(task.data["failed_checks"]).to include(
+            "no_valid_reason_for_not_starting_qualification"
+          )
+        end
+      end
+
+      context "when the claimant has a valid reason for not starting their qualification" do
+        let(:eligibility) do
+          build(
+            :further_education_payments_eligibility,
+            :eligible,
+            :provider_verification_completed,
+            provider_verification_not_started_qualification_reasons: ["workload"],
+            contract_type: "fixed_term",
+            provider_verification_contract_type: "fixed_term"
+          )
+        end
+
+        it "doesn't create a failed task" do
+          expect { subject.perform }.not_to change { claim.tasks.where(passed: false).count }
+        end
+      end
+
+      context "when the provider states the claimant teaches fewer than 2.5 hours" do
+        let(:eligibility) do
+          build(
+            :further_education_payments_eligibility,
+            :eligible,
+            :provider_verification_completed,
+            provider_verification_teaching_hours_per_week: "fewer_than_2_and_a_half_hours_per_week"
+          )
+        end
+
+        it "creates a failed task" do
+          subject.perform
+
+          task = claim.tasks.find_by!(name: "fe_provider_verification_v2")
+
+          expect(task.failed?).to be true
+          expect(task.manual?).to be false
+          expect(task.data["failed_checks"]).to include(
+            "insufficient_teaching_hours_per_week"
+          )
+        end
+      end
+
+      context "when the provider states the claimant teaches 2.5 hours or more" do
+        context "when the the claimant selects 12 or more hours" do
+          let(:eligibility) do
+            build(
+              :further_education_payments_eligibility,
+              :eligible,
+              :provider_verification_completed,
+              provider_verification_teaching_hours_per_week: "2_and_a_half_to_12_hours_per_week",
+              teaching_hours_per_week: "more_than_12"
+            )
+          end
+
+          it "creates a failed task" do
+            subject.perform
+
+            task = claim.tasks.find_by!(name: "fe_provider_verification_v2")
+
+            expect(task.failed?).to be true
+            expect(task.manual?).to be false
+            expect(task.data["failed_checks"]).to include(
+              "mismatch_in_teaching_hours"
+            )
+          end
+        end
+
+        context "when the claimant select 20 hours or more" do
+          let(:eligibility) do
+            build(
+              :further_education_payments_eligibility,
+              :eligible,
+              :provider_verification_completed,
+              provider_verification_teaching_hours_per_week: "2_and_a_half_to_12_hours_per_week",
+              teaching_hours_per_week: "more_than_20"
+            )
+          end
+
+          it "creates a failed task" do
+            subject.perform
+
+            task = claim.tasks.find_by!(name: "fe_provider_verification_v2")
+
+            expect(task.failed?).to be true
+            expect(task.manual?).to be false
+            expect(task.data["failed_checks"]).to include(
+              "mismatch_in_teaching_hours"
+            )
+          end
+        end
+      end
+
+      context "when the claimant teaches between_2_5_and_12" do
+        context "when the provider selects 12 or more hours" do
+          let(:eligibility) do
+            build(
+              :further_education_payments_eligibility,
+              :eligible,
+              :provider_verification_completed,
+              provider_verification_teaching_hours_per_week: "12_to_20_hours_per_week",
+              teaching_hours_per_week: "between_2_5_and_12"
+            )
+          end
+
+          it "creates a failed task" do
+            subject.perform
+
+            task = claim.tasks.find_by!(name: "fe_provider_verification_v2")
+
+            expect(task.failed?).to be true
+            expect(task.manual?).to be false
+            expect(task.data["failed_checks"]).to include(
+              "mismatch_in_teaching_hours"
+            )
+          end
+        end
+
+        context "when the provider selects 20 hours or more" do
+          let(:eligibility) do
+            build(
+              :further_education_payments_eligibility,
+              :eligible,
+              :provider_verification_completed,
+              provider_verification_teaching_hours_per_week: "20_or_more_hours_per_week",
+              teaching_hours_per_week: "between_2_5_and_12"
+            )
+          end
+
+          it "creates a failed task" do
+            subject.perform
+
+            task = claim.tasks.find_by!(name: "fe_provider_verification_v2")
+
+            expect(task.failed?).to be true
+            expect(task.manual?).to be false
+            expect(task.data["failed_checks"]).to include(
+              "mismatch_in_teaching_hours"
+            )
+          end
+        end
       end
     end
 
-    context "when the claimant doesn't have a valid reason for not starting their qualification" do
+    context "automatically passing the task" do
       let(:eligibility) do
         build(
           :further_education_payments_eligibility,
-          :eligible,
-          :provider_verification_completed,
-          provider_verification_not_started_qualification_reasons: ["no_valid_reason"]
+          provider_verification_teaching_responsibilities: true,
+          provider_verification_teaching_start_year_matches_claim: true,
+          provider_verification_half_teaching_hours: true,
+          provider_verification_half_timetabled_teaching_time: true,
+          provider_verification_performance_measures: false,
+          provider_verification_disciplinary_action: false,
+          provider_verification_contract_type: "permanent",
+          contract_type: "permanent",
+          provider_verification_teaching_qualification: "yes",
+          provider_verification_continued_employment: true,
+          provider_verification_teaching_hours_per_week: "20_or_more_hours_per_week",
+          teaching_hours_per_week: "more_than_20"
         )
       end
 
-      it "creates a failed task" do
+      it "passes the task" do
         subject.perform
 
         task = claim.tasks.find_by!(name: "fe_provider_verification_v2")
 
-        expect(task.failed?).to be true
+        expect(task.passed?).to be true
         expect(task.manual?).to be false
-        expect(task.data).to eq({"failed_checks" => ["no_valid_reason_for_not_starting_qualification"]})
-      end
-    end
-
-    context "when the claimant has a valid reason for not starting their qualification" do
-      let(:eligibility) do
-        build(
-          :further_education_payments_eligibility,
-          :eligible,
-          :provider_verification_completed,
-          provider_verification_not_started_qualification_reasons: ["workload"]
-        )
-      end
-
-      it "doesn't create a task" do
-        expect { subject.perform }.not_to change { claim.tasks.count }
-      end
-    end
-
-    context "when the provider states the claimant teaches fewer than 2.5 hours" do
-      let(:eligibility) do
-        build(
-          :further_education_payments_eligibility,
-          :eligible,
-          :provider_verification_completed,
-          provider_verification_teaching_hours_per_week: "fewer_than_2_and_a_half_hours_per_week"
-        )
-      end
-
-      it "creates a failed task" do
-        subject.perform
-
-        task = claim.tasks.find_by!(name: "fe_provider_verification_v2")
-
-        expect(task.failed?).to be true
-        expect(task.manual?).to be false
-        expect(task.data).to eq({"failed_checks" => ["insufficient_teaching_hours_per_week"]})
-      end
-    end
-
-    context "when the provider states the claimant teaches 2.5 hours or more" do
-      context "when the the claimant selects 12 or more hours" do
-        let(:eligibility) do
-          build(
-            :further_education_payments_eligibility,
-            :eligible,
-            :provider_verification_completed,
-            provider_verification_teaching_hours_per_week: "2_and_a_half_to_12_hours_per_week",
-            teaching_hours_per_week: "more_than_12"
-          )
-        end
-
-        it "creates a failed task" do
-          subject.perform
-
-          task = claim.tasks.find_by!(name: "fe_provider_verification_v2")
-
-          expect(task.failed?).to be true
-          expect(task.manual?).to be false
-          expect(task.data).to eq({"failed_checks" => ["mismatch_in_teaching_hours"]})
-        end
-      end
-
-      context "when the claimant select 20 hours or more" do
-        let(:eligibility) do
-          build(
-            :further_education_payments_eligibility,
-            :eligible,
-            :provider_verification_completed,
-            provider_verification_teaching_hours_per_week: "2_and_a_half_to_12_hours_per_week",
-            teaching_hours_per_week: "more_than_20"
-          )
-        end
-
-        it "creates a failed task" do
-          subject.perform
-
-          task = claim.tasks.find_by!(name: "fe_provider_verification_v2")
-
-          expect(task.failed?).to be true
-          expect(task.manual?).to be false
-          expect(task.data).to eq({"failed_checks" => ["mismatch_in_teaching_hours"]})
-        end
-      end
-    end
-
-    context "when the claimant teaches between_2_5_and_12" do
-      context "when the provider selects 12 or more hours" do
-        let(:eligibility) do
-          build(
-            :further_education_payments_eligibility,
-            :eligible,
-            :provider_verification_completed,
-            provider_verification_teaching_hours_per_week: "12_to_20_hours_per_week",
-            teaching_hours_per_week: "between_2_5_and_12"
-          )
-        end
-
-        it "creates a failed task" do
-          subject.perform
-
-          task = claim.tasks.find_by!(name: "fe_provider_verification_v2")
-
-          expect(task.failed?).to be true
-          expect(task.manual?).to be false
-          expect(task.data).to eq({"failed_checks" => ["mismatch_in_teaching_hours"]})
-        end
-      end
-
-      context "when the provider selects 20 hours or more" do
-        let(:eligibility) do
-          build(
-            :further_education_payments_eligibility,
-            :eligible,
-            :provider_verification_completed,
-            provider_verification_teaching_hours_per_week: "20_or_more_hours_per_week",
-            teaching_hours_per_week: "between_2_5_and_12"
-          )
-        end
-
-        it "creates a failed task" do
-          subject.perform
-
-          task = claim.tasks.find_by!(name: "fe_provider_verification_v2")
-
-          expect(task.failed?).to be true
-          expect(task.manual?).to be false
-          expect(task.data).to eq({"failed_checks" => ["mismatch_in_teaching_hours"]})
-        end
+        expect(task.data).to be nil
       end
     end
   end
