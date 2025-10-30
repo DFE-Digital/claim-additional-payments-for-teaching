0f67e15eb36405359c4aee2dcff406e64d41eadd
Phil Lee <asmega@users.noreply.github.com>
2025-10-30 12:37:15 +0000

    tweak rails_semantic_logger monkey patches

--- config/initializers/rails_semantic_logger_8_monkeypatch.rb
+++ config/initializers/rails_semantic_logger_8_monkeypatch.rb
@@ -1,21 +1,35 @@
 # frozen_string_literal: true
 
 # see https://github.com/reidmorrison/rails_semantic_logger/issues/249
+# see https://github.com/reidmorrison/rails_semantic_logger/pull/276
 
-LAST_TESTED_VERSION = "4.17.0"
+LAST_TESTED_VERSION = "4.18.0"
 
 require "rails_semantic_logger/version"
 
 unless RailsSemanticLogger::VERSION == LAST_TESTED_VERSION
-  raise "rails_semantic_logger is version #{RailsSemanticLogger::VERSION} but the monkey patch was last tested on #{LAST_TESTED_VERSION} - manually check if it supports Rails 8 now and this can be removed, or that this still works as intended"
+  raise "rails_semantic_logger is version #{RailsSemanticLogger::VERSION} but the monkey patch was last tested on " \
+        "#{LAST_TESTED_VERSION} - manually check if it can find the sql_runtime module."
 end
 
-module LogSubscriberMonkeyPatch
-  def self.included(base)
-    base.alias_method(:bind_values, :bind_values_v6_1)
-    base.alias_method(:render_bind, :render_bind_v6_1)
-    base.alias_method(:type_casted_binds, :type_casted_binds_v5_1_5)
+module RailsSemanticLogger
+  module ActiveRecord
+    class LogSubscriber < ActiveSupport::LogSubscriber
+      def self.runtime=(value)
+        if ::ActiveRecord::RuntimeRegistry.respond_to?(:stats)
+          ::ActiveRecord::RuntimeRegistry.stats.sql_runtime = value
+        else
+          ::ActiveRecord::RuntimeRegistry.sql_runtime = value
+        end
+      end
+
+      def self.runtime
+        if ::ActiveRecord::RuntimeRegistry.respond_to?(:stats)
+          ::ActiveRecord::RuntimeRegistry.stats.sql_runtime ||= 0
+        else
+          ::ActiveRecord::RuntimeRegistry.sql_runtime ||= 0
+        end
+      end
+    end
   end
 end
-
-RailsSemanticLogger::ActiveRecord::LogSubscriber.include(LogSubscriberMonkeyPatch)
