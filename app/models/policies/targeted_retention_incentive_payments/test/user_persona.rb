6413a84b04c6035b33e94f08ace3743475d75664
Phil Lee <asmega@users.noreply.github.com>
2025-12-17 15:27:13 +0000

    generate school workfoce census data from personas

--- app/models/policies/targeted_retention_incentive_payments/test/user_persona.rb
+++ app/models/policies/targeted_retention_incentive_payments/test/user_persona.rb
@@ -4,16 +4,31 @@ module Policies
       class UserPersona
         FILE = Rails.root.join("spec/personas/targeted_retention_incentive_payments.csv")
 
-        attr_reader :school_name
-
-        def initialize(csv_row)
-          @school_name = csv_row["School name"]
-        end
+        attr_reader :school_name, :trn, :teaching_subject, :expected_result
 
         def self.all
           collection = []
           CSV.foreach(FILE, headers: true) { |row| collection << new(row) }
           collection
+        end
+
+        def self.eligible
+          all.select { |persona| persona.eligible? }
+        end
+
+        def initialize(csv_row)
+          @school_name = csv_row["School name"]
+          @trn = csv_row["TRN"]
+          @teaching_subject = csv_row["Teaching subject"]
+          @expected_result = csv_row["Expected result"]
+        end
+
+        def eligible?
+          expected_result == "Eligible"
+        end
+
+        def school
+          @school ||= School.find_by(name: school_name)
         end
       end
     end
--- lib/tasks/test_data_seeder.rake
+++ lib/tasks/test_data_seeder.rake
@@ -55,4 +55,9 @@ namespace :test_data_seeder do
   task "generate_stri_awards" => :environment do
     puts Policies::TargetedRetentionIncentivePayments::Test::StriAwardsGenerator.to_csv
   end
+
+  desc "generate School Workforce Census CSV from personas"
+  task "generate_school_workforce_census" => :environment do
+    puts Policies::TargetedRetentionIncentivePayments::Test::SchoolWorkforceCensusGenerator.to_csv
+  end
 end
