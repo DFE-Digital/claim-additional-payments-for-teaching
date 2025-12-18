f2dc0da03b263664387bc3409279472873ce5743
Richard Lynch <rjlynchdev@gmail.com>
2025-12-18 14:05:23 +0000

    Add ability to export page to CSV

--- app/controllers/admin/task_lists_controller.rb
+++ app/controllers/admin/task_lists_controller.rb
@@ -5,7 +5,16 @@ module Admin
     def index
       @presenter = Admin::TaskListForm.new(task_list_params)
 
-      # TODO respond to CSV
+      respond_to do |format|
+        format.html
+
+        format.csv do
+          send_data(
+            @presenter.to_csv,
+            filename: "task-list-#{Time.zone.now.strftime("%Y%m%d%H%M%S")}.csv"
+          )
+        end
+      end
     end
 
     private
