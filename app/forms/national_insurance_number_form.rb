60ab0281042385b3e940e1e776e2f3db81b42d2a
Richard Lynch <rjlynchdev@gmail.com>
2026-01-23 11:36:44 +0000

    Upper case nino before validation
    We waant to allow users to enter their nino in lower case

--- app/forms/national_insurance_number_form.rb
+++ app/forms/national_insurance_number_form.rb
@@ -1,6 +1,12 @@
 class NationalInsuranceNumberForm < Form
   NINO_REGEX_FILTER = /\A[A-Z]{2}[0-9]{6}[A-D]{1}\Z/
 
+  before_validation do
+    if national_insurance_number.present?
+      self.national_insurance_number = national_insurance_number.gsub(/\s/, "").upcase
+    end
+  end
+
   attribute :national_insurance_number, :string, strip_all_whitespace: true
 
   validates :national_insurance_number, presence: {message: "Enter a National Insurance number in the correct format"}
@@ -16,16 +22,10 @@ class NationalInsuranceNumberForm < Form
     return false if invalid?
 
     journey_session.answers.assign_attributes(
-      national_insurance_number: normalised_ni_number
+      national_insurance_number: national_insurance_number
     )
     journey_session.save!
 
     true
-  end
-
-  private
-
-  def normalised_ni_number
-    national_insurance_number.gsub(/\s/, "").upcase
   end
 end
