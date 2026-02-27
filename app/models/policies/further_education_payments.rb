24094e2d6f70b665efce156218277c8c03f5e5a1
Richard Lynch <rjlynchdev@gmail.com>
2026-02-27 13:36:38 +0000

    Don't allow approving fe/tri/loans claims
    Adds a feature flag to prevent fe/tri/loans claims from being approved.
    We can enable this feature flag when we get the go ahead for allowing
    approvals. Once the feature flag has been enabled we should open a new
    pr to remove this code.

--- app/models/policies/further_education_payments.rb
+++ app/models/policies/further_education_payments.rb
@@ -153,6 +153,8 @@ module Policies
     end
 
     def approvable?(claim)
+      return false unless FeatureFlag.enabled?(:fe_claims_approvable)
+
       ClaimCheckingTasks.new(claim).incomplete_task_names.exclude?(
         "alternative_identity_verification"
       ) && !claim.tasks.find_by(name: "alternative_identity_verification")&.failed?
