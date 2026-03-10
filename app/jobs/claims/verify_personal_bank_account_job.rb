8017915033a5a1d83edf227e8e74f373ce5c7c96
Richard Lynch <rjlynchdev@gmail.com>
2026-03-10 16:38:05 +0000

    Add job to retry bank validation
    We had some issues with validating bank details against the HMRC api.
    This commit adds a job to retry the validation.
    We'll enqueue RetryFailedPersonalBankAccountVerificationJob from the
    rails console and let that queue up jobs for each claim.

---
+++ app/jobs/claims/verify_personal_bank_account_job.rb
@@ -0,0 +1,36 @@
+module Claims
+  class VerifyPersonalBankAccountJob < ApplicationJob
+    def perform(claim)
+      return if claim.hmrc_bank_validation_succeeded
+
+      response = get_hmrc_response(claim)
+
+      claim.hmrc_bank_validation_succeeded = !response.errored? && response.success?
+
+      code = response.try(:code) || response.try(:status) || 1
+
+      new_response_data = {
+        code: code,
+        body: response.safe_body
+      }
+
+      claim.hmrc_bank_validation_responses << new_response_data
+
+      claim.save!
+    end
+
+    def priority
+      10
+    end
+
+    private
+
+    def get_hmrc_response(claim)
+      Hmrc.client.verify_personal_bank_account(
+        claim.bank_sort_code,
+        claim.bank_account_number,
+        claim.banking_name
+      )
+    end
+  end
+end
---
+++ spec/jobs/claims/retry_failed_personal_bank_account_verification_job_spec.rb
@@ -0,0 +1,65 @@
+require "rails_helper"
+
+RSpec.describe Claims::RetryFailedPersonalBankAccountVerificationJob, type: :job do
+  context "when the claim is outside the date range" do
+    it "doesn't queue a verification job" do
+      create(
+        :claim,
+        :submitted,
+        created_at: DateTime.new(2026, 3, 1, 9, 0, 0),
+        hmrc_bank_validation_succeeded: false
+      )
+
+      described_class.perform_now
+
+      expect(Claims::VerifyPersonalBankAccountJob).not_to have_been_enqueued
+    end
+  end
+
+  context "when the claim is within the date range" do
+    context "when the claim has already been successfully validated" do
+      it "doesn't queue a verification job" do
+        create(
+          :claim,
+          :submitted,
+          created_at: DateTime.new(2026, 3, 2, 9, 0, 0),
+          hmrc_bank_validation_succeeded: true
+        )
+
+        described_class.perform_now
+
+        expect(Claims::VerifyPersonalBankAccountJob).not_to have_been_enqueued
+      end
+    end
+
+    context "when the claim has not been successfully validated" do
+      it "queues a verification job for each claim spaced 5 seconds a part" do
+        claim_1 = create(
+          :claim,
+          :submitted,
+          created_at: DateTime.new(2026, 3, 2, 9, 0, 0),
+          hmrc_bank_validation_succeeded: false,
+          reference: "claim_1"
+        )
+
+        claim_2 = create(
+          :claim,
+          :submitted,
+          created_at: DateTime.new(2026, 3, 2, 9, 0, 5),
+          hmrc_bank_validation_succeeded: false,
+          reference: "claim_2"
+        )
+
+        described_class.perform_now
+
+        expect(Claims::VerifyPersonalBankAccountJob).to(
+          have_been_enqueued.with(claim_1)
+        )
+
+        expect(Claims::VerifyPersonalBankAccountJob).to(
+          have_been_enqueued.with(claim_2)
+        )
+      end
+    end
+  end
+end
