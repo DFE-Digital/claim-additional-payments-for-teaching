216198cf5e374f00525a447f8b377989313779a8
Phil Lee <asmega@users.noreply.github.com>
2026-03-16 14:14:39 +0000

    fix OL query syntax

--- app/models/one_login_account.rb
+++ app/models/one_login_account.rb
@@ -9,7 +9,7 @@ class OneLoginAccount
     academic_year ||= journey.configuration.current_academic_year
 
     journey::Session
-      .where("answers -> 'onelogin_uid' ? :uid", uid:)
+      .where("answers #>> '{onelogin_uid}' = :uid", uid:)
       .where("answers #>> '{academic_year, start_year}' = :start_year", start_year: academic_year.start_year)
       .where("answers #>> '{academic_year, end_year}' = :end_year", end_year: academic_year.end_year)
       .order(updated_at: :asc)
