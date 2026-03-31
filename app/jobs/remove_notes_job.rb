5d4c0f6f6177df3d572385a8462804b5712d2432
James Grant <1993jamesgrant@gmail.com>
2026-03-31 10:20:26 +0100

    trigger census note creation
    Update census_subjects_taught.rb
    
    Update trigger_census_subjects_taught_job.rb

--- app/jobs/remove_notes_job.rb
+++
@@ -1,7 +0,0 @@
-class RemoveNotesJob < ApplicationJob
-  def perform
-    range = Date.new(2026, 3, 2).beginning_of_day..Date.new(2026, 3, 2).end_of_day
-
-    Task.where(name: "census_subjects_taught", created_at: range).destroy_all
-  end
-end
