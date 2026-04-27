28cb7f13432853ab6fb892c352a3ed2f070aecb4
Phil Lee <asmega@users.noreply.github.com>
2026-04-27 17:06:28 +0100

    EYTFI provider upload creates FileUpload

---
+++ spec/features/admin/early_years_teachers_financial_incentive_payments/eligible_eytfi_providers_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.feature "Admin of eligible EYTFI providers" do
+  let(:eligible_eytfi_providers_csv_file) do
+    file_fixture("eligible_eytfi_providers.csv")
+  end
+
+  scenario "manage eligible EYTFI providers" do
+    when_eytfi_journey_configuration_exists
+    sign_in_as_service_operator
+
+    click_link "Manage services"
+    click_link "Change Early Years Teachers Financial Incentive Payments"
+
+    select AcademicYear.current.to_s, from: "Academic year"
+    attach_file "eligible-eytfi-providers-upload-file-field", eligible_eytfi_providers_csv_file
+
+    expect {
+      click_button "Upload CSV"
+    }.to change(FileUpload, :count).by(1)
+  end
+end
