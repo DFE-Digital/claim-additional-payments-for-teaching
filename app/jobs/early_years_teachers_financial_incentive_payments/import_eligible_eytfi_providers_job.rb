4aa0001763cc594144309d13adcbee1eb55066dc
Phil Lee <asmega@users.noreply.github.com>
2026-04-29 12:42:27 +0100

    refactor factory into parser

--- app/jobs/early_years_teachers_financial_incentive_payments/import_eligible_eytfi_providers_job.rb
+++ app/jobs/early_years_teachers_financial_incentive_payments/import_eligible_eytfi_providers_job.rb
@@ -13,12 +13,39 @@ module EarlyYearsTeachersFinancialIncentivePayments
         @row = row
       end
 
+      def to_provider(file_upload:)
+        Policies::
+          EarlyYearsTeachersFinancialIncentivePayments::
+          EligibleEytfiProvider.new(
+            urn: row["Provider URN"],
+            name: row["Provider name"],
+            address_line_1: row["Provider address line 1"],
+            address_line_2: row["Provider address line 2"],
+            address_line_3: row["Provider address line 3"],
+            town: row["Provider town"],
+            postcode: row["Postcode"],
+            eligible: cast_bool(row["Eligible"]),
+            file_upload:
+          )
+      end
+
       private
 
       def validate_eligible_input
         return if ["TRUE", "FALSE"].include?(row["Eligible"])
 
         errors.add(:eligible, "must be TRUE or FALSE")
+      end
+
+      def cast_bool(value)
+        case value
+        when "TRUE"
+          true
+        when "FALSE"
+          false
+        else
+          raise "#{value} could not be cast to a boolean"
+        end
       end
     end
 
@@ -40,37 +67,12 @@ module EarlyYearsTeachersFinancialIncentivePayments
       end
 
       CSV.foreach(body_io, headers: true) do |row|
-        provider = Policies::
-          EarlyYearsTeachersFinancialIncentivePayments::
-          EligibleEytfiProvider.new(
-            urn: row["Provider URN"],
-            name: row["Provider name"],
-            address_line_1: row["Provider address line 1"],
-            address_line_2: row["Provider address line 2"],
-            address_line_3: row["Provider address line 3"],
-            town: row["Provider town"],
-            postcode: row["Postcode"],
-            eligible: cast_bool(row["Eligible"]),
-            file_upload:
-          )
-
+        parser = RowParser.new(row:)
+        provider = parser.to_provider(file_upload:)
         provider.save!
       end
 
       file_upload.update completed_processing_at: Time.zone.now
-    end
-
-    private
-
-    def cast_bool(value)
-      case value
-      when "TRUE"
-        true
-      when "FALSE"
-        false
-      else
-        raise "#{value} could not be cast to a boolean"
-      end
     end
   end
 end
