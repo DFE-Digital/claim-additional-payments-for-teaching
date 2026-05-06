dcf67a0cb8b43c9236a1fce1b4de939009061552
Richard Lynch <rjlynchdev@gmail.com>
2026-05-06 12:26:11 +0100

    Handle file upload versioning stuff

--- spec/factories/policies/early_years_teachers_financial_incentive_payments/eligible_eytfi_providers.rb
+++ spec/factories/policies/early_years_teachers_financial_incentive_payments/eligible_eytfi_providers.rb
@@ -2,7 +2,11 @@ FactoryBot.define do
   factory :eligible_eytfi_provider, class: "Policies::EarlyYearsTeachersFinancialIncentivePayments::EligibleEytfiProvider" do
     file_upload {
       FileUpload.latest_version_for(Policies::EarlyYearsTeachersFinancialIncentivePayments::EligibleEytfiProvider).first ||
-        create(:file_upload, target_data_model: Policies::EarlyYearsTeachersFinancialIncentivePayments::EligibleEytfiProvider.to_s)
+        create(
+          :file_upload,
+          :with_current_academic_year,
+          target_data_model: Policies::EarlyYearsTeachersFinancialIncentivePayments::EligibleEytfiProvider.to_s
+        )
     }
 
     urn { "EY#{rand(100000..999999)}" }
