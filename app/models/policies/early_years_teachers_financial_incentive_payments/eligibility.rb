9d43e6b5c6ee7337ec3c4fac3cc82db6aa485564
Richard Lynch <rjlynchdev@gmail.com>
2026-05-13 13:32:40 +0100

    Handle file uploads
    File upload interaction with models created from file uploads are a bit
    weird so we've removed the foreign key and will instead find the
    provider by urn.

--- app/models/policies/early_years_teachers_financial_incentive_payments/eligibility.rb
+++ app/models/policies/early_years_teachers_financial_incentive_payments/eligibility.rb
@@ -4,7 +4,6 @@ module Policies
       self.table_name = "early_years_teachers_financial_incentive_payments_eligibilities"
 
       has_one :claim, as: :eligibility, inverse_of: :eligibility
-      belongs_to :eligible_eytfi_provider
       has_many_attached :employment_proofs
 
       # does nothing, simply here for duck typing compatability
@@ -14,6 +13,12 @@ module Policies
 
       def policy
         Policies::EarlyYearsTeachersFinancialIncentivePayments
+      end
+
+      def eligible_eytfi_provider
+        EligibleEytfiProvider
+          .by_academic_year(claim.academic_year)
+          .find_by!(urn: eligible_eytfi_provider_urn)
       end
     end
   end
