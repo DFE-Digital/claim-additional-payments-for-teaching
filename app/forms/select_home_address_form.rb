511edeebd52bdb10f91b6343d8c252f6a7eb8842
Richard Lynch <rjlynchdev@gmail.com>
2026-06-09 14:27:43 +0100

    Don't validate session answers
    Previously this form was validating the session answers rather than it's
    attributes due to the string splitting involved in passing the address
    between the view and the form. Instead we can overload the
    `load_current_value` method to map the answers to the `address`
    attribute on the form, and use the `address` attribute to find the
    selected address radio option. This would be a bit more straightforward
    if the addresses were active record objects. The addresses will be read
    from the rails cache so we're not paying an API call penalty in this
    form.

--- app/forms/select_home_address_form.rb
+++ app/forms/select_home_address_form.rb
@@ -1,24 +1,20 @@
 class SelectHomeAddressForm < Form
   attribute :address, :string
-  attribute :address_line_1, :string
-  attribute :postcode, :string
   attribute :skip_postcode_search, :boolean
 
-  validate :validate_address_selected, unless: -> { skip_postcode_search? }
-  validate :validate_address_entered, if: -> { skip_postcode_search? }
+  validates(
+    :address,
+    inclusion: {
+      in: ->(form) { form.radio_options.map(&:id) },
+      message: "Select an address"
+    },
+    unless: :skip_postcode_search
+  )
 
   def radio_options
     address_data.map do |option|
-      id = [
-        option[:address],
-        option[:address_line_1],
-        option[:address_line_2],
-        option[:address_line_3],
-        option[:postcode]
-      ].join(":")
-
       Option.new(
-        id:,
+        id: option[:address],
         name: option[:address]
       )
     end
@@ -37,15 +33,15 @@ class SelectHomeAddressForm < Form
         postcode:
       )
     else
-      address_parts = address.split(":")
+      selected_address = address_data.detect { it[:address] == address }
 
       journey_session.answers.assign_attributes({
         skip_postcode_search: false,
-        address_line_1: address_parts[1].titleize,
-        address_line_2: address_parts[2].titleize,
-        address_line_3: address_parts[3].titleize,
+        address_line_1: selected_address[:address_line_1].titleize,
+        address_line_2: selected_address[:address_line_2].titleize,
+        address_line_3: selected_address[:address_line_3].titleize,
         address_line_4: nil,
-        postcode: address_parts[4]
+        postcode: selected_address[:postcode]
       })
     end
 
@@ -53,40 +49,36 @@ class SelectHomeAddressForm < Form
   end
 
   def completed?
-    skip_postcode_search? || address_selected_in_answers? || valid?
+    skip_postcode_search? || answers.address_present? || valid?
   end
 
   private
 
-  def address_data
-    return [] if postcode.blank?
+  def load_current_value(attribute)
+    if attribute.to_s == "address"
+      [
+        answers.address_line_1,
+        answers.address_line_2,
+        answers.address_line_3,
+        answers.address_line_4,
+        answers.postcode
+      ].compact.join(", ")
+    else
+      super
+    end
+  end
 
-    @address_data ||= Rails.cache.fetch("address_data/#{postcode}", expires_in: 1.hour) do
+  def address_data
+    return [] if answers.postcode.blank?
+
+    @address_data ||= Rails.cache.fetch("address_data/#{answers.postcode}", expires_in: 1.hour) do
       OrdnanceSurvey::Client.new.api.search_places.index(
-        params: {postcode:}
+        params: {postcode: answers.postcode}
       )
     end
   end
 
   def skip_postcode_search?
     journey_session.answers.skip_postcode_search || skip_postcode_search
-  end
-
-  def validate_address_selected
-    if address.present?
-      return
-    end
-
-    errors.add(:address, "Select an address")
-  end
-
-  def validate_address_entered
-    if journey_session.answers.address_line_1.blank? && journey_session.answers.postcode.blank?
-      errors.add(:address, "Enter an address") if journey_session.answers.address_line_1.blank?
-    end
-  end
-
-  def address_selected_in_answers?
-    journey_session.answers.address_line_1.present? && journey_session.answers.postcode.present?
   end
 end
