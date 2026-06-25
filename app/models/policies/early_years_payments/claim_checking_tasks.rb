92eb0dc0280555cd20894630f673b560a7502c3f
Richard Lynch <rjlynchdev@gmail.com>
2026-06-25 13:59:37 +0100

    Feature flag using persisted matching claims
    If something goes wrong we can quickly cut back to the old approach.

--- app/models/policies/early_years_payments/claim_checking_tasks.rb
+++ app/models/policies/early_years_payments/claim_checking_tasks.rb
@@ -13,7 +13,7 @@ module Policies
         tasks << "student_loan_plan" if claim.submitted_without_slc_data?
         tasks << "payroll_details" if claim.must_manually_validate_bank_details?
         tasks << "payroll_gender" if claim.payroll_gender_missing? || task_exists?("payroll_gender")
-        tasks << "matching_details" if task_exists?("matching_details")
+        tasks << "matching_details" if FeatureFlag.enabled?(:persist_matching_claims) ? task_exists?("matching_details") : matching_claims.exists?
 
         tasks
       end
