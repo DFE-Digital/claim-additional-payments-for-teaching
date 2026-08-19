9f965de889e6d20efaf7a8e8449d54bbe0e8cd26
Phil Lee <asmega@users.noreply.github.com>
2026-08-19 09:37:19 +0100

    admin sign out also calls DfE Sign In

--- spec/features/admin/admin_sessions_spec.rb
+++ spec/features/admin/admin_sessions_spec.rb
@@ -1,18 +1,31 @@
 require "rails_helper"
 
 RSpec.feature "Admin session management" do
-  scenario "A user is redirected to the admin root path after sign in" do
-    sign_in_as_service_operator
+  context "when DfE sign in bypassed" do
+    scenario "A user is redirected to the admin sign in page" do
+      sign_in_as_service_operator
 
-    expect(page).to have_link("Sign out")
-    expect(current_path).to eql(admin_root_path)
+      allow(DfeSignIn::Config.instance).to receive(:bypass?).and_return(true)
+      visit admin_claims_path
+      allow(DfeSignIn::Config.instance).to receive(:bypass?).and_return(false)
+      click_link("Sign out")
 
-    # - A signed in user can sign out
+      expect(current_path).to eql(admin_sign_in_path)
+    end
+  end
 
-    click_on "Sign out"
+  context "when DfE sign in not bypassed" do
+    before do
+      allow(DfeSignIn::Config.instance).to receive(:bypass?).and_return(false)
+    end
 
-    expect(page).to have_content("You've been signed out")
-    expect(current_path).to eql(admin_sign_in_path)
+    scenario "sign out link goes to DfE sign out" do
+      sign_in_as_service_operator
+
+      expect(page).to have_link("Sign out")
+      expect(current_path).to eql(admin_root_path)
+      expect(page).to have_link("Sign out", href: "https://issuer.example.com/session/end?post_logout_redirect_uri=https%3A%2F%2Ftest.example.com%2Fadmin%2Fauth%2Fsign-out&client_id=teacherpaymentsadmin")
+    end
   end
 
   scenario "A user is redirected to their original url after sign in" do
